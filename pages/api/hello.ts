80aedfe4c6ed56264cb21536cf6ebc7dec85a8e1
VHollund <vetlehoo@gmail.com>
2023-07-18 12:58:04 +0200

    :alembic: checking logging on serverside

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { logger } from '@navikt/aap-felles-utils';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  logger.info('Loggin works on serverside');
+  res.status(200).json({ name: 'John Doe' });
 }
