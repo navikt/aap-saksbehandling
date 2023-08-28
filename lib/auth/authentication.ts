ab068ab563e9f63ae0d9bd519d2874cdcd5b187a
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-08-28 13:39:50 +0200

    Legger til auth

---
+++ lib/auth/authentication.ts
@@ -0,0 +1,34 @@
+//import { logger } from '@navikt/aap-felles-utils';
+import { validateAzureToken } from '@navikt/next-auth-wonderwall';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export async function verifyUserLoggedIn(): Promise<void> {
+  const requestHeaders = headers();
+
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'localhost') {
+    console.log('Running locally, skipping authentication');
+    return;
+  }
+
+  const redirectPath = requestHeaders.get('x-path');
+  if (!redirectPath == null) {
+    //logger.warn("Missing 'x-path' header, is middleware middlewaring?");
+  }
+
+  const bearerToken = requestHeaders.get('Authorization');
+  if (!bearerToken) {
+    redirect(`${process.env.NEXT_PUBLIC_BASE_PATH}/oauth2/login?redirect=${redirectPath}`);
+  }
+
+  const validationResult = await validateAzureToken(bearerToken);
+  if (validationResult !== 'valid') {
+    const error = new Error(
+      `Invalid JWT token found (cause: ${validationResult.errorType} ${validationResult.message}, redirecting to login.`,
+      { cause: validationResult.error }
+    );
+    //logger.warn(error);
+
+    redirect(`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/oauth2/login?redirect=${redirectPath}`);
+  }
+}
