2e06d05437f160c112ecdb82bc5955b226d2f71f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-08-29 13:28:53 +0200

    ✨ Legger til route for å finne en sak

---
+++ app/api/sak/finn/route.ts
@@ -0,0 +1,22 @@
+import { FinnSakForIdent } from '../../../../lib/types/types';
+
+export async function POST(req: Request) {
+  const body: FinnSakForIdent = await req.json();
+
+  const res = await fetch('http://localhost:8080/api/sak/finn', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+
+  if (res.ok) {
+    return new Response(JSON.stringify(data), { status: 200 });
+  } else {
+    return new Response(JSON.stringify({ message: 'Noe gikk galt ved henting av sak.' }), { status: 200 });
+  }
+}
--- app/api/sak/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ sak: 'hei' }), { status: 200 });
-}
