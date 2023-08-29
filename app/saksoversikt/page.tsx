c8246a4b4f76c205008816a4edfc382a92059c6f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-08-29 15:46:07 +0200

    :fire: Fjerner useEffect og tilhørende kode

--- app/saksoversikt/page.tsx
+++ app/saksoversikt/page.tsx
@@ -1,37 +1,8 @@
 'use client';
 
-import { finnSak, hentAlleSaker, hentBehandling, hentSak, opprettSak } from '../../lib/api';
-import { useEffect } from 'react';
+import { opprettSak } from '../../lib/api';
 
 const Page = () => {
-  useEffect(() => {
-    async function fetchEverything() {
-      const alleSaker = await hentAlleSaker();
-
-      console.log('alleSaker', alleSaker);
-
-      const finnSakResponse = await finnSak('12345678910');
-
-      console.log('finn', finnSakResponse);
-
-      if (finnSakResponse && finnSakResponse[0].saksnummer) {
-        const sak = await hentSak(finnSakResponse[0].saksnummer);
-
-        console.log('sak', sak);
-
-        if (sak?.behandlinger) {
-          sak.behandlinger.map(async (sak, index) => {
-            if (sak.referanse) {
-              const behandlinger = await hentBehandling(sak.referanse);
-              console.log(`behandling nr. ${index}`, behandlinger);
-            }
-          });
-        }
-      }
-    }
-    fetchEverything();
-  });
-
   return (
     <>
       <h1>Saksoversikt page</h1>
