6dc7700a077af2d2f4ac6228a80b2ee8eac427ed
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-08-31 14:30:17 +0200

    💄Legger til grid på sakpage

--- app/sak/[saksId]/page.tsx
+++ app/sak/[saksId]/page.tsx
@@ -1,5 +1,13 @@
+import styles from './page.module.css';
+
 const Page = async ({ params }: { params: { saksId: string } }) => {
-  return <div>Sakspage {params.saksId}</div>;
+  return (
+    <div className={styles.kolonner}>
+      <div className={`${styles.kolonne} ${styles.venstrekolonne}`}>SakId: {params.saksId}</div>
+      <div className={styles.kolonne} />
+      <div className={`${styles.kolonne} ${styles.høyrekolonne}`} />
+    </div>
+  );
 };
 
 export default Page;
