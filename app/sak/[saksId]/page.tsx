ec720a3024fb721881d4cd9e3ec8d3424355862e
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-09-05 10:20:41 +0200

    :recycle: Bytter til Hgrid

--- app/sak/[saksId]/page.tsx
+++ app/sak/[saksId]/page.tsx
@@ -1,12 +1,13 @@
 import styles from './page.module.css';
+import { HGrid } from 'components/DsClient';
 
 const Page = async ({ params }: { params: { saksId: string } }) => {
   return (
-    <div className={styles.kolonner}>
+    <HGrid columns={'1fr 3fr 1fr'} className={styles.kolonner}>
       <div className={`${styles.kolonne} ${styles.venstrekolonne}`}>SakId: {params.saksId}</div>
       <div className={styles.kolonne} />
       <div className={`${styles.kolonne} ${styles.høyrekolonne}`} />
-    </div>
+    </HGrid>
   );
 };
 
