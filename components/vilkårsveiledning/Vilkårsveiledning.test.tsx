f2402018770d187bf8b10d57ff555296c0f6d7bb
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-10-23 13:27:48 +0200

    :sparkles: Legger til komponent for Vilkårsveiledning
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>
    Co-authored-by: Tor Idland <tor.idland@nav.no>

---
+++ components/vilkårsveiledning/Vilkårsveiledning.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { Vilkårsveildening } from 'components/vilkårsveiledning/Vilkårsveiledning';
+
+describe('Vilkårsveildening', () => {
+  it('skal ha en overskrift', () => {
+    render(<Vilkårsveildening />);
+    expect(screen.getByText('Slik vurderes vilkåret')).toBeVisible();
+  });
+
+  it('skal vise korrekt tekst for avklar student', () => {
+    render(<Vilkårsveildening />);
+    expect(screen.getByText('Her kommer det noe tekst som beskriver hvordan vilkåret skal vurderes')).toBeVisible();
+  });
+});
---
+++ components/vilkårsveiledning/Vilkårsveiledning.tsx
@@ -0,0 +1,9 @@
+import { ReadMore } from '@navikt/ds-react';
+
+export const Vilkårsveildening = () => {
+  return (
+    <ReadMore header={'Slik vurderes vilkåret'}>
+      Her kommer det noe tekst som beskriver hvordan vilkåret skal vurderes
+    </ReadMore>
+  );
+};
