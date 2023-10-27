34b663baf89f28a2ec8e676289a8ea565cf1eafe
Øivind Stensrud <oivind.stensrud@nav.no>
2023-10-27 14:08:44 +0200

    :recycle: Oppdaterer import-sti

--- app/saksoversikt/page.tsx
+++ app/saksoversikt/page.tsx
@@ -1,8 +1,8 @@
 import { getToken } from 'lib/auth/authentication';
-import { hentAlleSaker } from '../../lib/services/saksbehandlingservice/saksbehandlingService';
+import { hentAlleSaker } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { headers } from 'next/headers';
 
-import { OpprettSak } from '../../components/opprettsak/OpprettSak';
+import { OpprettSak } from 'components/opprettsak/OpprettSak';
 import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
 
 const Page = async () => {
