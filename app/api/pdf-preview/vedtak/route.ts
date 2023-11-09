0239e0747ddd5dc01a8350cea84a9d8f71ff7b01
Tor Idland <tor.idland@nav.no>
2023-11-09 15:59:34 +0100

    :construction: endepunkt /api/pdf-preview/vedtak som går mot aap-saksbehandling-pdfgen lokalt med knapp på editor siden

---
+++ app/api/pdf-preview/vedtak/route.ts
@@ -0,0 +1,28 @@
+const dummyBody = {
+  mottaker: {
+    navn: 'Ola Nordmann',
+    ident: '12345678910',
+  },
+  saksnummer: 'AABBCC123',
+  dato: '11. august 2023',
+  underblokker: [
+    {
+      type: 'heading-one',
+      text: 'Dette er en overskrift',
+    },
+    {
+      type: 'paragraph',
+      text: 'Dette er et avsnitt',
+    },
+  ],
+};
+export async function POST() {
+  return fetch('http://127.0.0.1:8080/api/v1/genpdf/aap-saksbehandling-pdfgen/vedtak', {
+    method: 'POST',
+    body: JSON.stringify(dummyBody),
+    headers: {
+      Accept: 'application/pdf',
+      'Content-Type': 'application/json',
+    },
+  });
+}
--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,9 +1,35 @@
+'use client';
+
 import { BrevEditor } from 'components/breveditor/BrevEditor';
 
 const Page = async () => {
+  async function hentPdfForhåndsvisning() {
+    const pdf = await fetch('/api/pdf-preview/vedtak', { method: 'POST', body: 'hei' }).then((r) => {
+      return r.arrayBuffer();
+    });
+    const pdfBlob = new Blob([new Uint8Array(pdf, 0)], { type: 'application/pdf' });
+    const pdfViewer = document.createElement('iframe');
+
+    pdfViewer.src = URL.createObjectURL(pdfBlob);
+    pdfViewer.onload = () => URL.revokeObjectURL(pdfViewer.src);
+
+    // Styling related to demo
+    pdfViewer.style.height = '100vh';
+    pdfViewer.style.width = '100vw';
+    pdfViewer.style.border = '0';
+    pdfViewer.style.borderRadius = '6px';
+    document.body.style.overflow = 'hidden';
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';
+
+    document.body.appendChild(pdfViewer);
+  }
   return (
-    <div style={{ padding: '3rem' }}>
-      <BrevEditor />
+    <div>
+      <div style={{ padding: '3rem' }}>
+        <BrevEditor />
+      </div>
+      <button onClick={() => hentPdfForhåndsvisning()}>Hent brev</button>
     </div>
   );
 };
