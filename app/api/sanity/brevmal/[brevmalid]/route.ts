109ef744560e39b7e824a97ba94cfb7c3e2ba0a0
Øivind Stensrud <oivind.stensrud@nav.no>
2024-01-03 10:34:53 +0100

    :recycle: Retter opp i feil som førte til at bygget ikke gikk igjennom

--- app/api/sanity/brevmal/[brevmalid]/route.ts
+++ app/api/sanity/brevmal/[brevmalid]/route.ts
@@ -1,18 +1,15 @@
-import { NextRequest } from 'next/server';
-
-export async function GET(req: NextRequest, { params }: { params: { brevmalid: string } }) {
-
-  const response = await fetch("http://aap-brev/${params.brevmalid}", {
-    method: GET,
+export async function GET({ params }: { params: { brevmalid: string } }) {
+  const response = await fetch(`http://aap-brev/${params.brevmalid}`, {
+    method: 'GET',
     headers: {
       Accept: 'application/json',
     },
   });
-  
+
   if (response.ok) {
     const body = await response.json();
     return new Response(JSON.stringify(body), { status: 200 });
-  } 
+  }
 
-  return new Response("klarte ikke finne brevmal", { status: 400 })
+  return new Response('klarte ikke finne brevmal', { status: 400 });
 }
