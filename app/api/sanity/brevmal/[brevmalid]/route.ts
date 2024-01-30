3fbbe945327ba5346d73e7cc353eb485895e1f96
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-01-30 09:59:41 +0100

    :bug: Fikser sanity brev

--- app/api/sanity/brevmal/[brevmalid]/route.ts
+++ app/api/sanity/brevmal/[brevmalid]/route.ts
@@ -1,15 +1,43 @@
-export async function GET({ params }: { params: { brevmalid: string } }) {
-  const response = await fetch(`http://aap-brev/${params.brevmalid}`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
+import { NextRequest } from 'next/server';
+import { sanityservice } from 'lib/services/sanityservice/sanityservice';
+import { groq } from 'next-sanity';
+import { Brevmal } from 'lib/utils/sanity';
+
+export async function GET(req: NextRequest, { params }: { params: { brevmalid: string } }) {
+  const data = await sanityservice.fetch<Brevmal>(groq`*[_id == "${params.brevmalid}"][0]{
+  brevtittel,
+  innhold[] -> {
+    _type,
+    _id,
+    _type == 'systeminnhold' => {
+      systemNokkel,
+      overskrift,
+      "niva": niva->.level
     },
-  });
-
-  if (response.ok) {
-    const body = await response.json();
-    return new Response(JSON.stringify(body), { status: 200 });
+    _type == 'standardtekst' => {
+      overskrift,
+      "niva": niva->.level,
+      hjelpetekst,
+      kanRedigeres,
+        innhold[]{
+        _type == 'content' => {
+          ...,
+          children[] {
+            ...,
+            _type == 'systemVariabel' => {
+              ...,
+              "systemVariabel": @->.tekniskNavn
+            },
+            _type == 'inlineElement' => {
+              ...,
+              "text": @->.tekst
+            },
+          }
+        }
+      }
+    }
   }
+}`);
 
-  return new Response('klarte ikke finne brevmal', { status: 400 });
+  return new Response(JSON.stringify(data), { status: 200 });
 }
