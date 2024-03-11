62f0d80b739330fb867d55e109a310621cb0ff93
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-03-11 16:02:58 +0100

    ⏪️ Legger tilbake sykdomsvurdering med yrkesskade på sykdomsvurdering

--- components/behandlinger/sykdom/sykdomsvurdering/SykdomsvurderingMedDataFetching.tsx
+++ components/behandlinger/sykdom/sykdomsvurdering/SykdomsvurderingMedDataFetching.tsx
@@ -1,5 +1,6 @@
 import { Sykdomsvurdering } from 'components/behandlinger/sykdom/sykdomsvurdering/Sykdomsvurdering';
 import { hentSykdomsGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { SykdomsvurderingMedYrkesskade } from 'components/behandlinger/sykdom/sykdomsvurdering/SykdomsvurderingMedYrkesskade';
 
 interface Props {
   behandlingsReferanse: string;
@@ -8,5 +9,9 @@ interface Props {
 export const SykdomsvurderingMedDataFetching = async ({ behandlingsReferanse }: Props) => {
   const grunnlag = await hentSykdomsGrunnlag(behandlingsReferanse);
 
-  return <Sykdomsvurdering behandlingsReferanse={behandlingsReferanse} grunnlag={grunnlag} />;
+  return grunnlag.skalVurdereYrkesskade ? (
+    <SykdomsvurderingMedYrkesskade behandlingsReferanse={behandlingsReferanse} grunnlag={grunnlag} />
+  ) : (
+    <Sykdomsvurdering behandlingsReferanse={behandlingsReferanse} grunnlag={grunnlag} />
+  );
 };
