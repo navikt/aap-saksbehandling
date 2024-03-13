b5a7a3186c62e0d50103f7d3dbc1bf8f24bf14ef
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-03-13 17:12:21 +0100

    :sparkles: Legger til dokumentasjontabell i alle stegene

--- components/behandlinger/sykdom/sykdomsvurdering/SykdomsvurderingMedYrkesskade.test.tsx
+++ components/behandlinger/sykdom/sykdomsvurdering/SykdomsvurderingMedYrkesskade.test.tsx
@@ -114,27 +114,6 @@ describe('Sykdomsvurdering med yrkesskade', () => {
     expect(await screen.findByRole('group', { name: /er arbeidsevnen nedsatt med minst 50%\?/i })).toBeVisible();
   });
 
-  it.skip('Skal vise felt for skadetidspunkt dersom vilkåret i § 11-22 er oppfylt og arbeidsevnen er nedsatt med minst grenseverdi', async () => {
-    const group = screen.getByRole('group', {
-      name: /er yrkesskaden helt eller delvis medvirkende årsak til den nedsatte arbeidsevnen\? \(§ 11-22 1\.ledd\)\./i,
-    });
-    const jaValg = within(group).getByRole('radio', { name: /ja/i });
-
-    await user.click(jaValg);
-
-    const erArbeidsevnenNedsattMedMinst30prosentJaValg = within(
-      screen.getByRole('group', { name: /er arbeidsevnen nedsatt med minst 30%\?/i })
-    ).getByRole('radio', { name: /ja/i });
-
-    expect(
-      await screen.queryByRole('textbox', { name: /dato for skadetidspunkt for yrkesskaden/i })
-    ).not.toBeInTheDocument();
-
-    await user.click(erArbeidsevnenNedsattMedMinst30prosentJaValg);
-
-    expect(await screen.findByRole('group', { name: /dato for skadetidspunkt for yrkesskaden/i })).toBeVisible();
-  });
-
   it('Skal ikke vise dato felt for skadetidspunkt dersom vilkåret i § 11-22 ikke er oppfylt', async () => {
     const group = screen.getByRole('group', {
       name: /er yrkesskaden helt eller delvis medvirkende årsak til den nedsatte arbeidsevnen\? \(§ 11-22 1\.ledd\)\./i,
