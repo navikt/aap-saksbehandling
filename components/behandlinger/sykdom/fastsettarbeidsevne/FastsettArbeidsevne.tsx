0604852992f489b4a4bc90ab896c865a0a7565b6
tidnav <90849097+tidnav@users.noreply.github.com>
2024-03-14 15:25:14 +0100

    :sparkles: Fastsett arbeidsevne (#248)
    * :sparkles: Første versjon av perioder i fastsett arbeidsevne
    
    * :sparkles: veldig enkel liste med slett-knapp. superhacky id på periode
    
    * :sparkles: bruker table fra aksel til arbeidsevneperiode. arbeidsevne i ett felt og benevning til å angi om det er timer eller prosent
    
    * :sparkles: Legger til dokumentasjontabell i alle stegene
    
    * :sparkles: Legger til dokumentasjontabell i alle stegene
    
    * :sparkles: Legger til Medlemskap og lovvalg
    
    * :recycle: Endrer overskrift på foreslå vedtak
    
    * :sparkles: Legger til uttak, tilkjent ytelse, simulering og barnetillegg
    
    * :sparkles: Legger til dokumenttabell i Sykepengeerstatning
    
    * trigger build
    
    * Fjerner yarn-berry branch fra build-and-deploy.yml
    
    * bruker main workflow av frontend bygg
    
    * :recycle: alle formfelter er nå under periodetabellen og inne i FastsettArbeidsevnePeriodeForm
    
    ---------
    
    Co-authored-by: Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>

--- components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevne.tsx
+++ components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevne.tsx
@@ -1,47 +1,21 @@
 'use client';
 
-import { useConfigForm } from 'hooks/FormHook';
 import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
-import { Form } from 'components/form/Form';
-import { handleSubmitWithCallback } from 'lib/utils/form';
-import { FormField } from 'components/input/formfield/FormField';
-import { DokumentTabell } from 'components/dokumenttabell/DokumentTabell';
-import { ReadMore, ToggleGroup } from '@navikt/ds-react';
+import { Button, Heading } from '@navikt/ds-react';
 import { useState } from 'react';
+import {
+  FastSettArbeidsevnePeriode,
+  FastsettArbeidsevnePeriodeForm,
+} from 'components/fastsettarbeidsevneperiodeform/FastsettArbeidsevnePeriodeForm';
+import { FastsettArbeidsevnePeriodeTableItem } from 'components/fastsettarbeidsevneperiodetable/FastsettArbeidsevnePeriodeTableItem';
+import { FastsettArbeidsevnePeriodeTable } from 'components/fastsettarbeidsevneperiodetable/FastsettArbeidsevnePeriodeTable';
 
-interface FormFields {
-  dokumenterBruktIVurderingen: string[];
-  begrunnelse: string;
-  arbeidsevne: string;
-  arbeidsevnenGjelderFra: Date;
-}
 interface Props {
   behandlingsReferanse: string;
 }
 
 export const FastsettArbeidsevne = ({ behandlingsReferanse }: Props) => {
-  const [activeToggle, setActiveToggle] = useState('');
-  const { form, formFields } = useConfigForm<FormFields>({
-    begrunnelse: {
-      type: 'textarea',
-      label: 'Vurder om brukeren har arbeidsevne',
-      description: 'En beksrivelse av hva som skal gjøres her',
-      rules: { required: 'Du må begrunne avgjørelsen din.' },
-    },
-    dokumenterBruktIVurderingen: {
-      type: 'checkbox_nested',
-      label: 'Dokumenter funnet som er relevant for vurdering',
-      description: 'Tilknytt minst ett dokument til vurdering',
-    },
-    arbeidsevne: {
-      type: 'text',
-      label: 'Hvor stor er arbeidsevnen sett opp mot en arbeidsuke på 37,5 timer',
-    },
-    arbeidsevnenGjelderFra: {
-      type: 'date',
-      label: 'Arbeidsevnen gjelder fra og med',
-    },
-  });
+  const [perioder, setPerioder] = useState<FastSettArbeidsevnePeriode[]>([]);
 
   return (
     <VilkårsKort
@@ -50,33 +24,19 @@ export const FastsettArbeidsevne = ({ behandlingsReferanse }: Props) => {
       erNav={true}
       defaultOpen={false}
     >
-      <Form
-        onSubmit={handleSubmitWithCallback(form, (data) => {
-          console.log('Waddap!', data, behandlingsReferanse);
-        })}
-        steg={'VURDER_UUTNYTTET_ARBEIDSEVNE'}
-      >
-        <FormField form={form} formField={formFields.dokumenterBruktIVurderingen}>
-          <DokumentTabell />
-        </FormField>
-        <ReadMore header={'Slik vurderes vilkåret'}>
-          ref § ... Her kan vi gi en fin veiledning til hvordan man skal begrunne vilkårsvurderingen hvis de er usikre
-        </ReadMore>
+      <Heading size={'small'} level={'4'}>
+        Regisrerte perioder med arbeidsevne
+      </Heading>
+      <FastsettArbeidsevnePeriodeTable>
+        {perioder.map((periode) => (
+          <FastsettArbeidsevnePeriodeTableItem key={periode.id} {...periode} />
+        ))}
+      </FastsettArbeidsevnePeriodeTable>
 
-        <FormField form={form} formField={formFields.begrunnelse} />
-
-        <ToggleGroup defaultValue="timer" onChange={(value) => setActiveToggle(value)} size={'small'}>
-          <ToggleGroup.Item value="timer">Timer</ToggleGroup.Item>
-          <ToggleGroup.Item value="prosent">Prosent</ToggleGroup.Item>
-        </ToggleGroup>
-        <div>
-          <div style={{ width: '20%' }}>
-            <FormField form={form} formField={formFields.arbeidsevne} />
-          </div>
-          {activeToggle === 'prosent' && <span>%</span>}
-        </div>
-        <FormField form={form} formField={formFields.arbeidsevnenGjelderFra} />
-      </Form>
+      <FastsettArbeidsevnePeriodeForm onSave={(periode) => setPerioder([...perioder, periode])} />
+      <form onSubmit={() => console.log('bekreft fastsettarbeidsevne', behandlingsReferanse, perioder)}>
+        <Button form={'fastsettArbeidsevne'}>Bekreft</Button>
+      </form>
     </VilkårsKort>
   );
 };
---
+++ components/fastsettarbeidsevneperiodeform/FastsettArbeidsevnePeriodeForm.tsx
@@ -0,0 +1,94 @@
+import { useConfigForm } from 'hooks/FormHook';
+import { FormField } from 'components/input/formfield/FormField';
+import { useState } from 'react';
+import { Button, Heading, ReadMore, ToggleGroup } from '@navikt/ds-react';
+import styles from './FastsettArbeidsevnePeriodeForm.module.css';
+import { DokumentTabell } from 'components/dokumenttabell/DokumentTabell';
+
+interface Props {
+  periode?: FastSettArbeidsevnePeriode;
+  onSave: (periode: FastSettArbeidsevnePeriode) => void;
+}
+
+export interface FastSettArbeidsevnePeriode extends FormFields {
+  benevning: string;
+  id: string;
+}
+
+interface FormFields {
+  arbeidsevne: string;
+  fraDato: Date;
+  dokumenterBruktIVurderingen: string[];
+  begrunnelse: string;
+}
+
+export const FastsettArbeidsevnePeriodeForm = ({ periode, onSave }: Props) => {
+  const [activeToggle, setActiveToggle] = useState('timer');
+  const { form, formFields } = useConfigForm<FormFields>({
+    begrunnelse: {
+      type: 'textarea',
+      label: 'Vurder om brukeren har arbeidsevne',
+      description: 'En beksrivelse av hva som skal gjøres her',
+      rules: { required: 'Du må begrunne avgjørelsen din.' },
+    },
+    dokumenterBruktIVurderingen: {
+      type: 'checkbox_nested',
+      label: 'Dokumenter funnet som er relevant for vurdering',
+      description: 'Tilknytt minst ett dokument til vurdering',
+    },
+    arbeidsevne: {
+      type: 'text',
+      label: '',
+      defaultValue: periode?.arbeidsevne,
+    },
+    fraDato: {
+      type: 'date',
+      label: 'Arbeidsevnen gjelder fra og med',
+      defaultValue: periode?.fraDato,
+    },
+  });
+  const labelTimer = 'Hvor stor er arbeidsevnen sett opp mot en arbeidsuke på 37,5 timer';
+  const labelProsent = 'Hvor stor er arbeidsevnen i prosent?';
+
+  return (
+    <form
+      onSubmit={form.handleSubmit((data) => {
+        if (periode) {
+          onSave({ ...data, id: periode.id, benevning: activeToggle });
+        } else {
+          onSave({ ...data, id: `placeholder-id-${Math.floor(Math.random() * 1000)}`, benevning: activeToggle });
+        }
+      })}
+      className={styles.fastsettArbeidsEvneForm}
+    >
+      <Heading size={'small'} level={'4'} spacing>
+        Legg til en ny periode med arbeidsevne
+      </Heading>
+      <div className={styles.fastsettArbeidsEvneFormFelter}>
+        <FormField form={form} formField={formFields.dokumenterBruktIVurderingen}>
+          <DokumentTabell />
+        </FormField>
+        <ReadMore header={'Slik vurderes vilkåret'}>
+          ref § ... Her kan vi gi en fin veiledning til hvordan man skal begrunne vilkårsvurderingen hvis de er usikre
+        </ReadMore>
+
+        <FormField form={form} formField={formFields.begrunnelse} />
+        <div>
+          <ToggleGroup defaultValue="timer" onChange={(value) => setActiveToggle(value)} size={'small'}>
+            <ToggleGroup.Item value="timer">Timer</ToggleGroup.Item>
+            <ToggleGroup.Item value="prosent">Prosent</ToggleGroup.Item>
+          </ToggleGroup>
+          <span>
+            <FormField
+              form={form}
+              formField={{ ...formFields.arbeidsevne, label: activeToggle === 'timer' ? labelTimer : labelProsent }}
+            />
+            {`${activeToggle === 'timer' ? 'timer' : '%'}`}
+          </span>
+        </div>
+        <FormField form={form} formField={formFields.fraDato} />
+        <Button>Lagre</Button>
+      </div>
+    </form>
+  );
+};
