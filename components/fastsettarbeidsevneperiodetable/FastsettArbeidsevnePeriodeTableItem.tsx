30d6bb89bc3600773ef691bbb98d0071db8721a4
tidnav <90849097+tidnav@users.noreply.github.com>
2024-03-14 15:25:14 +0100

    :sparkles: Fastsett arbeidsevne (#248)
    * :sparkles: Første versjon av perioder i fastsett arbeidsevne
    
    * :sparkles: veldig enkel liste med slett-knapp. superhacky id på periode
    
    * :sparkles: bruker table fra aksel til arbeidsevneperiode. arbeidsevne i ett felt og benevning til å angi om det er timer eller prosent
    
    * :sparkles: Legger til dokumentasjontabell i alle stegene
    
    * :sparkles: Legger til dokumentasjontabell i alle stegene
    
    * :sparkles: Legger til Medlemskap og lovvalg
    
    * :recycle: Endrer overskrift på foreslå vedtak
    
    * :sparkles: Legger til uttak, tilkjent ytelse, simulering og barnetillegg
    
    * :sparkles: Legger til dokumenttabell i Sykepengeerstatning
    
    * trigger build
    
    * Fjerner yarn-berry branch fra build-and-deploy.yml
    
    * bruker main workflow av frontend bygg
    
    * :recycle: alle formfelter er nå under periodetabellen og inne i FastsettArbeidsevnePeriodeForm
    
    ---------
    
    
    Co-authored-by: Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>

---
+++ components/fastsettarbeidsevneperiodetable/FastsettArbeidsevnePeriodeTableItem.tsx
@@ -0,0 +1,18 @@
+import { FastSettArbeidsevnePeriode } from 'components/fastsettarbeidsevneperiodeform/FastsettArbeidsevnePeriodeForm';
+import { formaterDato } from 'lib/utils/date';
+import { Table } from '@navikt/ds-react';
+export const FastsettArbeidsevnePeriodeTableItem = ({
+  id,
+  fraDato,
+  arbeidsevne,
+  benevning,
+  dokumenterBruktIVurderingen,
+}: FastSettArbeidsevnePeriode) => {
+  return (
+    <Table.Row key={id}>
+      <Table.DataCell>{formaterDato(fraDato)}</Table.DataCell>
+      <Table.DataCell>{`${arbeidsevne} ${benevning}`}</Table.DataCell>
+      <Table.DataCell>{`${dokumenterBruktIVurderingen.join()}`}</Table.DataCell>
+    </Table.Row>
+  );
+};
