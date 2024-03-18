f018dab274471d57e969ac881cb534a18da3fa6a
Tor Idland <tor.idland@nav.no>
2024-03-18 12:53:35 +0100

    :goal_net: håndtere feil i /test/opprett

--- app/api/test/opprett/route.ts
+++ app/api/test/opprett/route.ts
@@ -1,11 +1,17 @@
 import { opprettTestSak } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
+import { logError } from '@navikt/aap-felles-utils';
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  await opprettTestSak(body);
+  try {
+    await opprettTestSak(body);
+  } catch (err: unknown) {
+    logError('/test/opprett/', err);
+    return new Response(JSON.stringify({ message: err?.toString() }), { status: 500 });
+  }
   revalidatePath('/saksoversikt', 'page');
   return new Response(JSON.stringify({ message: 'Sak opprettet' }), { status: 200 });
 }
