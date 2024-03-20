bf67ae569620a427bfbe2f829ed9c2f53ae71606
Tor Idland <tor.idland@nav.no>
2024-03-20 13:57:12 +0100

    :white_check_mark: tester FastsettArbeidsevne.test.tsx

--- components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevne.test.tsx
+++ components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevne.test.tsx
@@ -31,6 +31,32 @@ describe('FastsettArbeidsevne', () => {
     const textbox = screen.getByRole('textbox', { name: /Vurder om brukeren har arbeidsevne/i });
     expect(textbox).toBeVisible();
   });
+  it('Skal ha feltet dokumenterBruktIVurderingen', async () => {
+    await åpneVilkårskort();
+    const checkboxGroup = await screen.findByRole('group', { name: 'Dokumenter funnet som er relevant for vurdering' });
+    expect(checkboxGroup).toBeVisible();
+  });
+  it('Skal ha feltet arbeidsevne (timer)', async () => {
+    await åpneVilkårskort();
+    const textbox = screen.getByRole('textbox', {
+      name: /Hvor stor er arbeidsevnen sett opp mot en arbeidsuke på 37,5 timer/i,
+    });
+    expect(textbox).toBeVisible();
+  });
+  it('Skal ha feltet arbeidsevne (prosent)', async () => {
+    await åpneVilkårskort();
+    const toggle = screen.getByRole('radio', { name: /Prosent/i });
+    await user.click(toggle);
+    const textbox = screen.getByRole('textbox', {
+      name: /Hvor stor er arbeidsevnen i prosent?/i,
+    });
+    expect(textbox).toBeVisible();
+  });
+  it('Skal ha feltet fraDato', async () => {
+    await åpneVilkårskort();
+    const textbox = screen.getByRole('textbox', { name: /Arbeidsevnen gjelder fra og med/i });
+    expect(textbox).toBeVisible();
+  });
 
   async function åpneVilkårskort() {
     const region = screen.getByRole('region', { name: /Reduksjon ved delvis nedsatt arbeidsevne - § 11-23 2.ledd/i });
