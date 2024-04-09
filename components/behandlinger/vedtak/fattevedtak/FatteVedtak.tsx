1b5e6fb1c0e4ccc46f0c6dea83e353c638c3e5da
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-04-09 15:02:51 +0200

    :sparkles: Henter alle grunnlagene og sender de inn i FatteVedtak
    Co-authored-by: Tor Idland <tor.idland@nav.no>
    Co-authored-by: Anders Østby <anders.ostby@nav.no>

--- components/behandlinger/vedtak/fattevedtak/FatteVedtak.tsx
+++ components/behandlinger/vedtak/fattevedtak/FatteVedtak.tsx
@@ -1,15 +1,39 @@
 'use client';
 
 import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
+import {
+  BistandsGrunnlag,
+  FritakMeldepliktGrunnlag,
+  StudentGrunnlag,
+  SykdomsGrunnlag,
+  SykepengeerstatningGrunnlag,
+} from 'lib/types/types';
 
 interface Props {
   behandlingsReferanse: string;
+  sykdomsGrunnlag: SykdomsGrunnlag;
+  studentGrunnlag: StudentGrunnlag;
+  bistandsGrunnlag: BistandsGrunnlag;
+  sykepengeerstatningGrunnlag: SykepengeerstatningGrunnlag;
+  fritakMeldepliktGrunnlag: FritakMeldepliktGrunnlag;
 }
 
-export const FatteVedtak = ({ behandlingsReferanse }: Props) => {
+export const FatteVedtak = ({
+  behandlingsReferanse,
+  fritakMeldepliktGrunnlag,
+  sykepengeerstatningGrunnlag,
+  studentGrunnlag,
+  sykdomsGrunnlag,
+  bistandsGrunnlag,
+}: Props) => {
+  console.log(fritakMeldepliktGrunnlag);
+  console.log(sykepengeerstatningGrunnlag);
+  console.log(studentGrunnlag);
+  console.log(sykdomsGrunnlag);
+  console.log(bistandsGrunnlag);
   return (
     <VilkårsKort heading={'Fatte vedtak'} steg={'FATTE_VEDTAK'}>
-      hello pello fatte vedtak {behandlingsReferanse}
+      {behandlingsReferanse}
     </VilkårsKort>
   );
 };
--- components/behandlinger/vedtak/fattevedtak/FatteVedtakMedDataFetching.tsx
+++ components/behandlinger/vedtak/fattevedtak/FatteVedtakMedDataFetching.tsx
@@ -1,9 +1,34 @@
 import { FatteVedtak } from 'components/behandlinger/vedtak/fattevedtak/FatteVedtak';
+import {
+  hentBistandsbehovGrunnlag,
+  hentStudentGrunnlag,
+  hentSykdomsGrunnlag,
+  hentSykepengerErstatningGrunnlag,
+  hentUnntakMeldepliktGrunnlag,
+} from 'lib/services/saksbehandlingservice/saksbehandlingService';
 
 interface Props {
   behandlingsReferanse: string;
 }
 
-export const FatteVedtakMedDataFetching = ({ behandlingsReferanse }: Props) => {
-  return <FatteVedtak behandlingsReferanse={behandlingsReferanse} />;
+export const FatteVedtakMedDataFetching = async ({ behandlingsReferanse }: Props) => {
+  const [sykdomsgrunnlag, studentGrunnlag, bistandsbehovGrunnlag, sykepengeerstatningGrunnlag, meldepliktGrunnlag] =
+    await Promise.all([
+      hentSykdomsGrunnlag(behandlingsReferanse),
+      hentStudentGrunnlag(behandlingsReferanse),
+      hentBistandsbehovGrunnlag(behandlingsReferanse),
+      hentSykepengerErstatningGrunnlag(behandlingsReferanse),
+      hentUnntakMeldepliktGrunnlag(behandlingsReferanse),
+    ]);
+
+  return (
+    <FatteVedtak
+      behandlingsReferanse={behandlingsReferanse}
+      sykepengeerstatningGrunnlag={sykepengeerstatningGrunnlag}
+      studentGrunnlag={studentGrunnlag}
+      sykdomsGrunnlag={sykdomsgrunnlag}
+      bistandsGrunnlag={bistandsbehovGrunnlag}
+      fritakMeldepliktGrunnlag={meldepliktGrunnlag}
+    />
+  );
 };
