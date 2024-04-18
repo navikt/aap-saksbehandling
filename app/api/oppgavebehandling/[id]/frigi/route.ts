6b35c758b2e565b82d11ca80e984ab9bb291e3e6
Øivind Stensrud <oivind.stensrud@nav.no>
2024-04-18 20:39:39 +0200

    :bug: Glemte api-endepunkt

---
+++ app/api/oppgavebehandling/[id]/frigi/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from 'next/server';
+import { fetchProxy } from 'lib/services/fetchProxy';
+import { isLocal } from 'lib/utils/environment';
+
+const oppgavestyringApiBaseUrl = process.env.OPPGAVESTYRING_API_BASE_URL;
+const oppgavestyringApiScope = process.env.OPPGAVESTYRING_API_SCOPE ?? '';
+
+interface Params {
+  params: { id: string };
+}
+
+export async function PATCH(req: NextRequest, { params }: Params) {
+  if (isLocal()) {
+    return new Response(JSON.stringify({ message: 'Oppgave frigitt' }), { status: 200 });
+  }
+
+  const body = await req.json();
+  try {
+    await fetchProxy<void>(
+      `${oppgavestyringApiBaseUrl}/oppgaver/${params.id}/frigi`,
+      oppgavestyringApiScope,
+      'PATCH',
+      body
+    );
+    return new Response(JSON.stringify({ message: 'Oppgave frigitt' }), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: JSON.stringify(error) }), { status: 500 });
+  }
+}
