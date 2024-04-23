a6b2a87a7e2d3f06fd45fe38e6c6e692d2b70277
Øivind Stensrud <oivind.stensrud@nav.no>
2024-04-23 12:20:32 +0200

    :bug: api-endepunkt håndterte ikke response

--- app/api/oppgavebehandling/route.ts
+++ app/api/oppgavebehandling/route.ts
@@ -1,6 +1,7 @@
 import { fetchProxy } from 'lib/services/fetchProxy';
 import { isLocal } from 'lib/utils/environment';
 import { mockOppgaver } from 'mocks/mockOppgaver';
+import { Oppgaver } from 'lib/types/oppgavebehandling';
 
 const oppgavestyringApiBaseUrl = process.env.OPPGAVESTYRING_API_BASE_URL;
 const oppgavestyringApiScope = process.env.OPPGAVESTYRING_API_SCOPE ?? '';
@@ -11,8 +12,8 @@ export async function GET() {
   }
 
   try {
-    await fetchProxy<void>(`${oppgavestyringApiBaseUrl}/oppgaver`, oppgavestyringApiScope, 'GET');
-    return new Response(JSON.stringify({ message: 'Oppgave fordelt', status: 200 }), { status: 200 });
+    const res = await fetchProxy<Oppgaver>(`${oppgavestyringApiBaseUrl}/oppgaver`, oppgavestyringApiScope, 'GET');
+    return new Response(JSON.stringify(res), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ message: JSON.stringify(error), status: 500 }), { status: 500 });
   }
