273645f753ead9e534950660b43890525c55bed8
Øivind Stensrud <oivind.stensrud@nav.no>
2024-04-23 14:55:42 +0200

    :fire: Sletter høyrekolonne
    Ikke en del av scope i nær fremtid. Bruker
    heller plassen på oppgavelisten.

--- app/oppgaveliste/page.tsx
+++ app/oppgaveliste/page.tsx
@@ -2,7 +2,6 @@ import { HGrid } from '@navikt/ds-react';
 
 import { Kort } from 'components/oppgavebehandling/kort/Kort';
 import { Køvelger } from 'components/oppgavebehandling/køvelger/Køvelger';
-import { HøyreKolonne } from 'components/oppgavebehandling/høyrekolonne/Høyrekolonne';
 
 import styles from './page.module.css';
 import { Oppgavekø } from 'components/oppgavebehandling/oppgavekø/Oppgavekø';
@@ -10,14 +9,13 @@ import { Oppgavekø } from 'components/oppgavebehandling/oppgavekø/Oppgavekø';
 const Page = () => {
   return (
     <main className={styles.content}>
-      <HGrid gap={'8'} columns={'4fr 1fr'}>
+      <HGrid gap={'8'} columns={'1fr'}>
         <HGrid>
           <Kort>
             <Køvelger />
             <Oppgavekø />
           </Kort>
         </HGrid>
-        <HøyreKolonne />
       </HGrid>
     </main>
   );
