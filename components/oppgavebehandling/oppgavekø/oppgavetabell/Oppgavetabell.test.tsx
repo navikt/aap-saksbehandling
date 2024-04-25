600a4840b820e5b1207c99840694d4d969be6c1f
Øivind Stensrud <oivind.stensrud@nav.no>
2024-04-25 14:45:42 +0200

    :white_check_mark: Mocker mutate for oppgavetabell

--- components/oppgavebehandling/oppgavekø/oppgavetabell/Oppgavetabell.test.tsx
+++ components/oppgavebehandling/oppgavekø/oppgavetabell/Oppgavetabell.test.tsx
@@ -2,9 +2,11 @@ import { Oppgavetabell } from 'components/oppgavebehandling/oppgavekø/oppgaveta
 import { fordeltOppgave, mockOppgaver, ufordeltOppgave } from 'mocks/mockOppgaver';
 import { render, screen } from '@testing-library/react';
 
+const mutateMock = jest.fn();
+
 describe('Oppgavetabell', () => {
   test('har en tabellheader', () => {
-    render(<Oppgavetabell oppgaver={mockOppgaver.oppgaver} />);
+    render(<Oppgavetabell oppgaver={mockOppgaver.oppgaver} mutate={mutateMock} />);
     expect(screen.getByRole('columnheader', { name: 'Innbygger' })).toBeVisible();
     expect(screen.getByRole('columnheader', { name: 'Oppgavetype' })).toBeVisible();
     expect(screen.getByRole('columnheader', { name: 'Gjelder' })).toBeVisible();
@@ -14,32 +16,32 @@ describe('Oppgavetabell', () => {
   });
 
   test('viser melding om ingen oppgaver når listen er tom', () => {
-    render(<Oppgavetabell oppgaver={[]} />);
+    render(<Oppgavetabell oppgaver={[]} mutate={mutateMock} />);
     expect(screen.getByText('Fant ingen oppgaver')).toBeVisible();
   });
 
   test('viser "ufordelt" for en oppgave som ikke har en saksbehandler', () => {
-    render(<Oppgavetabell oppgaver={ufordeltOppgave} />);
+    render(<Oppgavetabell oppgaver={ufordeltOppgave} mutate={mutateMock} />);
     expect(screen.getByRole('cell', { name: 'Ufordelt' })).toBeVisible();
   });
 
   test('viser navn på saksbehandler når en oppgave er fordelt', () => {
-    render(<Oppgavetabell oppgaver={fordeltOppgave} />);
+    render(<Oppgavetabell oppgaver={fordeltOppgave} mutate={mutateMock} />);
     expect(screen.getByRole('cell', { name: fordeltOppgave[0].tilordnetRessurs })).toBeVisible();
   });
 
   test('har knapp for å ta en ufordelt oppgave', () => {
-    render(<Oppgavetabell oppgaver={ufordeltOppgave} />);
+    render(<Oppgavetabell oppgaver={ufordeltOppgave} mutate={mutateMock} />);
     expect(screen.getByRole('button', { name: 'Behandle' })).toBeVisible();
   });
 
   test('har knapp for å frigi en fordelt oppgave', () => {
-    render(<Oppgavetabell oppgaver={fordeltOppgave} />);
+    render(<Oppgavetabell oppgaver={fordeltOppgave} mutate={mutateMock} />);
     expect(screen.getByRole('button', { name: 'Frigjør' })).toBeVisible();
   });
 
   test('tegner en rad pr oppgave', () => {
-    render(<Oppgavetabell oppgaver={mockOppgaver.oppgaver} />);
+    render(<Oppgavetabell oppgaver={mockOppgaver.oppgaver} mutate={mutateMock} />);
     // row tar og med header-rad, legger derfor til en på forventet lengde
     expect(screen.getAllByRole('row')).toHaveLength(mockOppgaver.oppgaver.length + 1);
   });
