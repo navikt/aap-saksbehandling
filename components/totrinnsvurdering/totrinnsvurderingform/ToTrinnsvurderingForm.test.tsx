c9cf69ec9fe7712b06442a84b0aed927bafb2959
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-04-25 17:22:06 +0200

    ✅ tester på ToTrinnsvurderingForm

---
+++ components/totrinnsvurdering/totrinnsvurderingform/ToTrinnsvurderingForm.test.tsx
@@ -0,0 +1,126 @@
+import { render, screen } from '@testing-library/react';
+import { Behovstype } from 'lib/utils/form';
+import { TotrinnsvurderingForm } from 'components/totrinnsvurdering/totrinnsvurderingform/TotrinnsvurderingForm';
+import { FatteVedtakGrunnlag } from 'lib/types/types';
+import userEvent from '@testing-library/user-event';
+
+const grunnlag: FatteVedtakGrunnlag = {
+  vurderinger: [
+    {
+      definisjon: Behovstype.AVKLAR_SYKDOM_KODE,
+    },
+  ],
+  historikk: [],
+};
+
+const link = `/sak/123/456`;
+
+describe('totrinnsvurderingform', () => {
+  const user = userEvent.setup();
+
+  it('skal ha en overskrift som er en lenke til vilkårsvurderingen', () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const overskriftLenke = screen.getByRole('link', { name: /§ 11-5 avklar sykdom/i });
+    expect(overskriftLenke).toBeVisible();
+    expect(overskriftLenke).toHaveAttribute('href', `${link}/SYKDOM#AVKLAR_SYKDOM`);
+  });
+
+  it('skal ha en radio group hvor beslutter kan godkjenne eller avslå vurderingen til saksbehandler/veileder', () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const godkjennValg = screen.getByRole('radio', { name: /godkjenn/i });
+    expect(godkjennValg).toBeVisible();
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    expect(vurderPåNyttValg).toBeVisible();
+  });
+
+  it('skal ha en knapp for å sende inn totrinnsvurderingene', () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const knapp = screen.getByRole('button', { name: /send inn/i });
+    expect(knapp).toBeVisible();
+  });
+
+  it('skal dukke opp felt for begrunnelse dersom vurderingen har blitt avslått', async () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    await user.click(vurderPåNyttValg);
+
+    const begrunnelseFelt = await screen.getByRole('textbox', { name: /begrunnelse/i });
+    expect(begrunnelseFelt).toBeVisible();
+  });
+
+  it('skal dukke opp felt for å velge grunner for avslag dersom vurderingen har blitt avslått', async () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    await user.click(vurderPåNyttValg);
+
+    const mangelfullBegrunnelse = screen.getByRole('checkbox', { name: /mangelfull begrunnelse/i });
+    const manglendeUtredning = screen.getByRole('checkbox', { name: /manglende utredning/i });
+    const feilLovanvendelse = screen.getByRole('checkbox', { name: /feil lovanvendelse/i });
+    const annet = screen.getByRole('checkbox', {
+      name: /annet \(skriv i begrunnelsen\)/i,
+    });
+
+    expect(mangelfullBegrunnelse).toBeVisible();
+    expect(manglendeUtredning).toBeVisible();
+    expect(feilLovanvendelse).toBeVisible();
+    expect(annet).toBeVisible();
+  });
+
+  it('skal ha riktige vlag i feltet for å velge grunner', async () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    await user.click(vurderPåNyttValg);
+
+    const grunnerFelt = screen.getByRole('group', { name: /velg grunn/i });
+    expect(grunnerFelt).toBeVisible();
+  });
+
+  it('skal dukke opp feilmelding hvis begrunnelse ikke har blitt besvart ved innsending', async () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    await user.click(vurderPåNyttValg);
+
+    const knapp = screen.getByRole('button', { name: /send inn/i });
+    await user.click(knapp);
+
+    const errorMessage = screen.getByText('Du må gi en begrunnelse');
+    expect(errorMessage).toBeVisible();
+  });
+
+  it('skal dukke opp feilmelding hvis ingen grunn har blitt valgt ved innsending', async () => {
+    render(
+      <TotrinnsvurderingForm fatteVedtakGrunnlag={grunnlag} link={link} readOnly={false} behandlingsReferanse={'456'} />
+    );
+
+    const vurderPåNyttValg = screen.getByRole('radio', { name: /vurdèr på nytt/i });
+    await user.click(vurderPåNyttValg);
+
+    const knapp = screen.getByRole('button', { name: /send inn/i });
+    await user.click(knapp);
+
+    const errorMessage = screen.getByText('Du må oppgi en grunn');
+    expect(errorMessage).toBeVisible();
+  });
+});
