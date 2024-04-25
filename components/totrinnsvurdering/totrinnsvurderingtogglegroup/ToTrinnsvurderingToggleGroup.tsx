3988bb86c80de610177f4f025c43c1a7ddaa3fee
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-04-25 13:23:22 +0200

    :sparkles: Legger på tooltip i togglegroup

--- components/totrinnsvurdering/totrinnsvurderingtogglegroup/ToTrinnsvurderingToggleGroup.tsx
+++ components/totrinnsvurdering/totrinnsvurderingtogglegroup/ToTrinnsvurderingToggleGroup.tsx
@@ -1,4 +1,4 @@
-import { ToggleGroup } from '@navikt/ds-react';
+import { ToggleGroup, Tooltip } from '@navikt/ds-react';
 import { ClockDashedIcon, PersonGavelFillIcon } from '@navikt/aksel-icons';
 import { Dispatch } from 'react';
 
@@ -9,15 +9,22 @@ interface Props {
 
 export const ToTrinnsvurderingToggleGroup = ({ activeToggle, setToggleValue }: Props) => {
   return (
-    <ToggleGroup size={'small'} defaultValue="lest" onChange={(value) => setToggleValue(value)} value={activeToggle}>
-      <ToggleGroup.Item value="totrinnsvurdering">
-        <PersonGavelFillIcon aria-hidden />
-        Totrinn
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="historikk">
-        <ClockDashedIcon aria-hidden />
-        Historikk
-      </ToggleGroup.Item>
+    <ToggleGroup
+      size={'small'}
+      defaultValue={activeToggle}
+      onChange={(value) => setToggleValue(value)}
+      value={activeToggle}
+    >
+      <Tooltip content={'Totrinnsvurdering'}>
+        <ToggleGroup.Item value="totrinnsvurdering">
+          <PersonGavelFillIcon aria-hidden />
+        </ToggleGroup.Item>
+      </Tooltip>
+      <Tooltip content={'Historikk'}>
+        <ToggleGroup.Item value="historikk">
+          <ClockDashedIcon aria-hidden />
+        </ToggleGroup.Item>
+      </Tooltip>
     </ToggleGroup>
   );
 };
