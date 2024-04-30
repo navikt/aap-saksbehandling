14181911e9318c0fbf039c03128308cff878d424
Tor Idland <tor.idland@nav.no>
2024-04-30 10:53:13 +0200

    :recycle: legger til hjemmel-select i mjølner
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Vetle Hollund <vetle.hollund@nav.no>

--- app/aktivitet/page.tsx
+++ app/aktivitet/page.tsx
@@ -6,12 +6,14 @@ import { JaEllerNei, JaEllerNeiOptions } from 'lib/utils/form';
 import styles from './page.module.css';
 import { FigureIcon } from '@navikt/aksel-icons';
 import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
+import { Button } from '@navikt/ds-react';
 
 interface FormFields {
   begrunnelse: string;
   aktivitetspliktOppfylt: string;
   grunn?: string;
   dato?: Date;
+  hjemmel: string;
 }
 export default function Page() {
   const { form, formFields } = useConfigForm<FormFields>({
@@ -26,9 +28,14 @@ export default function Page() {
       rules: { required: 'Du må svare på om vilkåret er oppfyllt' },
       options: JaEllerNeiOptions,
     },
+    hjemmel: {
+      type: 'select',
+      label: 'Hjemmel',
+      options: ['§11-7', '§11-8', '§11-9'],
+    },
     grunn: {
       type: 'radio',
-      label: 'Begrunnelse',
+      label: 'Årsak',
       options: [
         'Ikke møtt til møte med Nav',
         'Ikke møtt i behandling',
@@ -43,14 +50,22 @@ export default function Page() {
     },
   });
   const aktivitetspliktOppfylt = form.watch('aktivitetspliktOppfylt');
+  const hjemmel = form.watch('hjemmel');
+  const buttonText = hjemmel === '§11-7' || hjemmel === '§11-8' ? 'Send forhåndsvarsel' : 'Send';
   return (
     <div className={styles.aktivitetSkjema}>
       <VilkårsKort heading={'Vurder aktivitetsplikt'} steg={'FATTE_VEDTAK'} icon={<FigureIcon fontSize={'inherit'} />}>
-        <form>
+        <form className={styles.form}>
           <FormField form={form} formField={formFields.begrunnelse} />
           <FormField form={form} formField={formFields.aktivitetspliktOppfylt} />
-          {aktivitetspliktOppfylt === JaEllerNei.Nei && <FormField form={form} formField={formFields.grunn} />}
+          {aktivitetspliktOppfylt === JaEllerNei.Nei && (
+            <>
+              <FormField form={form} formField={formFields.hjemmel} />
+              <FormField form={form} formField={formFields.grunn} />
+            </>
+          )}
           <FormField form={form} formField={formFields.dato} />
+          <Button>{buttonText}</Button>
         </form>
       </VilkårsKort>
     </div>
