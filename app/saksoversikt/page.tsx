63e05db29f8363e7b41de71650342126efa11703
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-05-03 11:39:11 +0200

    :bug: Flytter knapp for å rekjøre feilede oppgaver til client component

--- app/saksoversikt/page.tsx
+++ app/saksoversikt/page.tsx
@@ -5,14 +5,11 @@ import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
 
 import styles from './page.module.css';
 import { isLocal } from 'lib/utils/environment';
-import { Button } from '@navikt/ds-react';
-import { rekjørFeiledeOppgaver } from 'lib/clientApi';
 
 const Page = async () => {
   const alleSaker = await hentAlleSaker();
   return (
     <main className={styles.main}>
-      <Button onClick={async () => await rekjørFeiledeOppgaver()}>Rekjør feilede oppgaver</Button>
       {isLocal() && <OpprettSak />}
       <AlleSakerListe alleSaker={alleSaker} />
     </main>
--- components/saksliste/AlleSakerListe.tsx
+++ components/saksliste/AlleSakerListe.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { hentAlleSaker } from 'lib/clientApi';
+import { hentAlleSaker, rekjørFeiledeOppgaver } from 'lib/clientApi';
 import { SaksInfo } from 'lib/types/types';
 import Link from 'next/link';
 import useSWR from 'swr';
-import { Table } from '@navikt/ds-react';
+import { Button, Table } from '@navikt/ds-react';
 import { formaterDatoForFrontend } from 'lib/utils/date';
 
 interface Props {
@@ -14,27 +14,30 @@ interface Props {
 export const AlleSakerListe = ({ alleSaker }: Props) => {
   const { data } = useSWR('api/sak/alle', hentAlleSaker, { fallbackData: alleSaker });
   return (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Saksnummer</Table.HeaderCell>
-          <Table.HeaderCell>Ident</Table.HeaderCell>
-          <Table.HeaderCell>Fra</Table.HeaderCell>
-          <Table.HeaderCell>Til</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {data?.map((sak) => (
-          <Table.Row key={sak.saksnummer}>
-            <Table.DataCell>
-              <Link href={`/sak/${sak.saksnummer}/`}>{sak.saksnummer}</Link>
-            </Table.DataCell>
-            <Table.DataCell>{sak.ident}</Table.DataCell>
-            <Table.DataCell>{formaterDatoForFrontend(sak.periode.fom)}</Table.DataCell>
-            <Table.DataCell>{formaterDatoForFrontend(sak.periode.tom)}</Table.DataCell>
+    <>
+      <Button onClick={async () => await rekjørFeiledeOppgaver()}>Rekjør feilede oppgaver</Button>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Saksnummer</Table.HeaderCell>
+            <Table.HeaderCell>Ident</Table.HeaderCell>
+            <Table.HeaderCell>Fra</Table.HeaderCell>
+            <Table.HeaderCell>Til</Table.HeaderCell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Header>
+        <Table.Body>
+          {data?.map((sak) => (
+            <Table.Row key={sak.saksnummer}>
+              <Table.DataCell>
+                <Link href={`/sak/${sak.saksnummer}/`}>{sak.saksnummer}</Link>
+              </Table.DataCell>
+              <Table.DataCell>{sak.ident}</Table.DataCell>
+              <Table.DataCell>{formaterDatoForFrontend(sak.periode.fom)}</Table.DataCell>
+              <Table.DataCell>{formaterDatoForFrontend(sak.periode.tom)}</Table.DataCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </>
   );
 };
