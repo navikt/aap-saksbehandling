a71c631c45782799f9268d25d49b638d8eeecb18
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-05-13 10:56:55 +0200

    Kommenterer ut hent beregningsgrunnlag

--- components/behandlinger/grunnlag/fastsettberegning/FastsettBeregningMedDataFeching.tsx
+++ components/behandlinger/grunnlag/fastsettberegning/FastsettBeregningMedDataFeching.tsx
@@ -1,8 +1,5 @@
 import { FastsettBeregning } from 'components/behandlinger/grunnlag/fastsettberegning/FastsettBeregning';
-import {
-  hentBeregningsGrunnlag,
-  hentBeregningsVurdering,
-} from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { hentBeregningsVurdering } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 
 interface Props {
   behandlingsReferanse: string;
@@ -11,14 +8,14 @@ interface Props {
 
 export const FastsettBeregningMedDataFeching = async ({ behandlingsReferanse, readOnly }: Props) => {
   const vurdering = await hentBeregningsVurdering(behandlingsReferanse);
-  const grunnlag = await hentBeregningsGrunnlag(behandlingsReferanse);
+  // const grunnlag = await hentBeregningsGrunnlag(behandlingsReferanse);
 
   return (
     <FastsettBeregning
       behandlingsReferanse={behandlingsReferanse}
       readOnly={readOnly}
       vurdering={vurdering}
-      grunnlag={grunnlag}
+      // grunnlag={grunnlag}
     />
   );
 };
