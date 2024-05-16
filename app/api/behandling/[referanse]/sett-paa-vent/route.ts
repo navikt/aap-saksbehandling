951cd352cc99f37747c940be0487bcc4f9aca31f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-05-16 15:54:30 +0200

    :sparkles: Henter flyten på nytt etter behandlingen er satt på vent

--- app/api/behandling/[referanse]/sett-paa-vent/route.ts
+++ app/api/behandling/[referanse]/sett-paa-vent/route.ts
@@ -1,11 +1,13 @@
 import { settBehandlingPåVent } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { NextRequest } from 'next/server';
+import { revalidateTag } from 'next/cache';
 
 export async function POST(req: NextRequest, { params }: { params: { referanse: string } }) {
   const body = await req.json();
 
   try {
     await settBehandlingPåVent(params.referanse, body);
+    revalidateTag(`api/hent/${params.referanse}/flyt`);
     return new Response(JSON.stringify({ message: 'Behov løst' }), { status: 200 });
   } catch (error) {
     console.log('error i route', error);
