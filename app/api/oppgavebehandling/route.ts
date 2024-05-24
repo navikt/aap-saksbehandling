95604c508254089333adc9cc5f9b3ae7b63fceac
Øivind Stensrud <oivind.stensrud@nav.no>
2024-05-24 09:48:07 +0200

    :recycle: Sender querystring videre til backend

--- app/api/oppgavebehandling/route.ts
+++ app/api/oppgavebehandling/route.ts
@@ -2,17 +2,22 @@ import { fetchProxy } from 'lib/services/fetchProxy';
 import { isLocal } from 'lib/utils/environment';
 import { mockOppgaver } from 'mocks/mockOppgaver';
 import { Oppgaver } from 'lib/types/oppgavebehandling';
+import { NextRequest } from 'next/server';
 
 const oppgavestyringApiBaseUrl = process.env.OPPGAVESTYRING_API_BASE_URL;
 const oppgavestyringApiScope = process.env.OPPGAVESTYRING_API_SCOPE ?? '';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   if (isLocal()) {
     return new Response(JSON.stringify(mockOppgaver), { status: 200 });
   }
 
   try {
-    const res = await fetchProxy<Oppgaver>(`${oppgavestyringApiBaseUrl}/oppgaver`, oppgavestyringApiScope, 'GET');
+    const res = await fetchProxy<Oppgaver>(
+      `${oppgavestyringApiBaseUrl}/oppgaver/?${req.nextUrl.searchParams}`,
+      oppgavestyringApiScope,
+      'GET'
+    );
     return new Response(JSON.stringify(res), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ message: JSON.stringify(error), status: 500 }), { status: 500 });
