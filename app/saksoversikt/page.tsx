3f9deaff434267f24dbfe4f5b271dba7bb2d983f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-05-30 15:24:56 +0200

    Legger til link på saksoversiktsiden til oppgavelisten

--- app/saksoversikt/page.tsx
+++ app/saksoversikt/page.tsx
@@ -5,12 +5,17 @@ import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
 import styles from './page.module.css';
 import { isLocal } from 'lib/utils/environment';
 import { OpprettSak } from 'components/opprettsak/OpprettSak';
+import { LinkPanel } from '@navikt/ds-react';
+import Link from 'next/link';
 
 const Page = async () => {
   const alleSaker = await hentAlleSaker();
   return (
     <main className={styles.main}>
       {isLocal() && <OpprettSak />}
+      <Link href={'/oppgaveliste'}>
+        <LinkPanel>Trykk her for å komme til oppgavelisten som oppgavestyring jobber med =)</LinkPanel>
+      </Link>
       <AlleSakerListe alleSaker={alleSaker} />
     </main>
   );
