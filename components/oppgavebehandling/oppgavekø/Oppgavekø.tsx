4dd03897bb7b20058cb69dcb42a9df5166cf47aa
Øivind Stensrud <oivind.stensrud@nav.no>
2024-06-03 08:49:24 +0200

    :sparkles: Vis antall oppgaver i liste

--- components/oppgavebehandling/oppgavekø/Oppgavekø.tsx
+++ components/oppgavebehandling/oppgavekø/Oppgavekø.tsx
@@ -8,14 +8,19 @@ import { OppgaveFetcher } from 'components/oppgavebehandling/oppgavekø/oppgavet
 import { KøContext } from 'components/oppgavebehandling/KøContext';
 import { useContext } from 'react';
 import { skjulPrototype } from 'lib/utils/skjulPrototype';
+import useSWR from 'swr';
 
 export const Oppgavekø = () => {
   const køContext = useContext(KøContext);
+  const { data } = useSWR('oppgaveliste');
+  const antallOppgaver = () => (data?.oppgaver.length ? `(${data.oppgaver.length})` : '');
   return (
     <section className={styles.oppgavekø}>
       <ExpansionCard aria-label={køContext.valgtKø.navn} defaultOpen>
         <ExpansionCard.Header>
-          <ExpansionCard.Title>{skjulPrototype() ? 'Felles AAP Kø' : køContext.valgtKø.navn}</ExpansionCard.Title>
+          <ExpansionCard.Title>
+            {skjulPrototype() ? 'Felles AAP Kø' : køContext.valgtKø.navn} {antallOppgaver()}
+          </ExpansionCard.Title>
         </ExpansionCard.Header>
         <ExpansionCard.Content>
           <Filter />
