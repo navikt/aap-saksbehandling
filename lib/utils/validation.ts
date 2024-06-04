f0b6ceab3c32bd042a99bc61e0e92d0d3ab6179f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-06-04 10:58:02 +0200

    :sparkles: Legger til validering for årstall og tilhørende tester

--- lib/utils/validation.ts
+++ lib/utils/validation.ts
@@ -1,9 +1,11 @@
 import { getYear, parse } from 'date-fns';
-export function validerÅrstall(val: string | string[], errorMessage: string) {
-  if (val.length !== 4) return errorMessage;
+
+export function validerÅrstall(val: string | string[]) {
+  if (val.length !== 4) return 'Verdien må inneholde 4 siffer';
   const parsedDate = parse(val as string, 'yyyy', new Date());
   if (getYear(parsedDate) === Number(val)) {
     return true;
+  } else {
+    return 'Verdien er et ugyldig årstall';
   }
-  return errorMessage;
 }
