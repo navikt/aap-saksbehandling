531b8fdc90abe8abc8952aaf4bf5cfad97814f51
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-06-11 13:08:04 +0200

    :egg: Under arbeid
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import 'styles/globals.css';
 
 import { AppHeader } from 'components/appheader/AppHeader';
 import { hentBrukerInformasjon } from 'lib/services/azureuserservice/azureUserService';
+import { isDev, isLocal } from 'lib/utils/environment';
+import styles from './layout.module.css';
+import { Label } from '@navikt/ds-react';
 
 export const metadata = {
   title: 'Kelvin',
@@ -17,6 +20,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="nb">
       <body>
+        {(isLocal() || isDev()) && (
+          <div className={styles.underArbeid}>
+            <Label spacing>Under arbeid</Label>
+            <br />
+            <Label>04.06.2024</Label>
+          </div>
+        )}
         <AppHeader brukerInformasjon={brukerInformasjon} />
         {children}
       </body>
