aae78d264bb8ba94db639cbe45629995c5ca4b22
Øivind Stensrud <oivind.stensrud@nav.no>
2024-06-11 12:48:17 +0200

    :bug: Endepunkt plukket ikke opp filter-id

--- app/api/oppgavebehandling/filter/[filterid]/route.ts
+++ app/api/oppgavebehandling/filter/[filterid]/route.ts
@@ -1,21 +1,18 @@
 import { isLocal } from 'lib/utils/environment';
 import { fetchProxy } from 'lib/services/fetchProxy';
 import { logError } from '@navikt/aap-felles-utils';
+import { NextRequest } from 'next/server';
 
 const oppgavestyringApiBaseUrl = process.env.OPPGAVESTYRING_API_BASE_URL;
 const oppgavestyringApiScope = process.env.OPPGAVESTYRING_API_SCOPE ?? '';
 
-interface Params {
-  params: { id: string };
-}
-
-export async function DELETE({ params }: Params) {
+export async function DELETE(req: NextRequest, { params }: { params: { filterid: string } }) {
   if (isLocal()) {
     return new Response(JSON.stringify({ message: 'Kø er slettet', status: 200 }), { status: 200 });
   }
 
   try {
-    await fetchProxy(`${oppgavestyringApiBaseUrl}/filter/${params.id}`, oppgavestyringApiScope, 'DELETE');
+    await fetchProxy(`${oppgavestyringApiBaseUrl}/filter/${params.filterid}`, oppgavestyringApiScope, 'DELETE');
   } catch (error) {
     logError('Feil ved sletting av kø', error);
     return new Response(JSON.stringify({ message: JSON.stringify(error) }), { status: 500 });
