b3bf4d952c223fab141c7e2d6563144bc5e672c8
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-06-12 13:05:26 +0200

    :fire: Fjerner under arbeid label. Sorry Halvor :(

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import 'styles/globals.css';
 
 import { AppHeader } from 'components/appheader/AppHeader';
 import { hentBrukerInformasjon } from 'lib/services/azureuserservice/azureUserService';
-import { isDev, isLocal } from 'lib/utils/environment';
-import styles from './layout.module.css';
-import { Label } from '@navikt/ds-react';
 
 export const metadata = {
   title: 'Kelvin',
@@ -20,13 +17,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="nb">
       <body>
-        {(isLocal() || isDev()) && (
-          <div className={styles.underArbeid}>
-            <Label spacing>Under arbeid</Label>
-            <br />
-            <Label>04.06.2024</Label>
-          </div>
-        )}
         <AppHeader brukerInformasjon={brukerInformasjon} />
         {children}
       </body>
