a0097978e2acf02821179287b5e505d47ecdf1d7
Tor Idland <tor.idland@nav.no>
2024-06-13 09:30:31 +0200

    :lipstick: Grid i Behandlingsinfo

--- components/behandlingsinfo/Behandlingsinfo.tsx
+++ components/behandlingsinfo/Behandlingsinfo.tsx
@@ -1,7 +1,6 @@
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort, HGrid, Label } from '@navikt/ds-react';
 import styles from './Behandlingsinfo.module.css';
 import { DetaljertBehandling } from 'lib/types/types';
-import { formaterDatoForFrontend } from 'lib/utils/date';
 
 interface Props {
   behandling: DetaljertBehandling;
@@ -14,23 +13,21 @@ export const Behandlingsinfo = ({ behandling }: Props) => {
         <Label as="p" size="medium" spacing>
           {behandling.type}
         </Label>
-        <div>
-          <Label as="p" size="medium" spacing>
-            Behandlingsstatus:
-          </Label>
-          <BodyShort size="medium" spacing>
-            {behandling.status}
-          </BodyShort>
-        </div>
-        <div>
-          <Label as="p" size="medium" spacing>
-            Opprettet:
-          </Label>
-          <BodyShort size="medium" spacing>
-            {formaterDatoForFrontend(behandling.opprettet)}
-          </BodyShort>
-        </div>
       </div>
+      <HGrid columns={'1fr 1fr'}>
+        <Label as="p" size="medium" spacing>
+          Behandlingsstatus:
+        </Label>
+        <BodyShort size="medium" spacing>
+          {behandling.status}
+        </BodyShort>
+        <Label as="p" size="medium" spacing>
+          Opprettet:
+        </Label>
+        <BodyShort size="medium" spacing>
+          {behandling.opprettet}
+        </BodyShort>
+      </HGrid>
     </div>
   );
 };
