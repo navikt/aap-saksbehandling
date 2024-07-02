d2c58c107d0867bae4b28443366d613a2b214bd8
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-07-02 09:20:47 +0200

    :fire: Fjern knapp til oppgavestyring

--- app/saksoversikt/page.tsx
+++ app/saksoversikt/page.tsx
@@ -5,16 +5,12 @@ import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
 import styles from './page.module.css';
 import { isLocal } from 'lib/utils/environment';
 import { OpprettSak } from 'components/opprettsak/OpprettSak';
-import { LinkPanel } from '@navikt/ds-react';
 
 const Page = async () => {
   const alleSaker = await hentAlleSaker();
   return (
     <main className={styles.main}>
       {isLocal() && <OpprettSak />}
-      <LinkPanel href={'/oppgaveliste'}>
-        Trykk her for å komme til oppgavelisten som oppgavestyring jobber med =)
-      </LinkPanel>
       <AlleSakerListe alleSaker={alleSaker} />
     </main>
   );
