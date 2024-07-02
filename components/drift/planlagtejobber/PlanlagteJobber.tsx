1278c4e766cb85d35a911e06c8392860e8bef3b3
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-07-02 15:24:38 +0200

    :sparkles: Litt mer info på PlanlagteJobber

--- components/drift/planlagtejobber/PlanlagteJobber.tsx
+++ components/drift/planlagtejobber/PlanlagteJobber.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Heading, Table } from '@navikt/ds-react';
+import { BodyShort, Heading, HStack, Label, Table, VStack } from '@navikt/ds-react';
 import { JobbInfo } from 'lib/types/types';
 import { formaterDatoMedTidspunktForFrontend } from 'lib/utils/date';
 
 interface Props {
   planlagteJobber: JobbInfo[];
 }
+
 export const PlanlagteJobber = ({ planlagteJobber }: Props) => {
   return (
     <div>
@@ -16,25 +17,52 @@ export const PlanlagteJobber = ({ planlagteJobber }: Props) => {
       <Table zebraStripes>
         <Table.Header>
           <Table.Row>
+            <Table.HeaderCell scope="col"></Table.HeaderCell>
             <Table.HeaderCell scope="col">Id</Table.HeaderCell>
+            <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
             <Table.HeaderCell scope="col">Status</Table.HeaderCell>
-            <Table.HeaderCell scope="col">Type</Table.HeaderCell>
             <Table.HeaderCell scope="col">Kjøretidspunkt</Table.HeaderCell>
             <Table.HeaderCell scope="col">Antall feilende forsøk</Table.HeaderCell>
-            <Table.HeaderCell scope="col">Feilmelding</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {planlagteJobber.map((jobb) => {
             return (
-              <Table.Row key={jobb.id}>
+              <Table.ExpandableRow
+                key={jobb.id}
+                content={
+                  <HStack>
+                    <VStack gap={'4'}>
+                      <div>
+                        <Label>Beskrivelse</Label>
+                        <BodyShort>{jobb.beskrivelse}</BodyShort>
+                      </div>
+                      {jobb.feilmelding && (
+                        <div>
+                          <Label>Feilmelding</Label>
+                          <BodyShort>{jobb.feilmelding}</BodyShort>
+                        </div>
+                      )}
+                    </VStack>
+                    <VStack gap={'4'}>
+                      {[...objectToMap(jobb.metadata)].map(([key, value]) => {
+                        return (
+                          <div key={key}>
+                            <Label>{key}</Label>
+                            <BodyShort>{value}</BodyShort>
+                          </div>
+                        );
+                      })}
+                    </VStack>
+                  </HStack>
+                }
+              >
                 <Table.DataCell>{jobb.id}</Table.DataCell>
-                <Table.DataCell>{jobb.type}</Table.DataCell>
+                <Table.DataCell>{jobb.navn}</Table.DataCell>
                 <Table.DataCell>{jobb.status}</Table.DataCell>
                 <Table.DataCell>{formaterDatoMedTidspunktForFrontend(jobb.planlagtKjøretidspunkt)}</Table.DataCell>
                 <Table.DataCell>{jobb.antallFeilendeForsøk}</Table.DataCell>
-                <Table.DataCell>{jobb.feilmelding}</Table.DataCell>
-              </Table.Row>
+              </Table.ExpandableRow>
             );
           })}
         </Table.Body>
@@ -42,3 +70,7 @@ export const PlanlagteJobber = ({ planlagteJobber }: Props) => {
     </div>
   );
 };
+
+function objectToMap(value: Object): Map<string, string> {
+  return new Map(Object.entries(value));
+}
