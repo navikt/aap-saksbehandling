4927e1799821f1f380176ac5c9d1a36c5ee2e47f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-08-13 15:23:57 +0200

    :sparkles: Legger til formateringsfunksjoner

--- lib/utils/string.test.ts
+++ lib/utils/string.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { storForbokstav } from 'lib/utils/string';
+import { formaterTilG, formaterTilNok, formaterTilProsent, storForbokstav } from 'lib/utils/string';
 
 describe('storForbokstav', () => {
   it('skal returnere en string med stor forbokstav dersom value er bare uppercase', () => {
@@ -8,5 +8,33 @@ describe('storForbokstav', () => {
 
   it('skal returnere en string med stor forbokstav dersom value er bare lowercase', () => {
     expect(storForbokstav('hello pello')).toBe('Hello pello');
+  });
+});
+
+describe('formaterTilG', () => {
+  it('skal returnere en string med korrekt formatering med 15 000', () => {
+    expect(formaterTilG(2)).toBe('2 G');
+  });
+
+  it('skal returnere en string med korrekt formatering med 150 000', () => {
+    expect(formaterTilG(4.3949)).toBe('4.3949 G');
+  });
+
+  it('skal returnere en string med korrekt formatering med 1 500 000', () => {
+    expect(formaterTilG(6)).toBe('6 G');
+  });
+});
+
+describe('formaterTilProsent', () => {
+  it('skal returnere en string med korrekt formatering med 15 000', () => {
+    expect(formaterTilProsent(50)).toBe('50 %');
+  });
+
+  it('skal returnere en string med korrekt formatering med 150 000', () => {
+    expect(formaterTilProsent(70)).toBe('70 %');
+  });
+
+  it('skal returnere en string med korrekt formatering med 1 500 000', () => {
+    expect(formaterTilProsent(80)).toBe('80 %');
   });
 });
--- lib/utils/string.ts
+++ lib/utils/string.ts
@@ -3,3 +3,15 @@ export const numberToString = (value?: number | null) => (value ? `${value}` : u
 export const storForbokstav = (value: string): string => {
   return value.charAt(0).toUpperCase().concat(value.slice(1).toLowerCase());
 };
+
+export function formaterTilNok(sum: number): string {
+  return `${sum.toLocaleString(`nb-NO`)} kr`;
+}
+
+export function formaterTilG(sum: number): string {
+  return `${sum} G`;
+}
+
+export function formaterTilProsent(sum: number): string {
+  return `${sum} %`;
+}
