1fef590b82fd41405d98030b163524d4821e4fba
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-08-21 14:34:32 +0200

    Legger til ikon på visningskort for beregning

--- components/behandlinger/grunnlag/visberegning/VisBeregning.tsx
+++ components/behandlinger/grunnlag/visberegning/VisBeregning.tsx
@@ -7,6 +7,7 @@ import { UføreVisning } from 'components/behandlinger/grunnlag/visberegning/vis
 import { YrkesskadeVisning } from 'components/behandlinger/grunnlag/visberegning/visning/yrkesskadevisning/YrkesskadeVisning';
 import { YrkesskadeUføreVisning } from 'components/behandlinger/grunnlag/visberegning/visning/yrkesskadeuførevisning/YrkesskadeUføreVisning';
 import { BeregningsGrunnlag } from 'lib/types/types';
+import { SackKronerIcon } from '@navikt/aksel-icons';
 
 interface Props {
   grunnlag: BeregningsGrunnlag;
@@ -19,8 +20,12 @@ export const VisBeregning = ({ grunnlag }: Props) => {
 
   return (
     <>
-      {/* @ts-ignore-line TODO: Finne ut hvordan vi skal vise disse kortene */}
-      <VilkårsKort heading={'Grunnlagsberegning § 11-19'} steg={'VIS_BEREGNING'}>
+      <VilkårsKort
+        heading={'Grunnlagsberegning § 11-19'}
+        // @ts-ignore-line TODO Finne ut hvordan vi skal vise disse kortene
+        steg={'VIS_BEREGNING'}
+        icon={<SackKronerIcon title="beregning-ikon" fontSize={'inherit'} />}
+      >
         {beregningsVisning === 'STANDARD' && <Grunnlag1119Visning grunnlag={grunnlag.grunnlag11_19} />}
         {beregningsVisning === 'UFØRE' && <UføreVisning grunnlag={grunnlag.grunnlagUføre} />}
         {beregningsVisning === 'YRKESSKADE' && <YrkesskadeVisning grunnlag={grunnlag.grunnlagYrkesskade} />}
