1091d8019c302fa8315b279e4557a4bac410a674
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-08-21 15:13:20 +0200

    Update string.test.ts

--- lib/utils/string.test.ts
+++ lib/utils/string.test.ts
@@ -12,29 +12,29 @@ describe('storForbokstav', () => {
 });
 
 describe('formaterTilG', () => {
-  it('skal returnere en string med korrekt formatering med 15 000', () => {
+  it('skal returnere en string med korrekt formatering med 2', () => {
     expect(formaterTilG(2)).toBe('2 G');
   });
 
-  it('skal returnere en string med korrekt formatering med 150 000', () => {
+  it('skal returnere en string med korrekt formatering med 4.39', () => {
     expect(formaterTilG(4.3949)).toBe('4.39 G');
   });
 
-  it('skal returnere en string med korrekt formatering med 1 500 000', () => {
+  it('skal returnere en string med korrekt formatering med 6', () => {
     expect(formaterTilG(6)).toBe('6 G');
   });
 });
 
 describe('formaterTilProsent', () => {
-  it('skal returnere en string med korrekt formatering med 15 000', () => {
+  it('skal returnere en string med korrekt formatering med 50', () => {
     expect(formaterTilProsent(50)).toBe('50 %');
   });
 
-  it('skal returnere en string med korrekt formatering med 150 000', () => {
+  it('skal returnere en string med korrekt formatering med 70', () => {
     expect(formaterTilProsent(70)).toBe('70 %');
   });
 
-  it('skal returnere en string med korrekt formatering med 1 500 000', () => {
+  it('skal returnere en string med korrekt formatering med 80', () => {
     expect(formaterTilProsent(80)).toBe('80 %');
   });
 });
