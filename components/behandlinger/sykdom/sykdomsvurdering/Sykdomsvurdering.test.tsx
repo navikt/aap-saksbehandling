25390dde36fd8b0d746d31939031ecb211118a5c
Øivind Stensrud <oivind.stensrud@nav.no>
2024-08-30 15:19:42 +0200

    :recycle: Forbedrer tester
    Spør på rolle og ikke kun tekst. Bytter ut noen regex-matcher
    med string-matching for å unngå "leaning toothpick syndrome"

--- components/behandlinger/sykdom/sykdomsvurdering/Sykdomsvurdering.test.tsx
+++ components/behandlinger/sykdom/sykdomsvurdering/Sykdomsvurdering.test.tsx
@@ -13,19 +13,19 @@ describe('sykdomsvurdering uten yrkesskade', () => {
 
   it('Skal ha korrekt heading', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const heading = screen.getByText('Nedsatt arbeidsevne - § 11-5');
+    const heading = screen.getByRole('heading', { name: 'Nedsatt arbeidsevne - § 11-5' });
     expect(heading).toBeVisible();
   });
 
   it('skal ha en liste over tilknyttede dokumenter til vilkåret ', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const tilknyttedeDokumenterListe = screen.getByRole('list', { name: /tilknyttede dokumenter/i });
+    const tilknyttedeDokumenterListe = screen.getByRole('list', { name: 'Tilknyttede dokumenter' });
     expect(tilknyttedeDokumenterListe).toBeVisible();
   });
 
   it('Skal ha et begrunnelsefelt', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const textbox = screen.getByRole('textbox', { name: /vurder den nedsatte arbeidsevnen/i });
+    const textbox = screen.getByRole('textbox', { name: /Vurder den nedsatte arbeidsevnen/ });
     expect(textbox).toBeVisible();
   });
 
@@ -33,7 +33,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
 
     const label = screen.getByText(
-      /hvilken sykdom \/ skade \/ lyte\. hva er det mest vesentlige\. hvorfor vurderes nedsatt arbeidsevne med minst 50%\?/i
+      'Hvilken sykdom / skade / lyte. Hva er det mest vesentlige. Hvorfor vurderes nedsatt arbeidsevne med minst 50%?'
     );
 
     expect(label).toBeVisible();
@@ -41,7 +41,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
 
   it('Skal ha et felt for om arbeidsevnen er nedsatt', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const textbox = screen.getByRole('group', { name: /er arbeidsevnen nedsatt\?/i });
+    const textbox = screen.getByRole('group', { name: 'Er arbeidsevnen nedsatt?' });
     expect(textbox).toBeVisible();
   });
 
@@ -49,7 +49,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
     const radioGroup = screen.getByRole('group', {
-      name: /er det sykdom, skade eller lyte som er vesentlig medvirkende til nedsatt arbeidsevne\? \(§ 11-5\)/i,
+      name: 'Er det sykdom, skade eller lyte som er vesentlig medvirkende til nedsatt arbeidsevne? (§ 11-5)',
     });
     expect(radioGroup).toBeVisible();
   });
@@ -57,40 +57,40 @@ describe('sykdomsvurdering uten yrkesskade', () => {
   it('Skal ha et felt for om arbeidsevnen er nedsatt med minst 50 prosent', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
-    const radioGroup = screen.getByRole('group', { name: /er arbeidsevnen nedsatt med minst 50%\?/i });
+    const radioGroup = screen.getByRole('group', { name: 'Er arbeidsevnen nedsatt med minst 50%?' });
     expect(radioGroup).toBeVisible();
   });
 
   it('Skal ha et felt for når arbeidsevnen ble nedsatt', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
-    const felt = screen.getByRole('textbox', { name: /hvilket år ble arbeidsevnen nedsatt\? \(§11-5\)/i });
+    const felt = screen.getByRole('textbox', { name: 'Hvilket år ble arbeidsevnen nedsatt? (§11-5)' });
     expect(felt).toBeVisible();
   });
 
   it('Skal ikke vise felt for når arbeidsevnen ble nedsatt dersom arbeidsevnen ikke er nedsatt', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const felt = screen.queryByRole('textbox', { name: /hvilket år ble arbeidsevnen nedsatt\? \(§11-5\)/i });
+    const felt = screen.queryByRole('textbox', { name: 'Hvilket år ble arbeidsevnen nedsatt? (§11-5)' });
     expect(felt).toBeNull();
   });
 
   it('Skal ikke vise felt for om sykdom, skade eller lyte er årsaken til nedsatt arbeidsevne dersom arbeidsevnen ikke er nedsatt', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     const radioGroup = screen.queryByRole('group', {
-      name: /er det sykdom, skade eller lyte som er vesentlig medvirkende til nedsatt arbeidsevne\? \(§ 11-5\)/i,
+      name: 'Er det sykdom, skade eller lyte som er vesentlig medvirkende til nedsatt arbeidsevne? (§ 11-5)',
     });
     expect(radioGroup).toBeNull();
   });
 
   it('Skal ikke vise felt for om arbeidsevnen er nedsatt med minst 50 prosent dersom arbeidsevnen ikke er nedsatt', () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const radioGroup = screen.queryByRole('group', { name: /er arbeidsevnen nedsatt med minst 50%\?/i });
+    const radioGroup = screen.queryByRole('group', { name: 'Er arbeidsevnen nedsatt med minst 50%?' });
     expect(radioGroup).toBeNull();
   });
 
   it('Skal vise feilmelding dersom begrunnelse felt ikke er besvart', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const button = screen.getByRole('button', { name: /bekreft/i });
+    const button = screen.getByRole('button', { name: /Bekreft/ });
     await user.click(button);
 
     expect(await screen.findByText('Du må begrunne')).toBeVisible();
@@ -98,7 +98,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
 
   it('Skal vise feilmelding dersom felt om arbeidsevnen er nedsatt ikke har blitt besvart', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
-    const button = screen.getByRole('button', { name: /bekreft/i });
+    const button = screen.getByRole('button', { name: /Bekreft/ });
     await user.click(button);
 
     expect(await screen.findByText('Du må svare på om arbeidsevnen er nedsatt')).toBeVisible();
@@ -107,7 +107,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
   it('Skal vise feilmelding dersom felt for om sykdom, skade eller lyte er årsaken til nedsatt arbeidsevne ikke er besvart', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
-    const button = screen.getByRole('button', { name: /bekreft/i });
+    const button = screen.getByRole('button', { name: /Bekreft/ });
     await user.click(button);
 
     expect(await screen.findByText('Du må svare på om vilkåret er oppfyllt')).toBeVisible();
@@ -116,7 +116,7 @@ describe('sykdomsvurdering uten yrkesskade', () => {
   it('Skal vise feilmelding dersom felt for om arbeidsevnen er nedsatt med minst 50% ikke er besvart', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
-    const button = screen.getByRole('button', { name: /bekreft/i });
+    const button = screen.getByRole('button', { name: /Bekreft/ });
     await user.click(button);
 
     expect(await screen.findByText('Du må svare på om arbeidsevnen er nedsatt med minst 50%')).toBeVisible();
@@ -125,15 +125,15 @@ describe('sykdomsvurdering uten yrkesskade', () => {
   it('Skal vise feilmelding dersom felt for hvilket år arbeisevnen ble nedsatt ikke er besvart', async () => {
     render(<Sykdomsvurdering grunnlag={grunnlag} readOnly={false} behandlingVersjon={0} tilknyttedeDokumenter={[]} />);
     await VelgAtArbeidsevneErNedsatt();
-    const button = screen.getByRole('button', { name: /bekreft/i });
+    const button = screen.getByRole('button', { name: /Bekreft/ });
     await user.click(button);
 
     expect(await screen.findByText('Du må sette en dato for når arbeidsevnen ble nedsatt')).toBeVisible();
   });
 
   async function VelgAtArbeidsevneErNedsatt() {
-    const group = screen.getByRole('group', { name: /er arbeidsevnen nedsatt\?/i });
-    const jaValg = within(group).getByRole('radio', { name: /ja/i });
+    const group = screen.getByRole('group', { name: 'Er arbeidsevnen nedsatt?' });
+    const jaValg = within(group).getByRole('radio', { name: /Ja/ });
     await user.click(jaValg);
   }
 });
