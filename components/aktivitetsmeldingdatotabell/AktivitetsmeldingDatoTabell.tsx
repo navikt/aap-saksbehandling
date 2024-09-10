27d0ef10b6d6685a4751d3391d07b1e1a4c9bc88
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-09-10 15:32:11 +0200

    glemte en prop

--- components/aktivitetsmeldingdatotabell/AktivitetsmeldingDatoTabell.tsx
+++ components/aktivitetsmeldingdatotabell/AktivitetsmeldingDatoTabell.tsx
@@ -14,16 +14,25 @@ export const AktivitetsmeldingDatoTabell = ({ bruddDatoPerioder, setBruddDatoPer
       <Table size={'small'}>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell textSize={'small'} scope={'col'}>Dato</Table.HeaderCell>
-            <Table.HeaderCell textSize={'small'} scope={'col'}>Til og med dato</Table.HeaderCell>
-            <Table.HeaderCell textSize={'small'} scope={'col'}>Type</Table.HeaderCell>
-            <Table.HeaderCell textSize={'small'} scope={'col'}>Handling</Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'} scope={'col'}>
+              Dato
+            </Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'} scope={'col'}>
+              Til og med dato
+            </Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'} scope={'col'}>
+              Type
+            </Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'} scope={'col'}>
+              Handling
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {bruddDatoPerioder.map((element) => (
             <AktivitetsmeldingDatoTabellRad
               key={element.id}
+              type={element.type}
               bruddDatoPeriode={element}
               onChange={(id, felt, value) => {
                 if (element.type === 'enkeltdag') {
