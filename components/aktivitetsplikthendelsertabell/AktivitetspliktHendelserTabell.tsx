7afbf3cb32641e27e121c9c139a1644c162170b5
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-09-17 16:34:31 +0200

    :sparkles: Mulig å registrere gyldig fravær

--- components/aktivitetsplikthendelsertabell/AktivitetspliktHendelserTabell.tsx
+++ components/aktivitetsplikthendelsertabell/AktivitetspliktHendelserTabell.tsx
@@ -1,15 +1,26 @@
 import { AktivitetspliktHendelse } from 'lib/types/types';
 
 import styles from 'components/aktivitetsplikthendelsertabell/AktivitetspliktHendelserTabell.module.css';
-import { Table } from '@navikt/ds-react';
+import { Button, Checkbox, Table } from '@navikt/ds-react';
 import { formaterDatoForFrontend } from 'lib/utils/date';
+import { FormField, useConfigForm } from '@navikt/aap-felles-react';
 
 interface Props {
   aktivitetspliktHendelser?: AktivitetspliktHendelse[];
 }
 
+interface FormFields {
+  gyldigFravær: string[];
+}
+
 export const AktivitetspliktHendelserTabell = ({ aktivitetspliktHendelser }: Props) => {
   const harAktivitetsmeldingeraktivitetsmeldinger = aktivitetspliktHendelser && aktivitetspliktHendelser.length > 0;
+
+  const { form, formFields } = useConfigForm<FormFields>({
+    gyldigFravær: {
+      type: 'checkbox_nested',
+    },
+  });
 
   return (
     <div>
@@ -18,26 +29,41 @@ export const AktivitetspliktHendelserTabell = ({ aktivitetspliktHendelser }: Pro
         {!harAktivitetsmeldingeraktivitetsmeldinger ? (
           <span>Ingen tidligere brudd registrert</span>
         ) : (
-          <Table size={'small'}>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Type brudd</Table.HeaderCell>
-                <Table.HeaderCell>Årsak</Table.HeaderCell>
-                <Table.HeaderCell>Dato</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {aktivitetspliktHendelser?.map((aktivitetspliktHendelse, index) => {
-                return (
-                  <Table.Row key={index}>
-                    <Table.DataCell>{aktivitetspliktHendelse.paragraf}</Table.DataCell>
-                    <Table.DataCell>{aktivitetspliktHendelse.brudd}</Table.DataCell>
-                    <Table.DataCell>{formaterDatoForFrontend(aktivitetspliktHendelse.periode.fom)}</Table.DataCell>
+          <form onSubmit={form.handleSubmit((data) => console.log(data))} className={'flex-column'}>
+            <FormField form={form} formField={formFields.gyldigFravær}>
+              <Table size={'small'}>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Type brudd</Table.HeaderCell>
+                    <Table.HeaderCell>Årsak</Table.HeaderCell>
+                    <Table.HeaderCell>Dato fra og med</Table.HeaderCell>
+                    <Table.HeaderCell>Dato til og med</Table.HeaderCell>
+                    <Table.HeaderCell>Er gyldig fravær?</Table.HeaderCell>
                   </Table.Row>
-                );
-              })}
-            </Table.Body>
-          </Table>
+                </Table.Header>
+                <Table.Body>
+                  {aktivitetspliktHendelser?.map((aktivitetspliktHendelse, index) => {
+                    return (
+                      <Table.Row key={index}>
+                        <Table.DataCell>{aktivitetspliktHendelse.paragraf}</Table.DataCell>
+                        <Table.DataCell>{aktivitetspliktHendelse.brudd}</Table.DataCell>
+                        <Table.DataCell>{formaterDatoForFrontend(aktivitetspliktHendelse.periode.fom)}</Table.DataCell>
+                        <Table.DataCell>{formaterDatoForFrontend(aktivitetspliktHendelse.periode.tom)}</Table.DataCell>
+                        <Table.DataCell>
+                          <Checkbox size={'small'} hideLabel value={aktivitetspliktHendelse.hendelseId}>
+                            Gyldig fravær
+                          </Checkbox>
+                        </Table.DataCell>
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+              </Table>
+            </FormField>
+            <Button className={'fit-content-button'} size={'small'}>
+              Send inn
+            </Button>
+          </form>
         )}
       </section>
     </div>
