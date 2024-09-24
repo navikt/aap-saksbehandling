91f486713a6e51c80d7a81ca97ea647e268c0288
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-09-24 14:47:34 +0200

    max-width på input

--- components/barn/manueltbarnvurderingfelter/ManueltBarnVurderingFelter.tsx
+++ components/barn/manueltbarnvurderingfelter/ManueltBarnVurderingFelter.tsx
@@ -11,7 +11,7 @@ import { RadioGroupWrapper } from 'components/input/RadioGroupWrapper';
 import { isBefore, isFuture } from 'date-fns';
 import { DATO_FORMATER, stringToDate } from 'lib/utils/date';
 
-import styles from './ManueltBarnVurderingFelter.module.css';
+import 'components/barn/manueltbarnvurderingfelter/ManueltBarnVurderingFelter.css';
 
 interface Props {
   ident: string;
@@ -49,56 +49,50 @@ export const ManueltBarnVurderingFelter = ({ readOnly, barneTilleggIndex, vurder
       </RadioGroupWrapper>
 
       {harForeldreAnsvar && (
-        <div className={styles.periode}>
-          <div className={styles.datofelt}>
+        <div className={'barnetilleggperiode'}>
+          <TextFieldWrapper
+            label={'Søker har forsørgeransvar for barnet fra'}
+            control={form.control}
+            name={`barnetilleggVurderinger.${barneTilleggIndex}.vurderinger.${vurderingIndex}.fom`}
+            type={'text'}
+            rules={{
+              validate: (value) => {
+                if (!value) {
+                  return 'Du må sette en dato for når søker har forsørgeransvar for barnet fra';
+                } else {
+                  const parsedValue = stringToDate(value as string, DATO_FORMATER.ddMMyyyy);
+                  if (!parsedValue) {
+                    return 'Dato for når søker har forsørgeransvar fra er ikke gyldig';
+                  } else {
+                    return isFuture(parsedValue)
+                      ? 'Dato for når søker har forsørgeransvar fra kan ikke være frem i tid'
+                      : false;
+                  }
+                }
+              },
+            }}
+          />
+          {leggTilSluttDato ? (
             <TextFieldWrapper
-              label={'Søker har forsørgeransvar for barnet fra'}
+              label={'Sluttdato for forsørgeransvaret'}
               control={form.control}
-              name={`barnetilleggVurderinger.${barneTilleggIndex}.vurderinger.${vurderingIndex}.fom`}
+              name={`barnetilleggVurderinger.${barneTilleggIndex}.vurderinger.${vurderingIndex}.tom`}
               type={'text'}
               rules={{
-                validate: (value) => {
-                  if (!value) {
-                    return 'Du må sette en dato for når søker har forsørgeransvar for barnet fra';
-                  } else {
-                    const parsedValue = stringToDate(value as string, DATO_FORMATER.ddMMyyyy);
-                    if (!parsedValue) {
-                      return 'Dato for når søker har forsørgeransvar fra er ikke gyldig';
-                    } else {
-                      return isFuture(parsedValue)
-                        ? 'Dato for når søker har forsørgeransvar fra kan ikke være frem i tid'
-                        : false;
-                    }
+                validate: (value, formValues) => {
+                  const parsedValueTom = stringToDate(value as string, DATO_FORMATER.ddMMyyyy);
+                  const parsedValueFom = stringToDate(
+                    formValues.barnetilleggVurderinger[barneTilleggIndex].vurderinger[vurderingIndex]
+                      .fom as unknown as string,
+                    DATO_FORMATER.ddMMyyyy
+                  );
+
+                  if (parsedValueTom && parsedValueFom) {
+                    return isBefore(parsedValueTom, parsedValueFom) ? 'Slutt-dato kan ikke være før start-dato' : false;
                   }
                 },
               }}
             />
-          </div>
-          {leggTilSluttDato ? (
-            <div className={styles.datofelt}>
-              <TextFieldWrapper
-                label={'Sluttdato for forsørgeransvaret'}
-                control={form.control}
-                name={`barnetilleggVurderinger.${barneTilleggIndex}.vurderinger.${vurderingIndex}.tom`}
-                type={'text'}
-                rules={{
-                  validate: (value, formValues) => {
-                    const parsedValueTom = stringToDate(value as string, DATO_FORMATER.ddMMyyyy);
-                    const parsedValueFom = stringToDate(
-                      formValues.barnetilleggVurderinger[barneTilleggIndex].vurderinger[vurderingIndex]
-                        .fom as unknown as string,
-                      DATO_FORMATER.ddMMyyyy
-                    );
-
-                    if (parsedValueTom && parsedValueFom) {
-                      return isBefore(parsedValueTom, parsedValueFom)
-                        ? 'Slutt-dato kan ikke være før start-dato'
-                        : false;
-                    }
-                  },
-                }}
-              />
-            </div>
           ) : (
             <Button
               onClick={() => setLeggTilSluttDato(true)}
