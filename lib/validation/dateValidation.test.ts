3fcae65603c08feb4ef53659e3d53d60b076cf57
Øivind Stensrud <oivind.stensrud@nav.no>
2024-09-27 10:11:22 +0200

    :white_check_mark: Test for validering av gyldige datoer

--- lib/validation/dateValidation.test.ts
+++ lib/validation/dateValidation.test.ts
@@ -9,4 +9,8 @@ describe('Dato-validering', () => {
   it('gir feilmelding når datoen er ugydlig', () => {
     expect(validerDato('31.02.2024')).toEqual('Dato format er ikke gyldig. Dato må være på formatet dd.mm.åååå');
   });
+
+  it('gir undefined når datoen er gyldig', () => {
+    expect(validerDato('12.07.2024')).toBeUndefined();
+  });
 });
