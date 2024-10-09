04a66f90b1c8d79b8201d4c8f95955b9b27a32f8
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-10-09 15:01:52 +0200

    :fire: Fjerner sanity og tiptap

--- app/sanity/page.tsx
+++
@@ -1,8 +0,0 @@
-import { hentAlleBrevmaler } from 'lib/services/sanityservice/sanityservice';
-import { BrevmalVelger } from 'components/brevmalvelger/BrevmalVelger';
-
-export default async function Page() {
-  const brevmaler = await hentAlleBrevmaler();
-
-  return <BrevmalVelger brevmaler={brevmaler} />;
-}
