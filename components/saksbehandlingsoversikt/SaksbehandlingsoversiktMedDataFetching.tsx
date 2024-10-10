e880bce66d6a149c1f3ff65a1ed23535ec538958
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-10-10 14:52:17 +0200

    foreløpig try/catch slik at vi ikke brekker dev

--- components/saksbehandlingsoversikt/SaksbehandlingsoversiktMedDataFetching.tsx
+++ components/saksbehandlingsoversikt/SaksbehandlingsoversiktMedDataFetching.tsx
@@ -1,12 +1,16 @@
 import { Saksbehandlingsoversikt } from 'components/saksbehandlingsoversikt/Saksbehandlingsoversikt';
 import { hentAlleDokumenterPåSak } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { logError } from '@navikt/aap-felles-utils';
 
 interface Props {
   saksnummer: string;
 }
 
 export const SaksbehandlingsoversiktMedDataFetching = async ({ saksnummer }: Props) => {
-  const dokumenter = await hentAlleDokumenterPåSak(saksnummer);
-
-  return <Saksbehandlingsoversikt dokumenter={dokumenter} />;
+  try {
+    const dokumenter = await hentAlleDokumenterPåSak(saksnummer);
+    return <Saksbehandlingsoversikt dokumenter={dokumenter} />;
+  } catch (error) {
+    logError('noe gikk galt i innhenting av dokumenter');
+  }
 };
