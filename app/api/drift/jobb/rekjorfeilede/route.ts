456048f6c286f50fe3cf1ee397972fbdb35b87a9
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-11-05 10:54:12 +0100

    :fire: Sletter alt som har med drift å gjøre

--- app/api/drift/jobb/rekjorfeilede/route.ts
+++
@@ -1,9 +0,0 @@
-import { rekjørFeiledeJobber } from 'lib/services/saksbehandlingservice/saksbehandlingService';
-
-export async function GET() {
-  try {
-    return new Response(await rekjørFeiledeJobber(), { status: 200 });
-  } catch (err: any) {
-    return new Response(err?.message, { status: 500 });
-  }
-}
