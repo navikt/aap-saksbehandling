58e97d04f60f808b3febf79c9e6c9d96ffad64cd
Øivind Stensrud <oivind.stensrud@nav.no>
2024-11-08 11:37:16 +0100

    :construction: Startet på kobling mot dokumentinnhenting

---
+++ app/api/dokumentinnhenting/behandleroppslag/route.ts
@@ -0,0 +1,19 @@
+import { logError } from '@navikt/aap-felles-utils';
+import { BehandleroppslagResponse } from 'components/innhentdokumentasjon/innhentdokumentasjonskjema/InnhentDokumentasjonSkjema';
+import { fetchProxy } from 'lib/services/fetchProxy';
+import { NextRequest } from 'next/server';
+
+const dokumentinnhentingApiBaseUrl = 'http://dokumentinnhenting';
+const dokumentinnhentingApiScope = 'api://dev-gcp.aap.dokumentinnhenting/.default';
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const url = `${dokumentinnhentingApiBaseUrl}/syfo/behandleroppslag/search`;
+
+    return await fetchProxy<BehandleroppslagResponse>(url, dokumentinnhentingApiScope, 'POST', body);
+  } catch (err) {
+    logError(`/dokumentinnhenting/behandleroppslag`, err);
+    return new Response(JSON.stringify({ message: 'Behandleroppslag feilet' }), { status: 500 });
+  }
+}
