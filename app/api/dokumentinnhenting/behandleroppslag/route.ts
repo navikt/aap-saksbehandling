5d06ecb57e618feb7086d3969f251464838bd4ba
Øivind Stensrud <oivind.stensrud@nav.no>
2024-11-08 12:32:49 +0100

    :bug: Returnerer response fra route

--- app/api/dokumentinnhenting/behandleroppslag/route.ts
+++ app/api/dokumentinnhenting/behandleroppslag/route.ts
@@ -10,8 +10,8 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
     const url = `${dokumentinnhentingApiBaseUrl}/syfo/behandleroppslag/search`;
-
-    return await fetchProxy<BehandleroppslagResponse>(url, dokumentinnhentingApiScope, 'POST', body);
+    const res = await fetchProxy<BehandleroppslagResponse>(url, dokumentinnhentingApiScope, 'POST', body);
+    return new Response(JSON.stringify(res), { status: 200 });
   } catch (err) {
     logError(`/dokumentinnhenting/behandleroppslag`, err);
     return new Response(JSON.stringify({ message: 'Behandleroppslag feilet' }), { status: 500 });
