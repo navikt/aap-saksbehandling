94f23a22bfed72c7e08203ce3be4cfae9604f647
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-11-14 12:50:06 +0100

    :alien: Legger på typer for brev og logger error dersom brevet ikke finnes
    Co-authored-by: Matias Vinjevoll <Matias.Vinjevoll@nav.no>

--- components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
+++ components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
@@ -1,7 +1,8 @@
 import { Brevbygger } from '@navikt/aap-breveditor/';
+import { Brev } from 'lib/types/types';
 
 import NavLogo from 'public/nav_logo.png';
 
-export const SkriveBrev = ({ grunnlag }: { grunnlag: any }) => {
-  return <Brevbygger brevmal={grunnlag.brevmal} logo={NavLogo} />;
+export const SkriveBrev = ({ grunnlag }: { grunnlag: Brev }) => {
+  return <Brevbygger brevmal={grunnlag} logo={NavLogo} />;
 };
