81453fe1755ec51ff35a174829a8be048dc0d001
Øivind Stensrud <oivind.stensrud@nav.no>
2024-11-18 12:01:38 +0100

    :bug: Gjør response om til en string

--- app/api/dokumentinnhenting/forhaandsvis/route.ts
+++ app/api/dokumentinnhenting/forhaandsvis/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
     const res = await forhåndsvisDialogmelding(body);
-    return new Response(res as string, { status: 200 });
+    return new Response(JSON.stringify(res), { status: 200 });
   } catch (error) {
     logError('Forhåndsvisning av dialogmelding feilet', error);
     return new Response(JSON.stringify({ message: JSON.stringify(error) }), { status: 500 });
