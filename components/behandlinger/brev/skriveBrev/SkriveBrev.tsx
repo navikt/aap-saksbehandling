be99863e96b66c574fa6155a753ee4f10c0a757a
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-11-22 12:19:10 +0100

    :triangular_flag_on_post: Viser alltid breveditor siden vi har lagt til ny brevmal for test
    Co-authored-by: Matias Vinjevoll <Matias.Vinjevoll@nav.no>
    Co-authored-by: Henrik Bugge Halvorsen <Henrik.Bugge.Halvorsen@nav.no>

--- components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
+++ components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
@@ -1,22 +1,10 @@
 'use client';
 
 import { Brevbygger } from '@navikt/aap-breveditor/';
-import { Heading } from '@navikt/ds-react';
 import { Brev } from 'lib/types/types';
-import { useSearchParams } from 'next/navigation';
 
 import NavLogo from 'public/nav_logo.png';
 
 export const SkriveBrev = ({ grunnlag }: { grunnlag: Brev }) => {
-  const visBrev = useSearchParams().get('visBrev') ?? '';
-  if (visBrev != 'enabled') {
-    return (
-      <div>
-        <Heading level="2" size="large">
-          Brev (Ikke implementert)
-        </Heading>
-      </div>
-    );
-  }
   return <Brevbygger brevmal={grunnlag} logo={NavLogo} />;
 };
