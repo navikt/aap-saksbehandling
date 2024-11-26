11facdacb51e5465483af50581fff3096df44d79
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-11-26 14:56:16 +0100

    :sparkles: Viser mer informasjon på Vilkårsoppsummering

--- components/vilkårsoppsummering/VilkårsoppsummeringItem.tsx
+++ components/vilkårsoppsummering/VilkårsoppsummeringItem.tsx
@@ -1,8 +1,9 @@
 import styles from 'components/vilkårsoppsummering/Vilkårsoppsummering.module.css';
-import { Vilkår, VilkårType } from 'lib/types/types';
+import { AvslagÅrsak, Vilkår, VilkårType } from 'lib/types/types';
 import { CheckmarkCircleFillIcon, ExclamationmarkTriangleFillIcon, XMarkOctagonFillIcon } from '@navikt/aksel-icons';
 import { vilkårErOppfylt } from 'components/vilkårsoppsummering/Vilkårsoppsummering';
 import { BodyShort } from '@navikt/ds-react';
+import { formaterDatoForFrontend } from 'lib/utils/date';
 
 interface Props {
   vilkår: Vilkår;
@@ -21,12 +22,47 @@ const vilkarsstatus = (vilkar: Vilkår) => {
 
 export const VilkårsoppsummeringItem = ({ vilkår }: Props) => {
   return (
-    <BodyShort className={styles.vilkårsitem}>
-      {vilkarsstatus(vilkår)}
-      {mapVilkårTypeTilVilkårNavn(vilkår.vilkårtype)}
-    </BodyShort>
+    <div>
+      <BodyShort className={styles.vilkårsitem}>
+        {vilkarsstatus(vilkår)}
+        {mapVilkårTypeTilVilkårNavn(vilkår.vilkårtype)}
+      </BodyShort>
+
+      <div className={'flex-column'}>
+        {vilkår.perioder.map((periode, index) => {
+          if (periode.utfall === 'IKKE_OPPFYLT') {
+            return (
+              <div key={index}>
+                <BodyShort>
+                  Periode: {formaterDatoForFrontend(periode.periode.fom)} -{' '}
+                  {formaterDatoForFrontend(periode.periode.tom)}
+                </BodyShort>
+                <BodyShort>Avslagårsak: {mapAvslagsårsakTilAvslagNavn(periode.avslagsårsak)}</BodyShort>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
   );
 };
+
+function mapAvslagsårsakTilAvslagNavn(avslagÅrsak: AvslagÅrsak) {
+  switch (avslagÅrsak) {
+    case 'BRUKER_OVER_67':
+      return 'Bruker er over 67 år';
+    case 'BRUKER_UNDER_18':
+      return 'Bruker er under 18 år';
+    case 'IKKE_NOK_REDUSERT_ARBEIDSEVNE':
+      return 'Ikke nok redusert arbeidsevne';
+    case 'MANGLENDE_DOKUMENTASJON':
+      return 'Manglende dokumentasjon';
+    case 'IKKE_SYKDOM_AV_VISS_VARIGHET':
+      return 'Ikke sykdom av viss varighet';
+    case 'IKKE_SYKDOM_SKADE_LYTE_VESENTLIGDEL':
+      return 'Sykdom, skade eller lyte er ikke vesentlig medvirkende til nedsatt arbeidsevne';
+  }
+}
 
 function mapVilkårTypeTilVilkårNavn(steg: VilkårType): string {
   switch (steg) {
