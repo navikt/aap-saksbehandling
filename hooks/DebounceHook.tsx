47db635ff85fcba038a1f78ae7431701f299e92c
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-11-27 09:14:57 +0100

    Merge with main

--- hooks/DebounceHook.tsx
+++ hooks/DebounceHook.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 
 export function useDebounce<T>(value: T, delay = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  // @ts-ignore
   const timerRef = useRef<ReturnType<typeof setTimeout>>();
 
   useEffect(() => {
