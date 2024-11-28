0f0b6d9368141a71f5a48d9448060b73c3dc17c3
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-11-28 10:15:22 +0100

    Kommenterer ut forbered behandling

--- app/sak/[saksId]/[behandlingsReferanse]/[aktivGruppe]/page.tsx
+++ app/sak/[saksId]/[behandlingsReferanse]/[aktivGruppe]/page.tsx
@@ -1,19 +1,17 @@
 import { OppgaveKolonne } from 'components/oppgavekolonne/OppgaveKolonne';
 
 import { StegGruppe } from 'lib/types/types';
-import { forberedBehandlingOgVentPåProsessering } from 'lib/services/saksbehandlingservice/saksbehandlingService';
-import { FlytProsesseringAlert } from 'components/flytprosesseringalert/FlytProsesseringAlert';
 
 const Page = async ({
   params,
 }: {
   params: { behandlingsReferanse: string; aktivGruppe: StegGruppe; saksId: string };
 }) => {
-  const forberedBehandlingResponse = await forberedBehandlingOgVentPåProsessering(params.behandlingsReferanse);
-
-  if (forberedBehandlingResponse && forberedBehandlingResponse.status === 'FEILET') {
-    return <FlytProsesseringAlert flytProsessering={forberedBehandlingResponse} />;
-  }
+  // const forberedBehandlingResponse = await forberedBehandlingOgVentPåProsessering(params.behandlingsReferanse);
+  //
+  // if (forberedBehandlingResponse && forberedBehandlingResponse.status === 'FEILET') {
+  //   return <FlytProsesseringAlert flytProsessering={forberedBehandlingResponse} />;
+  // }
 
   return (
     <OppgaveKolonne
