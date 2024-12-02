538d3f46652de9b7c0a95814d5efab489dafaaa4
Øivind Stensrud <oivind.stensrud@nav.no>
2024-12-02 15:39:54 +0100

    :alembic: Test av søkekomponent

---
+++ app/testing/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { FormTesting } from 'components/input/combosearch/ComboSearch2';
+
+const Page = () => {
+  return (
+    <div style={{ margin: '0 auto', maxWidth: '50vw' }}>
+      <FormTesting />
+    </div>
+  );
+};
+
+export default Page;
