9c5f9b2b37cd36c588eb9bb51b9c308be918df58
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-12-03 14:31:14 +0100

    :white_check_mark: Tester på diagnosesøker

---
+++ lib/diagnosesøker/DiagnoseSøker.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { diagnoseSøker } from 'lib/diagnosesøker/DiagnoseSøker';
+
+describe('diagnose søker', () => {
+  it('skal returnere 100 elementer fra ICPC2 dersom det ikke er sendt inn et søk', () => {
+    const resultat = diagnoseSøker('ICPC2', '');
+    expect(resultat.length).toBe(100);
+  });
+
+  it('skal returnere 100 elementer fra ICD10 dersom det ikke er sendt inn et søk', () => {
+    const resultat = diagnoseSøker('ICD10', '');
+    expect(resultat.length).toBe(100);
+  });
+
+  it('skal returnere korrekt resultat når man søker på kode', () => {
+    const resultat = diagnoseSøker('ICD10', 'u071');
+    expect(resultat.length).toBe(1);
+
+    const covid19 = resultat[0];
+    expect(covid19.label).toBe('Covid-19 med påvist virus (U071)');
+    expect(covid19.value).toBe('U071');
+  });
+
+  it('skal returnere et tomt array dersom det ikke finnes et treff', () => {
+    const resultat = diagnoseSøker('ICD10', 'hello pello diagnosen');
+    expect(resultat).toStrictEqual([]);
+  });
+});
