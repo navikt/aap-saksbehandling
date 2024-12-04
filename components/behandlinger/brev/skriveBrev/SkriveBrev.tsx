96ae3c06163dba2f439db555450633433b05dcbd
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-12-04 07:58:32 +0100

    :children_crossing: Enkel visning på når brevet sist ble mellomlagret, spinner som viser at mellomlagring pågår

--- components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
+++ components/behandlinger/brev/skriveBrev/SkriveBrev.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { Brevbygger } from '@navikt/aap-breveditor/';
-import { Button } from '@navikt/ds-react';
+import { Button, Label, Loader } from '@navikt/ds-react';
 import { useBehandlingsReferanse } from 'hooks/BehandlingHook';
 import { useDebounce } from 'hooks/DebounceHook';
 import { useLøsBehovOgGåTilNesteSteg } from 'hooks/LøsBehovOgGåTilNesteStegHook';
 import { clientMellomlagreBrev } from 'lib/clientApi';
 import { Brev } from 'lib/types/types';
+import { formaterDatoMedTidspunktForFrontend } from 'lib/utils/date';
 import { Behovstype } from 'lib/utils/form';
 
 import NavLogo from 'public/nav_logo.png';
 import { useCallback, useEffect, useState } from 'react';
+
+import style from './SkrivBrev.module.css';
 
 export const SkriveBrev = ({
   referanse,
@@ -23,11 +26,19 @@ export const SkriveBrev = ({
 }) => {
   const behandlingsReferanse = useBehandlingsReferanse();
   const [brev, setBrev] = useState<Brev>(grunnlag);
+  const [sistLagret, setSistLagret] = useState<Date | undefined>();
+  const [isSaving, setIsSaving] = useState(false);
 
   const debouncedBrev = useDebounce<Brev>(brev, 2000);
 
   const mellomlagreBackendRequest = useCallback(async () => {
-    await clientMellomlagreBrev(referanse, debouncedBrev);
+    setIsSaving(true);
+    const res = await clientMellomlagreBrev(referanse, debouncedBrev);
+    if (res != undefined) {
+      console.log('res', res);
+      setSistLagret(new Date());
+    }
+    setIsSaving(false);
   }, [debouncedBrev, referanse]);
 
   useEffect(() => {
@@ -42,6 +53,10 @@ export const SkriveBrev = ({
 
   return (
     <div>
+      <div className={style.sistLagret}>
+        {sistLagret && <Label as="p">Sist lagret: {formaterDatoMedTidspunktForFrontend(sistLagret)}</Label>}
+        {isSaving && <Loader />}
+      </div>
       <Brevbygger brevmal={brev} onBrevChange={onChange} logo={NavLogo} />
       <Button
         onClick={() =>
