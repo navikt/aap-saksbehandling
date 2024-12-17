ecd46039cc5ecd23fb3f3cced09a57d2b39e355f
Øivind Stensrud <oivind.stensrud@nav.no>
2024-12-17 14:44:15 +0100

    :white_check_mark: Legger til tester

---
+++ components/innhentdokumentasjon/InnhentDokumentasjon.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import { InnhentDokumentasjon } from 'components/innhentdokumentasjon/InnhentDokumentasjon';
+import { LegeerklæringStatus } from 'lib/types/types';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
+
+const testdata: LegeerklæringStatus[] = [
+  {
+    behandlerRef: '1234',
+    dialogmeldingUuid: 'uuid-1',
+    opprettet: '2024-08-12',
+    personId: '12345678910',
+    saksnummer: 'string',
+    status: 'SENDT',
+    behandlerNavn: 'Trude Lutt',
+    fritekst: 'Fritekst til behandler',
+  },
+];
+
+const fetchMock = createFetchMock(vi);
+fetchMock.enableMocks();
+
+describe('Innhent dokumentasjon', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  test('har en knapp for å åpne skjema for å etterspørre informasjon fra lege', () => {
+    fetchMock.mockResponseOnce(JSON.stringify(testdata), { status: 200 });
+    render(<InnhentDokumentasjon />);
+    expect(screen.getByRole('button', { name: 'Etterspør informasjon fra lege' })).toBeVisible();
+  });
+
+  test('viser en tabell over bestilte erklæringer', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(testdata), { status: 200 });
+    render(<InnhentDokumentasjon />);
+    expect(await screen.findAllByRole('row')).toHaveLength(testdata.length + 1);
+  });
+});
