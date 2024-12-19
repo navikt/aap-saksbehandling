2d3998cc9db1dd8e699b3b67d01ac53359343c34
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-12-19 15:43:00 +0100

    :white_check_mark: Tester for å verifisere at vi formaterer perioder korrekt

---
+++ components/aktivitetsplikt/AktivitetspliktUtil.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { formaterPerioder } from 'components/aktivitetsplikt/AktivitetspliktUtil';
+
+describe('formaterPerioder', () => {
+  it('skal returnere et array med et objekt som inneholder tom og fom dersom det er av type periode', () => {
+    const result = formaterPerioder(
+      [{ type: 'periode', fom: '2024-01-01', tom: '2024-01-02' }],
+      'IKKE_MØTT_TIL_BEHANDLING_ELLER_UTREDNING'
+    );
+
+    expect(result).toStrictEqual([{ fom: '2024-01-01', tom: '2024-01-02' }]);
+  });
+
+  it('skal returnere et array med et objekt som inneholder tom og fom med samme dato dersom det er av type enkeltdato og det ikke er "ikke bidratt til egen avklaring" som brudd ', () => {
+    const result = formaterPerioder(
+      [{ type: 'enkeltdag', dato: '2024-01-01' }],
+      'IKKE_MØTT_TIL_BEHANDLING_ELLER_UTREDNING'
+    );
+
+    expect(result).toStrictEqual([{ fom: '2024-01-01', tom: '2024-01-01' }]);
+  });
+
+  it('skal returnere et array med et objekt som inneholder fom og tom der tom er undefined dersom det er av type enkeltdato og bruddet er "ikke bidratt til egen avklaring" ', () => {
+    const result = formaterPerioder([{ type: 'enkeltdag', dato: '2024-01-01' }], 'IKKE_AKTIVT_BIDRAG');
+
+    expect(result).toStrictEqual([{ fom: '2024-01-01', tom: undefined }]);
+  });
+});
---
+++ components/aktivitetsplikt/AktivitetspliktUtil.ts
@@ -0,0 +1,36 @@
+import { AktivitetspliktBrudd } from 'lib/types/types';
+import { DatoBruddPåAktivitetsplikt } from 'components/aktivitetsplikt/Aktivitetsplikt';
+
+export function hentDatoLabel(valgtBrudd: AktivitetspliktBrudd): string {
+  switch (valgtBrudd) {
+    case 'IKKE_MØTT_TIL_MØTE':
+      return 'Dato for ikke møtt i tiltak';
+    case 'IKKE_MØTT_TIL_BEHANDLING_ELLER_UTREDNING':
+      return 'Dato for ikke møtt til behandling eller utredning';
+    case 'IKKE_MØTT_TIL_TILTAK':
+      return 'Dato for ikke møtt til tiltak';
+    case 'IKKE_MØTT_TIL_ANNEN_AKTIVITET':
+      return 'Dato for ikke møtt til annen aktivitet';
+    case 'IKKE_SENDT_INN_DOKUMENTASJON':
+      return 'Dato for ikke sendt inn dokumentasjon';
+    case 'IKKE_AKTIVT_BIDRAG':
+      return 'Dato for ikke bidratt til egen avklaring';
+    default: {
+      return 'Dato for fravær';
+    }
+  }
+}
+
+export function formaterPerioder(perioder: DatoBruddPåAktivitetsplikt[], brudd: AktivitetspliktBrudd) {
+  return perioder.map((periode) => {
+    if (periode.type === 'enkeltdag') {
+      if (brudd === 'IKKE_AKTIVT_BIDRAG') {
+        return { fom: periode.dato, tom: undefined };
+      } else {
+        return { fom: periode.dato, tom: periode.dato };
+      }
+    } else {
+      return { fom: periode.fom, tom: periode.tom };
+    }
+  });
+}
