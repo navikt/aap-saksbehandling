0b0f738b2ebc72f96aa746513d63bb27c74e3f57
Tor Idland <tor.idland@nav.no>
2025-01-16 20:36:59 +0100

    :recycle: rydder litt i CORS kaoset mitt

--- app/api/sak/finn/route.ts
+++ app/api/sak/finn/route.ts
@@ -3,11 +3,11 @@ import { NextResponse } from 'next/server';
 import { finnSakerForIdent } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { logError } from '@navikt/aap-felles-utils';
 
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://aap-saksbehandling.ansatt.dev.nav.no',
-//   'https://aap-oppgavestyring.ansatt.dev.nav.no',
-//   'https://www.nav.no'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://aap-saksbehandling.ansatt.dev.nav.no',
+  'https://aap-oppgavestyring.ansatt.dev.nav.no',
+  'https://www.nav.no'];
 
 export async function POST(req: Request) {
   const body: FinnSakForIdent = await req.json();
@@ -20,16 +20,15 @@ export async function POST(req: Request) {
     return new Response(JSON.stringify({ message: 'Noe gikk galt' }), { status: 500 });
   }
 
-  return NextResponse.json(data, { status: 200 });
-  // const origin = req.headers.get('Origin') ?? '';
-  // return NextResponse.json(data, {
-  //   headers: {
-  //     'Access-Control-Allow-Methods': `*`,
-  //     'Access-Control-Allow-Headers':
-  //       '*',
-  //     'Access-Control-Allow-Origin': '*',
-  //     'Access-Control-Allow-Credentials': 'true',
-  //   },
-  //   status: 200,
-  // });
+  const origin = req.headers.get('Origin') ?? '';
+  return NextResponse.json(data, {
+    headers: {
+      'Access-Control-Allow-Methods': `POST`,
+      'Access-Control-Allow-Headers':
+        '*',
+      'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : '',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    status: 200,
+  });
 }
--- next.config.js
+++ next.config.js
@@ -19,19 +19,6 @@ const nextConfig = {
 
   images: {
     formats: ['image/webp'],
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/sak/finn",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-        ]
-      }
-    ]
   }
 };
 
