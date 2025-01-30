42470d4c1e24585adb00ad5b4890bd3ff616f546
Øivind Stensrud <oivind.stensrud@nav.no>
2025-01-30 14:27:26 +0100

    :sparkles: Lenke tilbake til sakssiden

--- components/saksinfo/SaksInfo.tsx
+++ components/saksinfo/SaksInfo.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort, Label, Link } from '@navikt/ds-react';
 import { SakPersoninfo, SaksInfo as SaksInfoType } from 'lib/types/types';
 import styles from './SaksInfo.module.css';
 
@@ -10,7 +10,11 @@ export const SaksInfo = ({ personInformasjon, sak }: Props) => {
   return (
     <div className={styles.søkerinfo}>
       <div className={styles.ikon} />
-      <Label size="small">{personInformasjon.navn}</Label>
+      <Label size="small">
+        <Link href={`/sak/${sak.saksnummer}`} title="Tilbake til K-Hub">
+          {personInformasjon.navn}
+        </Link>
+      </Label>
       <span aria-hidden>/</span>
       <BodyShort>{sak?.ident}</BodyShort>
     </div>
