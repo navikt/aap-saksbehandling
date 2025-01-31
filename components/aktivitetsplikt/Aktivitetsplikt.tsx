1e22af8137fddad0efe61a5f5ff1fcb3f37baa21
Øivind Stensrud <oivind.stensrud@nav.no>
2025-01-31 14:43:41 +0100

    :recycle: Flytter registrerte brudd til egen boks

--- components/aktivitetsplikt/Aktivitetsplikt.tsx
+++ components/aktivitetsplikt/Aktivitetsplikt.tsx
@@ -19,18 +19,25 @@ export const Aktivitetsplikt = ({ aktivitetspliktHendelser, sak }: Props) => {
   const [skalRegistrereBrudd, setSkalRegistrereBrudd] = useState(false);
 
   return (
-    <SideProsessKort
-      heading={'Registrer fravær eller brudd på aktivitetsplikten'}
-      icon={<FigureIcon fontSize={'inherit'} aria-hidden />}
-    >
-      <div className={'flex-column'}>
-        {skalRegistrereBrudd ? (
-          <AktivitetspliktForm sak={sak} setSkalRegistrereBrudd={setSkalRegistrereBrudd} />
-        ) : (
-          <Button onClick={() => setSkalRegistrereBrudd(true)} className={'fit-content'}>
-            Registrer fravær eller brudd
-          </Button>
-        )}
+    <>
+      <SideProsessKort
+        heading={'Registrer fravær eller brudd på aktivitetsplikten'}
+        icon={<FigureIcon fontSize={'inherit'} aria-hidden />}
+      >
+        <div className={'flex-column'}>
+          {skalRegistrereBrudd ? (
+            <AktivitetspliktForm sak={sak} setSkalRegistrereBrudd={setSkalRegistrereBrudd} />
+          ) : (
+            <Button onClick={() => setSkalRegistrereBrudd(true)} className={'fit-content'}>
+              Registrer fravær eller brudd
+            </Button>
+          )}
+        </div>
+      </SideProsessKort>
+      <SideProsessKort
+        heading="Registrerte brudd på aktivitetsplikten"
+        icon={<FigureIcon fontSize={'inherit'} aria-hidden />}
+      >
         <AktivitetspliktHendelser
           hendelser={aktivitetspliktHendelser.map((hendelse) => {
             return {
@@ -39,7 +46,7 @@ export const Aktivitetsplikt = ({ aktivitetspliktHendelser, sak }: Props) => {
             };
           })}
         />
-      </div>
-    </SideProsessKort>
+      </SideProsessKort>
+    </>
   );
 };
