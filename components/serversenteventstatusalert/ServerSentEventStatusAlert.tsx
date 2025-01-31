c5f989816593ef37662cfd14b5f3c4cb8f866476
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-01-31 12:56:54 +0100

    :sparkles: Legger ved saksId og behaldingsreferanse i ServerSentEventStatusAlert

--- components/serversenteventstatusalert/ServerSentEventStatusAlert.tsx
+++ components/serversenteventstatusalert/ServerSentEventStatusAlert.tsx
@@ -1,18 +1,40 @@
 import { ServerSentEventStatus } from 'app/api/behandling/hent/[referanse]/[gruppe]/[steg]/nesteSteg/route';
-import { Alert } from '@navikt/ds-react';
+import { Alert, BodyShort } from '@navikt/ds-react';
+import { useParams } from 'next/navigation';
 
 interface Props {
   status?: ServerSentEventStatus;
 }
 
 export const ServerSentEventStatusAlert = ({ status }: Props) => {
+  const { behandlingsReferanse, saksId } = useParams<{ behandlingsReferanse: string; saksId: string }>();
   return (
     <>
       {status === 'ERROR' && (
-        <Alert variant="error">Det tok for lang tid å hente neste steg fra baksystemet. Kom tilbake senere..️</Alert>
+        <Alert variant="error">
+          <BodyShort spacing>Det tok for lang tid å hente neste steg fra baksystemet. Kom tilbake senere.</BodyShort>
+          <BodyShort size={'small'}>
+            <b>SakId:</b>
+            {` ${saksId}`}
+          </BodyShort>
+          <BodyShort size={'small'}>
+            <b>Behandlingsreferanse:</b>
+            {` ${behandlingsReferanse}`}
+          </BodyShort>
+        </Alert>
       )}
       {status === 'POLLING' && (
-        <Alert variant="info">Maskinen bruker litt lengre tid på å jobbe enn vanlig. Ta deg en kopp kaffe.</Alert>
+        <Alert variant="info">
+          <BodyShort spacing>Maskinen bruker litt lengre tid på å jobbe enn vanlig. Ta deg en kopp kaffe.</BodyShort>
+          <BodyShort size={'small'}>
+            <b>SakId:</b>
+            {` ${saksId}`}
+          </BodyShort>
+          <BodyShort size={'small'}>
+            <b>Behandlingsreferanse:</b>
+            {` ${behandlingsReferanse}`}
+          </BodyShort>
+        </Alert>
       )}
     </>
   );
