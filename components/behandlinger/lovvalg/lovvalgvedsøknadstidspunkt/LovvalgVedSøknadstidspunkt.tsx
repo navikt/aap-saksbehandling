614a2bff54602091f53d52ec12a09a715a8baba4
Tor Idland <tor.idland@nav.no>
2025-01-31 12:35:26 +0100

    :sparkles: riktige land med avtale i lovvalg landvelger

--- components/behandlinger/lovvalg/lovvalgvedsøknadstidspunkt/LovvalgVedSøknadstidspunkt.tsx
+++ components/behandlinger/lovvalg/lovvalgvedsøknadstidspunkt/LovvalgVedSøknadstidspunkt.tsx
@@ -1,9 +1,10 @@
-'use client'
+'use client';
 
-import {VilkårsKort} from "components/vilkårskort/VilkårsKort";
-import {FormField, useConfigForm} from "@navikt/aap-felles-react";
-import {Form} from "components/form/Form";
-import {useLøsBehovOgGåTilNesteSteg} from "hooks/LøsBehovOgGåTilNesteStegHook";
+import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
+import { FormField, useConfigForm } from '@navikt/aap-felles-react';
+import { Form } from 'components/form/Form';
+import { useLøsBehovOgGåTilNesteSteg } from 'hooks/LøsBehovOgGåTilNesteStegHook';
+import { landMedTrygdesamarbeid } from 'lib/utils/countries';
 
 interface Props {
   behandlingVersjon: number;
@@ -16,37 +17,45 @@ interface FormFields {
   lovvalgsLand: string;
   annetLovvalgslandMedAvtale?: string;
 }
-export const LovvalgVedSøknadstidspunkt = ({ readOnly}: Props) => {
+export const LovvalgVedSøknadstidspunkt = ({ readOnly }: Props) => {
   const { isLoading, status } = useLøsBehovOgGåTilNesteSteg('VURDER_LOVVALG');
-  const {form, formFields} = useConfigForm<FormFields>({
-    begrunnelse: {
-      type: 'textarea',
-      label: 'Vurder riktig lovvalg ved søknadstidspunkt',
-      rules: {required: 'Du må gi en begrunnelse på lovvalg ved søknadstidspunkt'},
+  const { form, formFields } = useConfigForm<FormFields>(
+    {
+      begrunnelse: {
+        type: 'textarea',
+        label: 'Vurder riktig lovvalg ved søknadstidspunkt',
+        rules: { required: 'Du må gi en begrunnelse på lovvalg ved søknadstidspunkt' },
+      },
+      lovvalgsLand: {
+        type: 'radio',
+        label: 'Hva er riktig lovvalgsland ved søknadstidspunkt?',
+        options: ['Norge', 'Annet land med avtale', 'Land uten avtale'],
+        rules: { required: 'Du må velge riktig lovvalg ved søknadstidspunkt' },
+      },
+      annetLovvalgslandMedAvtale: {
+        type: 'select',
+        label: 'Velg land som vi vurderer som lovvalgsland',
+        options: landMedTrygdesamarbeid,
+      },
     },
-    lovvalgsLand: {
-      type: 'radio',
-      label: 'Hva er riktig lovvalgsland ved søknadstidspunkt?',
-      options: ['Norge', 'Annet land med avtale', 'Land uten avtale'],
-      rules: {required: 'Du må velge riktig lovvalg ved søknadstidspunkt'},
-    },
-    annetLovvalgslandMedAvtale: {
-      type: 'select',
-      label: 'Velg land som vi vurderer som lovvalgsland',
-      options: ['Brasil', 'New Zealand']
-    }
-  }, {readOnly})
+    { readOnly }
+  );
 
-  const lovvalgsLand = form.watch('lovvalgsLand')
-
+  const lovvalgsLand = form.watch('lovvalgsLand');
   return (
     <VilkårsKort heading={'Lovvalg ved søknadstidspunkt'} steg={'VURDER_LOVVALG'}>
-      <Form steg={'VURDER_LOVVALG'} onSubmit={form.handleSubmit(data => console.log(data))} isLoading={isLoading} status={status}>
+      <Form
+        steg={'VURDER_LOVVALG'}
+        onSubmit={form.handleSubmit((data) => console.log(data))}
+        isLoading={isLoading}
+        status={status}
+      >
         <FormField form={form} formField={formFields.begrunnelse} />
         <FormField form={form} formField={formFields.lovvalgsLand} />
-        {lovvalgsLand === 'Annet land med avtale' &&
-            <FormField form={form} formField={formFields.annetLovvalgslandMedAvtale} />}
+        {lovvalgsLand === 'Annet land med avtale' && (
+          <FormField form={form} formField={formFields.annetLovvalgslandMedAvtale} />
+        )}
       </Form>
     </VilkårsKort>
-  )
-}
+  );
+};
---
+++ lib/utils/countries.ts
@@ -0,0 +1,20 @@
+import countries from 'i18n-iso-countries';
+import { ValuePair } from '@navikt/aap-felles-react';
+const { eeaMember } = require('is-european');
+countries.registerLocale(require('i18n-iso-countries/langs/no.json'));
+
+const countriesAlpha3: Record<string, string> = countries.getAlpha3Codes();
+const countryList = Object.entries(countriesAlpha3).map(([key, value]) => ({ alpha2: value, alpha3: key }));
+const eeaCountries: ValuePair[] = countryList
+  .filter((country) => eeaMember(country.alpha2))
+  .map((country) => ({
+    value: country.alpha3,
+    label: countries.getName(country.alpha3, 'no', { select: 'official' }) || '',
+  }));
+
+export const landMedTrygdesamarbeid: ValuePair[] = [
+  ...eeaCountries,
+  { value: 'CHE', label: countries.getName('CHE', 'no', { select: 'official' }) || '' },
+  { value: 'AUS', label: countries.getName('AUS', 'no', { select: 'official' }) || '' },
+  { value: 'GBR', label: countries.getName('GBR', 'no', { select: 'official' }) || '' },
+];
