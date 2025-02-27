a6c2201b6380dff3e2debceaea62a2428522e483
Tor Idland <tor.idland@nav.no>
2025-02-27 21:36:33 +0100

    :white_check_mark: test automatiske vurderinger i lovvalgmedlemskap og forutgående

---
+++ components/behandlinger/forutgåendemedlemskap/automatiskvurderingforutgåendemedlemskap/AutomatiskVurderingForutgåendeMedlemskap.test.tsx
@@ -0,0 +1,43 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { AutomatiskLovvalgOgMedlemskapVurdering } from 'lib/types/types';
+import { AutomatiskVurderingForutgåendeMedlemskap } from 'components/behandlinger/forutgåendemedlemskap/automatiskvurderingforutgåendemedlemskap/AutomatiskVurderingForutgåendeMedlemskap';
+
+const vurdering: AutomatiskLovvalgOgMedlemskapVurdering = {
+  kanBehandlesAutomatisk: true,
+  tilhørighetVurdering: [
+    {
+      indikasjon: 'I_NORGE',
+      kilde: ['SØKNAD'],
+      opplysning: 'opplysning',
+      resultat: true,
+    },
+  ],
+};
+describe('Automatisk vurdering av forutgående medlemskap', () => {
+  it('Skal vise overstyringsknapp', () => {
+    render(
+      <AutomatiskVurderingForutgåendeMedlemskap
+        vurdering={vurdering}
+        setOverstyring={() => {}}
+        visOverstyrKnapp={true}
+        visOverstyringsBehov={false}
+      />
+    );
+    const button = screen.getByText('Overstyr');
+    expect(button).toBeVisible();
+  });
+
+  it('Skal vise angre overstyringsknapp hvis det allerede er trykket overstyr', () => {
+    render(
+      <AutomatiskVurderingForutgåendeMedlemskap
+        vurdering={vurdering}
+        setOverstyring={() => {}}
+        visOverstyrKnapp={true}
+        visOverstyringsBehov={true}
+      />
+    );
+    const button = screen.getByText('Angre overstyring');
+    expect(button).toBeVisible();
+  });
+});
---
+++ components/behandlinger/lovvalg/automatiskvurderingavlovvalgogmedlemskap/AutomatiskVurderingAvLovvalgOgMedlemskap.test.tsx
@@ -0,0 +1,43 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { AutomatiskLovvalgOgMedlemskapVurdering } from 'lib/types/types';
+import { AutomatiskVurderingAvLovvalgOgMedlemskap } from 'components/behandlinger/lovvalg/automatiskvurderingavlovvalgogmedlemskap/AutomatiskVurderingAvLovvalgOgMedlemskap';
+
+const vurdering: AutomatiskLovvalgOgMedlemskapVurdering = {
+  kanBehandlesAutomatisk: true,
+  tilhørighetVurdering: [
+    {
+      indikasjon: 'I_NORGE',
+      kilde: ['SØKNAD'],
+      opplysning: 'opplysning',
+      resultat: true,
+    },
+  ],
+};
+describe('Automatisk vurdering av lovvalg og medlemskap', () => {
+  it('Skal vise overstyringsknapp', () => {
+    render(
+      <AutomatiskVurderingAvLovvalgOgMedlemskap
+        vurdering={vurdering}
+        setOverstyring={() => {}}
+        visOverstyrKnapp={true}
+        visOverstyringsBehov={false}
+      />
+    );
+    const button = screen.getByText('Overstyr');
+    expect(button).toBeVisible();
+  });
+
+  it('Skal vise angre overstyringsknapp hvis det allerede er trykket overstyr', () => {
+    render(
+      <AutomatiskVurderingAvLovvalgOgMedlemskap
+        vurdering={vurdering}
+        setOverstyring={() => {}}
+        visOverstyrKnapp={true}
+        visOverstyringsBehov={true}
+      />
+    );
+    const button = screen.getByText('Angre overstyring');
+    expect(button).toBeVisible();
+  });
+});
