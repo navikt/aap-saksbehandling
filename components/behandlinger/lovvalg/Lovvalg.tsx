67c938b0f84baa9d0d11b6649daabcd08d5352dc
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-05 12:45:19 +0100

    mock lovvalg til backend blir fikset

--- components/behandlinger/lovvalg/Lovvalg.tsx
+++ components/behandlinger/lovvalg/Lovvalg.tsx
@@ -1,11 +1,10 @@
 import { GruppeSteg } from 'components/gruppesteg/GruppeSteg';
-import {
-  hentAutomatiskLovvalgOgMedlemskapVurdering,
-  hentFlyt,
-} from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { hentFlyt } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { getStegSomSkalVises } from 'lib/utils/steg';
 import { LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringsWrapper } from 'components/behandlinger/lovvalg/LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringswrapper';
 import { LovvalgOgMedlemskapVedSKnadstidspunktMedDatafetching } from 'components/behandlinger/lovvalg/lovvalgogmedlemskapvedsøknadstidspunkt/LovvalgOgMedlemskapVedSøknadstidspunktMedDatafetching';
+import { AutomatiskLovvalgOgMedlemskapVurdering } from 'lib/types/types';
+
 interface Props {
   behandlingsReferanse: string;
   sakId: string;
@@ -15,8 +14,19 @@ export const Lovvalg = async ({ behandlingsReferanse }: Props) => {
   const behandlingsVersjon = flyt.behandlingVersjon;
   const stegSomSkalVises = getStegSomSkalVises('LOVVALG', flyt);
   const saksBehandlerReadOnly = flyt.visning.saksbehandlerReadOnly;
-  const vurderingAutomatisk = await hentAutomatiskLovvalgOgMedlemskapVurdering(behandlingsReferanse);
-  const visOverstyrKnapp = vurderingAutomatisk.kanBehandlesAutomatisk && stegSomSkalVises.length === 0;
+  // const vurderingAutomatisk = await hentAutomatiskLovvalgOgMedlemskapVurdering(behandlingsReferanse);
+  // const visOverstyrKnapp = vurderingAutomatisk.kanBehandlesAutomatisk && stegSomSkalVises.length === 0;
+  const vurdering: AutomatiskLovvalgOgMedlemskapVurdering = {
+    kanBehandlesAutomatisk: true,
+    tilhørighetVurdering: [
+      {
+        indikasjon: 'I_NORGE',
+        kilde: ['SØKNAD'],
+        opplysning: 'opplysning',
+        resultat: true,
+      },
+    ],
+  };
 
   return (
     <GruppeSteg
@@ -26,12 +36,12 @@ export const Lovvalg = async ({ behandlingsReferanse }: Props) => {
       behandlingVersjon={behandlingsVersjon}
     >
       <LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringsWrapper
-        automatiskVurdering={vurderingAutomatisk}
+        automatiskVurdering={vurdering}
         stegSomSkalVises={stegSomSkalVises}
         behandlingsReferanse={behandlingsReferanse}
         behandlingVersjon={behandlingsVersjon}
         readOnly={saksBehandlerReadOnly}
-        visOverstyrKnapp={visOverstyrKnapp}
+        visOverstyrKnapp={false}
       >
         {stegSomSkalVises.includes('VURDER_LOVVALG') && (
           <LovvalgOgMedlemskapVedSKnadstidspunktMedDatafetching
