bf2714820d494839ce5b62a3a580d265457ded84
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-05 12:59:54 +0100

    Revert "mock lovvalg til backend blir fikset"
    This reverts commit 67c938b0f84baa9d0d11b6649daabcd08d5352dc.

--- components/behandlinger/lovvalg/Lovvalg.tsx
+++ components/behandlinger/lovvalg/Lovvalg.tsx
@@ -1,10 +1,11 @@
 import { GruppeSteg } from 'components/gruppesteg/GruppeSteg';
-import { hentFlyt } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import {
+  hentAutomatiskLovvalgOgMedlemskapVurdering,
+  hentFlyt,
+} from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { getStegSomSkalVises } from 'lib/utils/steg';
 import { LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringsWrapper } from 'components/behandlinger/lovvalg/LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringswrapper';
 import { LovvalgOgMedlemskapVedSKnadstidspunktMedDatafetching } from 'components/behandlinger/lovvalg/lovvalgogmedlemskapvedsøknadstidspunkt/LovvalgOgMedlemskapVedSøknadstidspunktMedDatafetching';
-import { AutomatiskLovvalgOgMedlemskapVurdering } from 'lib/types/types';
-
 interface Props {
   behandlingsReferanse: string;
   sakId: string;
@@ -14,19 +15,8 @@ export const Lovvalg = async ({ behandlingsReferanse }: Props) => {
   const behandlingsVersjon = flyt.behandlingVersjon;
   const stegSomSkalVises = getStegSomSkalVises('LOVVALG', flyt);
   const saksBehandlerReadOnly = flyt.visning.saksbehandlerReadOnly;
-  // const vurderingAutomatisk = await hentAutomatiskLovvalgOgMedlemskapVurdering(behandlingsReferanse);
-  // const visOverstyrKnapp = vurderingAutomatisk.kanBehandlesAutomatisk && stegSomSkalVises.length === 0;
-  const vurdering: AutomatiskLovvalgOgMedlemskapVurdering = {
-    kanBehandlesAutomatisk: true,
-    tilhørighetVurdering: [
-      {
-        indikasjon: 'I_NORGE',
-        kilde: ['SØKNAD'],
-        opplysning: 'opplysning',
-        resultat: true,
-      },
-    ],
-  };
+  const vurderingAutomatisk = await hentAutomatiskLovvalgOgMedlemskapVurdering(behandlingsReferanse);
+  const visOverstyrKnapp = vurderingAutomatisk.kanBehandlesAutomatisk && stegSomSkalVises.length === 0;
 
   return (
     <GruppeSteg
@@ -36,12 +26,12 @@ export const Lovvalg = async ({ behandlingsReferanse }: Props) => {
       behandlingVersjon={behandlingsVersjon}
     >
       <LovvalgOgMedlemskapVedSøknadsTidspunktOverstyringsWrapper
-        automatiskVurdering={vurdering}
+        automatiskVurdering={vurderingAutomatisk}
         stegSomSkalVises={stegSomSkalVises}
         behandlingsReferanse={behandlingsReferanse}
         behandlingVersjon={behandlingsVersjon}
         readOnly={saksBehandlerReadOnly}
-        visOverstyrKnapp={false}
+        visOverstyrKnapp={visOverstyrKnapp}
       >
         {stegSomSkalVises.includes('VURDER_LOVVALG') && (
           <LovvalgOgMedlemskapVedSKnadstidspunktMedDatafetching
