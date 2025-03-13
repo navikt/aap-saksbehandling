cee9e996f7cd632e77e0b9c829710b2bdabc7d9f
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-03-13 15:46:58 +0100

    Fikset sortering og eskludering av Norge på lovvalg på land

--- lib/utils/countries.ts
+++ lib/utils/countries.ts
@@ -5,6 +5,7 @@ countries.registerLocale(require('i18n-iso-countries/langs/no.json'));
 
 const countriesAlpha3: Record<string, string> = countries.getAlpha3Codes();
 const countryList = Object.entries(countriesAlpha3).map(([key, value]) => ({ alpha2: value, alpha3: key }));
+console.log(countryList)
 const eeaCountries: ValuePair[] = countryList
   .filter((country) => eeaMember(country.alpha2))
   .map((country) => ({
@@ -12,10 +13,12 @@ const eeaCountries: ValuePair[] = countryList
     label: countries.getName(country.alpha3, 'no', { select: 'official' }) || '',
   }));
 
+const filteredEeaCountries = eeaCountries.filter(country => country.value !== 'NOR');
+
 export const landMedTrygdesamarbeid: ValuePair[] = [
   { value: '', label: '' },
-  ...eeaCountries,
+  ...filteredEeaCountries,
   { value: 'CHE', label: countries.getName('CHE', 'no', { select: 'official' }) || '' },
   { value: 'AUS', label: countries.getName('AUS', 'no', { select: 'official' }) || '' },
   { value: 'GBR', label: countries.getName('GBR', 'no', { select: 'official' }) || '' },
-];
+].sort((code, name) => code.label.localeCompare(name.label, 'no'));
