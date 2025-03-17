5b9cc386407674f064ac0b2520901cfdf064d835
Tor Idland <tor.idland@nav.no>
2025-03-17 08:47:22 +0100

    :fire: fjern Køer tab fra produksjonsstyring

--- app/oppgave/produksjonsstyring/page.tsx
+++ app/oppgave/produksjonsstyring/page.tsx
@@ -1,19 +1,15 @@
-import { hentKøer } from 'lib/services/oppgaveservice/oppgaveservice';
 import styles from './page.module.css';
 import { Produksjonsstyringsmeny } from 'components/produksjonsstyring/produksjonsstyringsmeny/Produksjonsstyringsmeny';
 import { TotaloversiktBehandlinger } from 'components/produksjonsstyring/totaloversiktbehandlinger/TotaloversiktBehandlinger';
 import { MinEnhet } from 'components/produksjonsstyring/minenhet/MinEnhet';
-import { KøOversikt } from 'components/oppgave/køoversikt/KøOversikt';
 
 export default async function Home() {
-  const køer = await hentKøer();
   return (
     <div className={styles.page}>
       <Produksjonsstyringsmeny
         totaloversikt={<TotaloversiktBehandlinger />}
         minenhet={<MinEnhet />}
         produktivitet={<></>}
-        oppgaveOversikt={<KøOversikt køer={køer} />}
       />
     </div>
   );
--- components/produksjonsstyring/produksjonsstyringsmeny/Produksjonsstyringsmeny.tsx
+++ components/produksjonsstyring/produksjonsstyringsmeny/Produksjonsstyringsmeny.tsx
@@ -9,24 +9,17 @@ interface Props {
   totaloversikt: ReactNode;
   minenhet: ReactNode;
   produktivitet: ReactNode;
-  oppgaveOversikt: ReactNode;
 }
-export const Produksjonsstyringsmeny = ({ totaloversikt, minenhet, oppgaveOversikt }: Props) => {
+export const Produksjonsstyringsmeny = ({ totaloversikt, minenhet }: Props) => {
   return (
     <AlleFiltereProvider>
       <Tabs defaultValue="total" fill className={styles.produksjonsstyringsmeny}>
         <Tabs.List>
           <Tabs.Tab value="total" label="Totaloversikt" />
           <Tabs.Tab value="min-enhet" label="Min enhet" />
-          <Tabs.Tab value="køer" label="Køer" />
-          {/*<Tabs.Tab value="produktivitet" label="Produktivitet" />*/}
-          {/*<Tabs.Tab value="oppgaver" label="Oppgaver" />*/}
         </Tabs.List>
         <Tabs.Panel value="total">{totaloversikt}</Tabs.Panel>
         <Tabs.Panel value="min-enhet">{minenhet}</Tabs.Panel>
-        <Tabs.Panel value="køer">{oppgaveOversikt}</Tabs.Panel>
-        {/*<Tabs.Panel value="produktivitet">{produktivitet}</Tabs.Panel>*/}
-        {/*<Tabs.Panel value="oppgaver">{oppgaver}</Tabs.Panel>*/}
       </Tabs>
     </AlleFiltereProvider>
   );
