39e8e33e4975c9aa0c9160d2f862ca9bd7e1316f
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-18 21:42:42 +0100

    Fjerner kall

--- components/gruppesteg/GruppeSteg.tsx
+++ components/gruppesteg/GruppeSteg.tsx
@@ -3,14 +3,11 @@ import { BehandlingPåVentKortMedDataFetching } from 'components/sideprosesser/B
 import { FlytProsesseringAlert } from 'components/flytprosesseringalert/FlytProsesseringAlert';
 import { FlytProsessering, FlytVisning, StegGruppe } from 'lib/types/types';
 import { BrevKortMedDataFetching } from 'components/brev/BrevKortMedDataFetching';
-import { Alert } from '@navikt/ds-react';
-import { sjekkTilgang } from 'lib/services/tilgangservice/tilgangsService';
 
 interface Props {
   prosessering: FlytProsessering;
   visning: FlytVisning;
   behandlingReferanse: string;
-  avklaringsbehovKode: string;
   behandlingVersjon: number;
   aktivGruppe?: StegGruppe;
   children: ReactNode;
@@ -20,16 +17,12 @@ export const GruppeSteg = async ({
   children,
   visning,
   behandlingReferanse,
-  avklaringsbehovKode,
   behandlingVersjon,
   aktivGruppe,
   prosessering,
 }: Props) => {
-  const harTilgang = await sjekkTilgang(behandlingReferanse, avklaringsbehovKode);
-  console.log(harTilgang);
   return (
     <div className={'flex-column'}>
-      {!harTilgang.tilgang && <Alert variant={'error'}>NEINEIHENEI</Alert>}
       {prosessering.status === 'FEILET' && <FlytProsesseringAlert flytProsessering={prosessering} />}
       {visning.visVentekort && (
         <BehandlingPåVentKortMedDataFetching
