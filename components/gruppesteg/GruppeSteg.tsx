f0f7066277846a50ae054edaed90ebdefd702239
Tor Idland <tor.idland@nav.no>
2025-03-18 14:38:59 +0100

    legg til tilgangservice og sjekkTilgang endepunkt
    Co-authored-by: Thomas Rognes <thomas.rognes@nav.no>

--- components/gruppesteg/GruppeSteg.tsx
+++ components/gruppesteg/GruppeSteg.tsx
@@ -3,26 +3,33 @@ import { BehandlingPåVentKortMedDataFetching } from 'components/sideprosesser/B
 import { FlytProsesseringAlert } from 'components/flytprosesseringalert/FlytProsesseringAlert';
 import { FlytProsessering, FlytVisning, StegGruppe } from 'lib/types/types';
 import { BrevKortMedDataFetching } from 'components/brev/BrevKortMedDataFetching';
+import { Alert } from '@navikt/ds-react';
+import { sjekkTilgang } from 'lib/services/tilgangservice/tilgangsService';
 
 interface Props {
   prosessering: FlytProsessering;
   visning: FlytVisning;
   behandlingReferanse: string;
+  avklaringsbehovKode: string;
   behandlingVersjon: number;
   aktivGruppe?: StegGruppe;
   children: ReactNode;
 }
 
-export const GruppeSteg = ({
+export const GruppeSteg = async ({
   children,
   visning,
   behandlingReferanse,
+  avklaringsbehovKode,
   behandlingVersjon,
   aktivGruppe,
   prosessering,
 }: Props) => {
+  const harTilgang = await sjekkTilgang(behandlingReferanse, avklaringsbehovKode);
+  console.log(harTilgang);
   return (
     <div className={'flex-column'}>
+      {!harTilgang.tilgang && <Alert variant={'error'}>NEINEIHENEI</Alert>}
       {prosessering.status === 'FEILET' && <FlytProsesseringAlert flytProsessering={prosessering} />}
       {visning.visVentekort && (
         <BehandlingPåVentKortMedDataFetching
