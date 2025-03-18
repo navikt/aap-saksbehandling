2d5114d1bf2dff511866815213ef768f5e6f171f
Øivind Stensrud <oivind.stensrud@nav.no>
2025-03-18 21:43:49 +0100

    :recycle: Redirect til oppgave som default
    Saksoversikten er kun til bruk under utvikling og skal ikke brukes ved
    testing av systemet ol. Endrer derfor redirect til oppgave.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { isLocal } from 'lib/utils/environment';
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-  redirect('/saksbehandling/saksoversikt');
+  if (!isLocal()) {
+    redirect('/saksbehandling/saksoversikt');
+  }
+  redirect('/oppgave');
 }
