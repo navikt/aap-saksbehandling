95cd011ad09090b525dc6812c19ab57a8036d793
Øivind Stensrud <oivind.stensrud@nav.no>
2025-03-19 18:38:43 +0100

    :lipstick: Fjern gammel skeleton-styling

--- components/stegsuspense/StegSuspense.tsx
+++ components/stegsuspense/StegSuspense.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Skeleton } from '@navikt/ds-react';
-import styles from 'components/stegsuspense/StegSuspense.module.css';
 import { ReactNode, Suspense } from 'react';
 
 interface Props {
@@ -11,8 +10,8 @@ export const StegSuspense = ({ children }: Props) => (
   <Suspense
     fallback={
       <div>
-        <Skeleton width={'100%'} height={56} variant={'rectangle'} className={styles.skeletonHeader} />
-        <Skeleton width={'100%'} height={180} variant={'rectangle'} className={styles.skeletonBody} />
+        <Skeleton width={'100%'} height={56} variant={'rectangle'} />
+        <Skeleton width={'100%'} height={180} variant={'rectangle'} />
       </div>
     }
   >
