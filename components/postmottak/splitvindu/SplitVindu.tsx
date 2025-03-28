3e3cec731c4568e06f4a4504dc8cb6e911e2df87
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-28 14:25:06 +0100

    padding på splitvindu

--- components/postmottak/splitvindu/SplitVindu.tsx
+++ components/postmottak/splitvindu/SplitVindu.tsx
@@ -15,7 +15,7 @@ export const SplitVindu = ({ children, journalpostId, dokumenter }: Props) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   return (
-    <HGrid padding={'4'} columns={isExpanded ? '1fr 2fr' : '1fr 1fr'} gap={'4'} className={styles.splitVindu}>
+    <HGrid columns={isExpanded ? '1fr 2fr' : '1fr 1fr'} gap={'4'} className={styles.splitVindu}>
       {children}
 
       <Dokumentvisning
