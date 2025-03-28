7c122903352dd49788b0e5557a03980070b42374
Tor Idland <tor.idland@nav.no>
2025-03-28 13:08:16 +0100

    :bug: ikke kall hentallesaker i prod

--- app/saksbehandling/saksoversikt/page.tsx
+++ app/saksbehandling/saksoversikt/page.tsx
@@ -1,13 +1,16 @@
 import { hentAlleSaker } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 
 import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
-
+import { SaksInfo } from 'lib/types/types';
 import styles from './page.module.css';
 import { isLocal, isProd } from 'lib/utils/environment';
 import { OpprettSak } from 'components/opprettsak/OpprettSak';
 
 const Page = async () => {
-  const alleSaker = await hentAlleSaker();
+  let alleSaker: SaksInfo[] = [];
+  if (!isProd()) {
+    alleSaker = await hentAlleSaker();
+  }
   return (
     <main className={styles.main}>
       {isLocal() && <OpprettSak />}
