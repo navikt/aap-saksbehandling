5534f9aec8976c72354db51b769e355528b8bd34
Tor Idland <tor.idland@nav.no>
2025-03-28 14:31:50 +0100

    :bug: redirect til oppgaveliste når vi ikke er lokalt

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { isLocal } from 'lib/utils/environment';
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-  if (!isLocal()) {
+  if (isLocal()) {
     redirect('/saksbehandling/saksoversikt');
   }
   redirect('/oppgave');
