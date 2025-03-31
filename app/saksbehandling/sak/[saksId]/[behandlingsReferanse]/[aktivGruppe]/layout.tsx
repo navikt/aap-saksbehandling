1dff9c097d40c3fa7ab1730ee0bd231f6529291c
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-31 15:38:23 +0200

    Viser en melding om at steget ikke er vurdert derosm bruker navigerer til et steg fram i flyten

--- app/saksbehandling/sak/[saksId]/[behandlingsReferanse]/[aktivGruppe]/layout.tsx
+++ app/saksbehandling/sak/[saksId]/[behandlingsReferanse]/[aktivGruppe]/layout.tsx
@@ -1,27 +1,21 @@
 import { hentFlyt } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { ReactNode, Suspense } from 'react';
-import { notFound } from 'next/navigation';
 import { StegGruppe } from 'lib/types/types';
-import { Skeleton } from '@navikt/ds-react';
+import { BodyShort, Skeleton } from '@navikt/ds-react';
 
-const Layout = async (
-  props: {
-    params: Promise<{ behandlingsReferanse: string; aktivGruppe: string }>;
-    children: ReactNode;
-  }
-) => {
+const Layout = async (props: {
+  params: Promise<{ behandlingsReferanse: string; aktivGruppe: string }>;
+  children: ReactNode;
+}) => {
   const params = await props.params;
 
-  const {
-    children
-  } = props;
+  const { children } = props;
 
   const flytResponse = await hentFlyt(params.behandlingsReferanse);
   const ferdigeSteg = flytResponse.flyt.filter((steg) => steg.erFullført).map((steg) => steg.stegGruppe);
 
   if (!ferdigeSteg.includes(params.aktivGruppe as StegGruppe) && flytResponse.aktivGruppe != params.aktivGruppe) {
-    // TODO: Undersøke hva brukere ønsker dersom de går til en gruppe de ikke kan vurdere enda
-    notFound();
+    return <BodyShort>Dette steget er ikke vurdert enda.</BodyShort>;
   }
 
   return <Suspense fallback={<Skeleton variant="rectangle" height={'40vh'} />}>{children}</Suspense>;
