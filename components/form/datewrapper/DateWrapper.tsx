0d8500581d5894bd8474eed0b6bd33ff32a1c077
Nicolas N <nicolas.nordhagen@nav.no>
2025-04-02 21:34:05 +0200

    sikre validering av dato

--- components/form/datewrapper/DateWrapper.tsx
+++ components/form/datewrapper/DateWrapper.tsx
@@ -1,5 +1,5 @@
 import { DatePicker, useDatepicker } from '@navikt/ds-react';
-import { subYears, addYears } from 'date-fns';
+import { subYears, addYears, isValid } from 'date-fns';
 import React from 'react';
 import { Control, Controller, FieldValues, RegisterOptions, FieldPath } from 'react-hook-form';
 
@@ -8,7 +8,7 @@ export interface DateProps<FormFieldValues extends FieldValues> {
   label?: string;
   description?: React.ReactNode;
   disableWeekend?: boolean;
-  rules?: RegisterOptions<FormFieldValues>;
+  rules?: Omit<RegisterOptions<FormFieldValues>, 'validate'>;
   control: Control<FormFieldValues>;
   fromDate?: Date;
   size?: 'small' | 'medium';
@@ -43,7 +43,10 @@ export const DateWrapper = <FormFieldValues extends FieldValues>({
     <Controller
       name={name}
       control={control}
-      rules={rules}
+      rules={{
+        ...rules,
+        validate: (value) => isValid(value as string) || 'Ugyldig dato',
+      }}
       render={({ field: { name, value, onChange }, fieldState: { error } }) => {
         return (
           <DatePicker
@@ -65,7 +68,7 @@ export const DateWrapper = <FormFieldValues extends FieldValues>({
               name={name}
               hideLabel={hideLabel}
               description={description}
-              error={error && error.message}
+              error={error?.message}
               label={label}
               readOnly={readOnly}
               {...inputProps}
