2e1ce7b3c10cd5990da9fcc8ef913ca2a01d0270
Øivind Stensrud <oivind.stensrud@nav.no>
2025-04-07 16:20:48 +0200

    Kort for å vise at søknad er trukket

---
+++ components/trukketsøknadkort/TrukketSøknadKort.tsx
@@ -0,0 +1,20 @@
+import { XMarkOctagonIcon } from '@navikt/aksel-icons';
+import { BodyLong, Label } from '@navikt/ds-react';
+import { SideProsessKort } from 'components/sideprosesskort/SideProsessKort';
+
+interface Props {
+  grunnlag: {
+    begrunnelse: string;
+  };
+}
+
+export const TrukketSøknadKort = ({ grunnlag }: Props) => {
+  return (
+    <SideProsessKort heading={'Søknaden er trukket'} icon={<XMarkOctagonIcon />}>
+      <div>
+        <Label>Begrunnelse</Label>
+        <BodyLong>{grunnlag.begrunnelse}</BodyLong>
+      </div>
+    </SideProsessKort>
+  );
+};
