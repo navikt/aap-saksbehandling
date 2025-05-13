0c2a148d26fdc9ceed0cfab753816caeb45c29c7
Øivind Stensrud <oivind.stensrud@nav.no>
2025-05-13 10:48:07 +0200

    Retter styling-feil i dev
    På vent-kortet fikk feil styling i dev grunnet import-rekkefølge på css
    etter bundling. Bruker en id for å tvinge høyere spesifisitet.

--- components/sideprosesskort/SideProsessKort.tsx
+++ components/sideprosesskort/SideProsessKort.tsx
@@ -3,6 +3,7 @@
 import { ExpansionCard } from '@navikt/ds-react';
 import { ReactNode } from 'react';
 import styles from './SideProsessKort.module.css';
+import './SideProsesskort.css';
 
 interface Props {
   heading: string;
@@ -13,7 +14,13 @@ interface Props {
 
 export const SideProsessKort = ({ heading, children, icon, defaultOpen = true }: Props) => {
   return (
-    <ExpansionCard aria-label={heading} className={styles.sideProsessKort} size={'small'} defaultOpen={defaultOpen}>
+    <ExpansionCard
+      aria-label={heading}
+      className={`${styles.sideProsessKort}`}
+      size={'small'}
+      defaultOpen={defaultOpen}
+      id={'sideProsesskort'}
+    >
       <ExpansionCard.Header className={styles.header}>
         <div className={styles.title}>
           {icon && <div>{icon}</div>}
