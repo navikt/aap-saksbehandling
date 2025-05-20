bb1fec7f00c08fd19332bc4b20c1a95d5bc0a8e3
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-05-20 12:49:39 +0200

    Legg til brevsteg for klage

---
+++ components/behandlinger/brev/skriveBrev/KlagesaksopplysningerKolonne.tsx
@@ -0,0 +1,44 @@
+import { SaksopplysningerKort } from 'components/behandlinger/brev/skriveBrev/SaksopplysningerKort';
+import styles from './SaksopplysningerKolonne.module.css';
+import { Label } from '@navikt/ds-react';
+import {
+  BistandsGrunnlag,
+  FormkravGrunnlag,
+  KlagebehandlingKontorGrunnlag,
+  KlagebehandlingNayGrunnlag,
+  RefusjonskravGrunnlag,
+  SykdomsGrunnlag,
+} from 'lib/types/types';
+import { formaterDatoForFrontend } from 'lib/utils/date';
+import { parse } from 'date-fns';
+
+interface Props {
+  formkravGrunnlag: FormkravGrunnlag;
+  klagebehandlingKontorGrunnlag: KlagebehandlingKontorGrunnlag;
+  klagebehandlingNayGrunnlag: KlagebehandlingNayGrunnlag;
+}
+
+export const KlagesaksopplysningerKolonne = ({
+  formkravGrunnlag,
+  klagebehandlingKontorGrunnlag,
+  klagebehandlingNayGrunnlag,
+}: Props) => {
+  const formkravVurdering = formkravGrunnlag.vurdering;
+  const klagebehandlingKontorVurdering = klagebehandlingKontorGrunnlag.vurdering;
+  const klagebehandlingNayVurdering = klagebehandlingNayGrunnlag.vurdering;
+  return (
+    <div className={styles.kolonne}>
+      <Label as="p">Vilkårsvurderinger</Label>
+      {formkravVurdering && <SaksopplysningerKort tittel="Formkrav" begrunnelse={formkravVurdering.begrunnelse} />}
+      {klagebehandlingKontorVurdering && (
+        <SaksopplysningerKort
+          tittel="Vurdering av klage - kontor"
+          begrunnelse={klagebehandlingKontorVurdering.begrunnelse}
+        />
+      )}
+      {klagebehandlingNayVurdering && (
+        <SaksopplysningerKort tittel="Vurdering av klage - Nay" begrunnelse={klagebehandlingNayVurdering.begrunnelse} />
+      )}
+    </div>
+  );
+};
