08ccca9c6c25fcde55c266827f95f80a2f93ecd2
thomasrognes <thomas.sebastian.rognes@nav.no>
2025-05-20 14:51:40 +0200

    Endringer i design på søkeresultat etter gjennomgang med designer
    Co-authored-by: Thea Jegerud <thea.kristiane.tollefsbol.jegerud@nav.no>

--- components/oppgavestatus/OppgaveStatus.tsx
+++ components/oppgavestatus/OppgaveStatus.tsx
@@ -1,4 +1,4 @@
-import { Tag } from '@navikt/ds-react';
+import { Tag, TagProps } from '@navikt/ds-react';
 import { HourglassTopFilledIcon, PadlockLockedFillIcon, XMarkOctagonIcon } from '@navikt/aksel-icons';
 
 import styles from './OppgaveStatus.module.css';
@@ -10,26 +10,28 @@ export interface OppgaveStatusType {
 
 interface Props {
   oppgaveStatus: OppgaveStatusType;
+  size?: TagProps['size'];
+  showLabel?: boolean;
 }
 
-export const OppgaveStatus = ({ oppgaveStatus }: Props) => {
+export const OppgaveStatus = ({ oppgaveStatus, size = 'small', showLabel = true }: Props) => {
   switch (oppgaveStatus.status) {
     case 'PÅ_VENT':
       return (
-        <Tag className={styles.tag} icon={<HourglassTopFilledIcon />} variant={'warning-moderate'} size={'small'}>
-          {oppgaveStatus.label}
+        <Tag className={styles.tag} icon={<HourglassTopFilledIcon />} variant={'warning-moderate'} size={size}>
+          {showLabel && oppgaveStatus.label}
         </Tag>
       );
     case 'RESERVERT':
       return (
-        <Tag className={styles.tag} icon={<PadlockLockedFillIcon />} variant={'error'} size={'small'}>
-          {oppgaveStatus.label}
+        <Tag className={styles.tag} icon={<PadlockLockedFillIcon />} variant={'error-moderate'} size={size}>
+          {showLabel && oppgaveStatus.label}
         </Tag>
       );
     case 'TRUKKET':
       return (
-        <Tag variant={'neutral-moderate'} icon={<XMarkOctagonIcon />} size={'small'}>
-          Trukket
+        <Tag variant={'neutral-moderate'} icon={<XMarkOctagonIcon />} size={size}>
+          {showLabel && oppgaveStatus.label}
         </Tag>
       );
   }
