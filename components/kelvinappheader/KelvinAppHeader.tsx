8c7ef5808ab519a7a7837f24b9e7bee8221ade2d
thomasrognes <thomas.sebastian.rognes@nav.no>
2025-06-04 15:54:44 +0200

    Bytte mellom identer lokalt

--- components/kelvinappheader/KelvinAppHeader.tsx
+++ components/kelvinappheader/KelvinAppHeader.tsx
@@ -14,43 +14,93 @@ import {
   VStack,
 } from '@navikt/ds-react';
 import { Kelvinsøk, SøkeResultat } from './Kelvinsøk';
-import { ArrowRightLeftIcon, LeaveIcon, XMarkIcon } from '@navikt/aksel-icons';
+import { ArrowRightLeftIcon, CheckmarkCircleFillIcon, LeaveIcon, XMarkIcon } from '@navikt/aksel-icons';
 import { Kelvinsøkeresultat } from './Kelvinsøkeresultat';
 import styles from './KelvinAppHeader.module.css';
 import { AppSwitcher } from 'components/kelvinappheader/AppSwitcher';
 import { isDev, isLocal, isProd } from 'lib/utils/environment';
+import { useRouter } from 'next/navigation';
 
 interface BrukerInformasjon {
   navn: string;
   NAVident?: string;
 }
 
-const Brukermeny = ({ brukerInformasjon }: { brukerInformasjon: BrukerInformasjon }) => (
-  <Dropdown>
-    <InternalHeader.UserButton name={brukerInformasjon.navn} as={Dropdown.Toggle} />
-    <Dropdown.Menu>
-      <Dropdown.Menu.List>
-        {!isProd() && (
-          <>
-            <Dropdown.Menu.List.Item as={Link} href={'/oauth2/login?prompt=select_account'}>
-              <BodyShort>Bytt bruker</BodyShort>
-              <Spacer />
-              <ArrowRightLeftIcon aria-hidden fontSize="1.5rem" />
-            </Dropdown.Menu.List.Item>
+type Brukere = 'VEILEDER' | 'KVALITETSSIKRER' | 'SAKSBEHANDLER' | 'BESLUTTER';
 
-            <Dropdown.Menu.Divider />
-          </>
+const Brukermeny = ({ brukerInformasjon }: { brukerInformasjon: BrukerInformasjon }) => {
+  const router = useRouter();
+
+  const switchUser = (bruker: Brukere) => {
+    document.cookie = `bruker=${bruker}; path=/; max-age=86400000`; // 1 dag
+    router.refresh();
+  };
+
+  function getCookie(name: string) {
+    const cookies = document.cookie.split('; ');
+    console.log(cookies);
+    const cookie = cookies.find((coockie) => coockie.startsWith(name + '='));
+    return cookie ? decodeURIComponent(cookie.split('=')[1]) : null;
+  }
+
+  const bruker = typeof window !== 'undefined' && typeof document !== 'undefined' ? getCookie('bruker') : undefined;
+
+  return (
+    <Dropdown>
+      <InternalHeader.UserButton name={brukerInformasjon.navn} as={Dropdown.Toggle} />
+      <Dropdown.Menu>
+        <Dropdown.Menu.GroupedList>
+          {!isProd() && (
+            <>
+              <Dropdown.Menu.List.Item as={Link} href={'/oauth2/login?prompt=select_account'}>
+                <BodyShort>Bytt bruker</BodyShort>
+                <Spacer />
+                <ArrowRightLeftIcon aria-hidden fontSize="1.5rem" />
+              </Dropdown.Menu.List.Item>
+              <Dropdown.Menu.Divider />
+            </>
+          )}
+
+          <Dropdown.Menu.List.Item as={Link} href={'/oauth2/logout'}>
+            <BodyShort>Logg ut</BodyShort>
+            <Spacer />
+            <LeaveIcon aria-hidden fontSize="1.5rem" />
+          </Dropdown.Menu.List.Item>
+        </Dropdown.Menu.GroupedList>
+        <Dropdown.Menu.Divider />
+        {isLocal() && (
+          <Dropdown.Menu.GroupedList>
+            <Dropdown.Menu.GroupedList.Heading>Lokal backend</Dropdown.Menu.GroupedList.Heading>
+            <Dropdown.Menu.GroupedList.Item onClick={() => switchUser('SAKSBEHANDLER')}>
+              <HStack gap={'1'} align={'center'}>
+                <BodyShort>Saksbehandler</BodyShort>
+                {bruker === 'SAKSBEHANDLER' && <CheckmarkCircleFillIcon color={'green'} />}
+              </HStack>
+            </Dropdown.Menu.GroupedList.Item>
+            <Dropdown.Menu.GroupedList.Item onClick={() => switchUser('KVALITETSSIKRER')}>
+              <HStack gap={'1'} align={'center'}>
+                <BodyShort>Kvalitetssikrer</BodyShort>
+                {bruker === 'KVALITETSSIKRER' && <CheckmarkCircleFillIcon color={'green'} />}
+              </HStack>
+            </Dropdown.Menu.GroupedList.Item>
+            <Dropdown.Menu.GroupedList.Item onClick={() => switchUser('VEILEDER')}>
+              <HStack gap={'1'} align={'center'}>
+                <BodyShort>Veileder</BodyShort>
+                {bruker === 'VEILEDER' && <CheckmarkCircleFillIcon color={'green'} />}
+              </HStack>
+            </Dropdown.Menu.GroupedList.Item>
+            <Dropdown.Menu.GroupedList.Item onClick={() => switchUser('BESLUTTER')}>
+              <HStack gap={'1'} align={'center'}>
+                <BodyShort>Beslutter</BodyShort>
+                {bruker === 'BESLUTTER' && <CheckmarkCircleFillIcon color={'green'} />}
+              </HStack>
+            </Dropdown.Menu.GroupedList.Item>
+          </Dropdown.Menu.GroupedList>
         )}
-
-        <Dropdown.Menu.List.Item as={Link} href={'/oauth2/logout'}>
-          <BodyShort>Logg ut</BodyShort>
-          <Spacer />
-          <LeaveIcon aria-hidden fontSize="1.5rem" />
-        </Dropdown.Menu.List.Item>
-      </Dropdown.Menu.List>
-    </Dropdown.Menu>
-  </Dropdown>
-);
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
 export const KelvinAppHeader = ({ brukerInformasjon }: { brukerInformasjon: BrukerInformasjon }) => {
   const [søkeresultat, setSøkeresultat] = useState<SøkeResultat | undefined>(undefined);
