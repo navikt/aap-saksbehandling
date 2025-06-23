87bf93e250fb43737cbc64ccb6129d61fefe2410
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2025-06-23 12:07:21 +0200

    AAP-1066 Fiks lenker til vilkårskort i besluttersteget (#924)
    * AAP-1066 Fiks lenker til vilkårskort i besluttersteget
    
    * AAP-1066 Fjern slash i url

--- components/totrinnsvurdering/ToTrinnsvurdering.tsx
+++ components/totrinnsvurdering/ToTrinnsvurdering.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { useState } from 'react';
-import { Behovstype } from 'lib/utils/form';
-import { useParams } from 'next/navigation';
-import { Historikk } from 'components/totrinnsvurdering/historikk/Historikk';
-import { Oppsummering } from 'components/totrinnsvurdering/oppsummering/Oppsummering';
+import {useState} from 'react';
+import {Behovstype} from 'lib/utils/form';
+import {useParams} from 'next/navigation';
+import {Historikk} from 'components/totrinnsvurdering/historikk/Historikk';
+import {Oppsummering} from 'components/totrinnsvurdering/oppsummering/Oppsummering';
 import {
   AvklaringsbehovKode,
   FatteVedtakGrunnlag,
   KvalitetssikringGrunnlag,
   ToTrinnsVurderingGrunn,
 } from 'lib/types/types';
-import { ToTrinnsvurderingToggleGroup } from 'components/totrinnsvurdering/totrinnsvurderingtogglegroup/ToTrinnsvurderingToggleGroup';
-import { TotrinnsvurderingForm } from 'components/totrinnsvurdering/totrinnsvurderingform/TotrinnsvurderingForm';
+import {
+  ToTrinnsvurderingToggleGroup
+} from 'components/totrinnsvurdering/totrinnsvurderingtogglegroup/ToTrinnsvurderingToggleGroup';
+import {TotrinnsvurderingForm} from 'components/totrinnsvurdering/totrinnsvurderingform/TotrinnsvurderingForm';
 
 import styles from 'components/totrinnsvurdering/ToTrinnsvurdering.module.css';
 
@@ -94,15 +96,31 @@ export function behovstypeTilVilkårskortLink(behovstype: Behovstype): string {
     case Behovstype.VURDER_SYKEPENGEERSTATNING_KODE:
       return 'SYKDOM/#VURDER_SYKEPENGEERSTATNING';
     case Behovstype.AVKLAR_BARNETILLEGG_KODE:
-      return '/BARNETILLEGG/#BARNETILLEGG';
+      return 'BARNETILLEGG/#BARNETILLEGG';
     case Behovstype.AVKLAR_SONINGSFORRHOLD:
-      return '/ET_ANNET_STED';
+      return 'ET_ANNET_STED';
     case Behovstype.AVKLAR_HELSEINSTITUSJON:
-      return '/ET_ANNET_STED';
+      return 'ET_ANNET_STED';
     case Behovstype.VURDER_KLAGE_KONTOR:
       return 'KLAGEBEHANDLING_KONTOR';
     case Behovstype.VURDER_KLAGE_NAY:
       return 'KLAGEBEHANDLING_NAY';
+    case Behovstype.AVKLAR_FORUTGÅENDE_MEDLEMSKAP:
+      return 'MEDLEMSKAP';
+    case Behovstype.AVKLAR_LOVVALG_MEDLEMSKAP:
+      return 'LOVVALG';
+    case Behovstype.AVKLAR_SAMORDNING_ANDRE_STATLIGE_YTELSER:
+      return 'SAMORDNING';
+    case Behovstype.AVKLAR_SAMORDNING_GRADERING:
+      return 'SAMORDNING';
+    case Behovstype.SAMORDNING_REFUSJONS_KRAV:
+      return 'SAMORDNING';
+    case Behovstype.AVKLAR_SAMORDNING_UFORE:
+      return 'SAMORDNING';
+    case Behovstype.FASTSETT_MANUELL_INNTEKT:
+      return 'GRUNNLAG';
+    case Behovstype.VURDER_RETTIGHETSPERIODE:
+      return 'RETTIGHETSPERIODE';
     default:
       return 'SYKDOM';
   }
