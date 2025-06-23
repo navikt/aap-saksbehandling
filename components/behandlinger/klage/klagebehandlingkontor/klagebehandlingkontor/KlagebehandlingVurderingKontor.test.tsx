ef75dd9febbe48fead425fb09513b49fae972896
Steffen Hageland <steffen.hageland@nav.no>
2025-06-23 19:56:47 +0200

    Legger til mulighet for å klage på resterende hjemler.
    For omgjøring er det ikke implementert revurdering av alle hjemmlene enda, så disse validerer man mot og viser feilmelding om man har valg omgjøring for.

--- components/behandlinger/klage/klagebehandlingkontor/klagebehandlingkontor/KlagebehandlingVurderingKontor.test.tsx
+++ components/behandlinger/klage/klagebehandlingkontor/klagebehandlingkontor/KlagebehandlingVurderingKontor.test.tsx
@@ -1,6 +1,9 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '../../../../../lib/test/CustomRender';
 import { KlagebehandlingVurderingKontor } from './KlagebehandlingVurderingKontor';
+import { userEvent } from '@testing-library/user-event';
+
+const user = userEvent.setup();
 
 describe('Klage - vurdering kontor', () => {
   it('Skal ha en overskrift', () => {
@@ -33,5 +36,51 @@ describe('Klage - vurdering kontor', () => {
     const begrunnelse = screen.getByRole('textbox', { name: 'Vurder klage' });
     expect(begrunnelse).toBeVisible();
     expect(begrunnelse).toHaveValue('Min begrunnelse');
+  });
+
+  it('Skal vise valideringsfeil når man velger en ikke-implementert hjemmel', async () => {
+    render(<KlagebehandlingVurderingKontor readOnly={false} behandlingVersjon={0} typeBehandling={'Klage'} />);
+
+    // Fyll ut obligatoriske felt og velg 11-3 (som ike er implementert) i dropdown
+    const begrunnelse = screen.getByRole('textbox', { name: 'Vurder klage' });
+    await user.type(begrunnelse, 'Test begrunnelse');
+
+    const omgjørRadio = screen.getByRole('radio', { name: 'Vedtak omgjøres' });
+    await user.click(omgjørRadio);
+
+    const combobox = screen.getByRole('combobox', { name: 'Hvilke vilkår skal omgjøres?' });
+    await user.click(combobox);
+
+    const option = screen.getByRole('option', { name: '§ 11-3' });
+    await user.click(option);
+
+    const bekreftKnapp = screen.getByRole('button', { name: 'Bekreft' });
+    await user.click(bekreftKnapp);
+
+    expect(screen.getByText(/Det er ikke mulig å opprette revurdering på/)).toBeVisible();
+    expect(combobox).toHaveAttribute('aria-invalid');
+  });
+
+  it('Skal ikke vise valideringsfeil når man velger en implementert hjemmel', async () => {
+    render(<KlagebehandlingVurderingKontor readOnly={false} behandlingVersjon={0} typeBehandling={'Klage'} />);
+
+    // Fyll ut obligatoriske felt og velg 11-5 i dropdown (som er implementert)
+    const begrunnelse = screen.getByRole('textbox', { name: 'Vurder klage' });
+    await user.type(begrunnelse, 'Test begrunnelse');
+
+    const omgjørRadio = screen.getByRole('radio', { name: 'Vedtak omgjøres' });
+    await user.click(omgjørRadio);
+
+    const combobox = screen.getByRole('combobox', { name: 'Hvilke vilkår skal omgjøres?' });
+    await user.click(combobox);
+
+    const option = screen.getByRole('option', { name: '§ 11-5' });
+    await user.click(option);
+
+    const bekreftKnapp = screen.getByRole('button', { name: 'Bekreft' });
+    await user.click(bekreftKnapp);
+
+    expect(screen.queryByText(/Det er ikke mulig å opprette revurdering på/)).not.toBeInTheDocument();
+    expect(combobox).not.toHaveAttribute('aria-invalid');
   });
 });
