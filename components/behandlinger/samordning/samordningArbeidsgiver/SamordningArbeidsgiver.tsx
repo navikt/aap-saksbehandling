cf199617affa968f7359d515f7d478e64dfde17c
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-07-07 11:11:37 +0200

    Samkjører med backend

--- components/behandlinger/samordning/samordningArbeidsgiver/SamordningArbeidsgiver.tsx
+++ components/behandlinger/samordning/samordningArbeidsgiver/SamordningArbeidsgiver.tsx
@@ -2,7 +2,7 @@
 
 import { FormField } from 'components/form/FormField';
 import { useConfigForm } from 'components/form/FormHook';
-import { Alert, VStack } from '@navikt/ds-react';
+import { Alert, Label, VStack } from '@navikt/ds-react';
 import { FormEvent } from 'react';
 import { Behovstype } from 'lib/utils/form';
 import { useLøsBehovOgGåTilNesteSteg } from 'hooks/LøsBehovOgGåTilNesteStegHook';
@@ -12,6 +12,7 @@ import { SamordningArbeidsgiverGrunnlag } from 'lib/types/types';
 import { DateInputWrapper } from 'components/form/dateinputwrapper/DateInputWrapper';
 import { validerDato } from 'lib/validation/dateValidation';
 import { isBefore, parse, startOfDay } from 'date-fns';
+import { formaterDatoForBackend, formaterDatoForFrontend } from 'lib/utils/date';
 
 interface Props {
   grunnlag: SamordningArbeidsgiverGrunnlag;
@@ -29,7 +30,7 @@ export const SamordningArbeidsgiver = ({ readOnly, behandlingVersjon, grunnlag }
     {
       begrunnelse: {
         type: 'textarea',
-        label: 'Vurder om brukeren har arbeidsgiverytelser som skal avregnes med AAP',
+        label: 'Vurder om brukeren skal ha 100% reduksjon av AAP i en periode som følge av ytelse fra arbeidsgiver',
         rules: { required: 'Du må gjøre en vilkårsvurdering' },
         defaultValue: grunnlag.vurdering?.begrunnelse,
       },
@@ -37,13 +38,13 @@ export const SamordningArbeidsgiver = ({ readOnly, behandlingVersjon, grunnlag }
         type: 'text',
         label: 'Fra dato',
         rules: { required: 'Du må velge en dato' },
-        defaultValue: grunnlag.vurdering?.fom,
+        defaultValue: grunnlag.vurdering?.fom ? formaterDatoForFrontend(grunnlag.vurdering?.fom) : '',
       },
       tom: {
         type: 'text',
         label: 'Til dato',
         rules: { required: 'Du må velge en dato' },
-        defaultValue: grunnlag.vurdering?.tom,
+        defaultValue: grunnlag.vurdering?.tom ? formaterDatoForFrontend(grunnlag.vurdering?.tom) : '',
       },
     },
     { readOnly: readOnly }
@@ -61,8 +62,8 @@ export const SamordningArbeidsgiver = ({ readOnly, behandlingVersjon, grunnlag }
           behovstype: Behovstype.AVKLAR_SAMORDNING_ARBEIDSGIVER,
           samordningArbeidsgiverVurdering: {
             vurdering: data.begrunnelse!!,
-            fom: data.fom!!,
-            tom: data.tom!!,
+            fom: formaterDatoForBackend(parse(data.fom!!, 'dd.MM.yyyy', new Date())),
+            tom: formaterDatoForBackend(parse(data.tom!!, 'dd.MM.yyyy', new Date())),
           },
         },
         referanse: behandlingsreferanse,
@@ -86,15 +87,14 @@ export const SamordningArbeidsgiver = ({ readOnly, behandlingVersjon, grunnlag }
     >
       {
         <VStack gap={'6'}>
-          <Alert variant={'info'} size={'small'} className={'fit-content'}>
-            Det er ikke støtte for refusjonskrav enda. Sett saken på vent og kontakt team AAP.
-          </Alert>
           <FormField form={form} formField={formFields.begrunnelse} className={'begrunnelse'} />
+          <Label size={'small'}>Legg til start- og sluttdato for reduksjon som følge av ytelse fra arbeidsgiver</Label>
           <div style={{ display: 'flex', gap: '1.0rem', flexDirection: 'row', flexWrap: 'wrap' }}>
             <DateInputWrapper
               control={form.control}
+              label={'Startdato'}
               name={`fom`}
-              hideLabel={true}
+              hideLabel={false}
               rules={{
                 required: 'Du må velge når sluttpakken gjelder fra',
                 validate: (value) => {
@@ -105,8 +105,9 @@ export const SamordningArbeidsgiver = ({ readOnly, behandlingVersjon, grunnlag }
             />
             <DateInputWrapper
               control={form.control}
+              label={'Sluttdato'}
               name={`tom`}
-              hideLabel={true}
+              hideLabel={false}
               rules={{
                 required: 'Du må velge når sluttpakken gjelder til',
                 validate: (value) => {
