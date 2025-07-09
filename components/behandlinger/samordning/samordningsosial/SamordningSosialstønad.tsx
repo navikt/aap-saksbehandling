75d62f2fab8ac201d30573586a4fb9e8d18080e4
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-07-09 15:59:47 +0200

    Fjerne ubrukt import

--- components/behandlinger/samordning/samordningsosial/SamordningSosialstønad.tsx
+++ components/behandlinger/samordning/samordningsosial/SamordningSosialstønad.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Alert, BodyShort, HStack, Table } from '@navikt/ds-react';
+import { BodyShort, HStack, Table } from '@navikt/ds-react';
 import { VilkårsKort } from 'components/vilkårskort/VilkårsKort';
 import { RefusjonskravGrunnlag } from 'lib/types/types';
 import { formaterDatoForFrontend } from 'lib/utils/date';
