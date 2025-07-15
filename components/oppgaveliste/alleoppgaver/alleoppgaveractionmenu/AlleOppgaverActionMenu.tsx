33b8908d08111351ad8dd6e25d1c8a2e1640bfdb
SelmaBergstrand <selma.kristine@gmail.com>
2025-07-15 10:03:56 +0200

    AAP-1156 Avreserver oppgaver fra Alle oppgaver-siden

--- components/oppgaveliste/alleoppgaver/alleoppgaveractionmenu/AlleOppgaverActionMenu.tsx
+++ components/oppgaveliste/alleoppgaver/alleoppgaveractionmenu/AlleOppgaverActionMenu.tsx
@@ -1,43 +1,66 @@
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { ActionMenu, Button, Loader } from '@navikt/ds-react';
+import { ActionMenu, Button } from '@navikt/ds-react';
 import { MenuElipsisVerticalIcon } from '@navikt/aksel-icons';
 import { byggKelvinURL } from 'lib/utils/request';
 import { Oppgave } from 'lib/types/oppgaveTypes';
+import { avreserverOppgaveClient } from 'lib/oppgaveClientApi';
+import { isSuccess } from 'lib/utils/api';
+import { useState, useTransition } from 'react';
 
 interface Props {
   oppgave: Oppgave;
+  revalidateFunction: () => void;
 }
 
-export const AlleOppgaverActionMenu = ({ oppgave }: Props) => {
+export const AlleOppgaverActionMenu = ({ oppgave, revalidateFunction }: Props) => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const [isPendingFrigi, startTransitionFrigi] = useTransition();
+  const erReservert = oppgave.reservertAv != null;
+
+  async function frigiOppgave(oppgave: Oppgave) {
+    startTransitionFrigi(async () => {
+      if (oppgave.id) {
+        const res = await avreserverOppgaveClient([oppgave.id]);
+
+        if (isSuccess(res)) {
+          await (revalidateFunction as () => Promise<void>)();
+        }
+      }
+    });
+  }
 
   return (
     <>
-      {!isLoading ? (
-        <ActionMenu>
-          <ActionMenu.Trigger>
-            <Button
-              variant={'tertiary-neutral'}
-              icon={<MenuElipsisVerticalIcon title={'Oppgavemeny'} />}
-              size={'small'}
-            />
-          </ActionMenu.Trigger>
-          <ActionMenu.Content>
+      <ActionMenu>
+        <ActionMenu.Trigger>
+          <Button
+            variant={'tertiary-neutral'}
+            icon={<MenuElipsisVerticalIcon title={'Oppgavemeny'} />}
+            size={'small'}
+            loading={isLoading || isPendingFrigi}
+          />
+        </ActionMenu.Trigger>
+        <ActionMenu.Content>
+          <ActionMenu.Item
+            onSelect={() => {
+              setIsLoading(true);
+              router.push(byggKelvinURL(oppgave));
+            }}
+          >
+            Åpne oppgave
+          </ActionMenu.Item>
+          {erReservert && (
             <ActionMenu.Item
-              onSelect={() => {
-                setIsLoading(true);
-                router.push(byggKelvinURL(oppgave));
+              onSelect={async () => {
+                await frigiOppgave(oppgave);
               }}
             >
-              Åpne oppgave
+              Frigi oppgave
             </ActionMenu.Item>
-          </ActionMenu.Content>
-        </ActionMenu>
-      ) : (
-        <Loader />
-      )}
+          )}
+        </ActionMenu.Content>
+      </ActionMenu>
     </>
   );
 };
