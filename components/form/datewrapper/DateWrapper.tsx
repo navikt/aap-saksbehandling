de1613d762398284604e6a720ad47092d4dcac48
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2025-07-28 20:59:24 +0200

    Datepicker oppdaterte ikke visning av verdi ved reset av felt gjennom rhf

--- components/form/datewrapper/DateWrapper.tsx
+++ components/form/datewrapper/DateWrapper.tsx
@@ -1,6 +1,6 @@
 import { DatePicker, useDatepicker } from '@navikt/ds-react';
-import { addYears, isValid, subYears } from 'date-fns';
-import React from 'react';
+import { addYears, isEqual, isValid, subYears } from 'date-fns';
+import React, { useEffect } from 'react';
 import { Control, FieldPath, FieldValues, RegisterOptions, useController } from 'react-hook-form';
 import { isDate } from 'lodash';
 
@@ -50,7 +50,12 @@ export const DateWrapper = <FormFieldValues extends FieldValues>({
     },
   });
 
-  const { datepickerProps, inputProps } = useDatepicker({
+  const {
+    datepickerProps,
+    inputProps,
+    selectedDay: selectedInternal,
+    setSelected,
+  } = useDatepicker({
     defaultSelected: selected,
     onDateChange: (date) => onChange(date),
     toDate,
@@ -58,13 +63,27 @@ export const DateWrapper = <FormFieldValues extends FieldValues>({
     disableWeekends,
   });
 
+  // Må synce state slik at resetting av felt i rhf også oppdaterer intern state i datepicker
+  useEffect(() => {
+    if (!value && !selectedInternal) {
+      return;
+    }
+
+    if (
+      (value && !selectedInternal) ||
+      (!value && selectedInternal) ||
+      (value && selectedInternal && !isEqual(value, selectedInternal))
+    ) {
+      if (isValid(value)) {
+        setSelected(value);
+      } else {
+        setSelected(undefined);
+      }
+    }
+  }, [value, selectedInternal, setSelected]);
+
   return (
-    <DatePicker
-      {...datepickerProps}
-      id={name}
-      dropdownCaption
-      strategy={strategy}
-    >
+    <DatePicker {...datepickerProps} id={name} dropdownCaption strategy={strategy}>
       <DatePicker.Input
         onChange={onChange}
         onInput={onChange}
