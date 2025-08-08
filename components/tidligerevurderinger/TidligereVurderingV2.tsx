0db6d5a0525fd259f3dde09edfe01dc08bc1c6fd
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-08-08 13:06:13 +0200

    flytt tidligerevurdering interface til v3

--- components/tidligerevurderinger/TidligereVurderingV2.tsx
+++ components/tidligerevurderinger/TidligereVurderingV2.tsx
@@ -1,21 +1,13 @@
 'use client';
 
 import { BodyShort, Label, Table, VStack } from '@navikt/ds-react';
-import { ÅpenPeriode } from 'lib/types/types';
-import { ValuePair } from 'components/form/FormField';
 import { formaterDatoForFrontend } from 'lib/utils/date';
+import { TidligereVurdering } from './TidligereVurderingerV3';
 
 interface Props {
   tidligereVurdering: TidligereVurdering;
 }
-// Todo: Flytt denne over til v3 når alle implementasjoner av "tidligere vurderinger" benytter sistnevnte
-export interface TidligereVurdering {
-  periode: ÅpenPeriode;
-  vurdertAvIdent: string;
-  vurdertDato: string;
-  felter: ValuePair[];
-  erGjeldendeVurdering: boolean;
-}
+
 export const TidligereVurderingV2 = ({ tidligereVurdering }: Props) => {
   return (
     <Table.ExpandableRow
