4735314111952306a23240edf2ba57638d4c6f02
Steffen Hageland <steffen.hageland@nav.no>
2025-08-08 11:25:25 +0200

    Fjerner opprett test sak fra saksoversikt frem til det er ordentlig testet
    Denne ble med, med et uhell. Og har en egen underside for verifisering i reelt miljø enn så lenge.

--- app/saksbehandling/saksoversikt/page.tsx
+++ app/saksbehandling/saksoversikt/page.tsx
@@ -1,14 +1,12 @@
 import { AlleSakerListe } from 'components/saksliste/AlleSakerListe';
 import styles from './page.module.css';
-import { isDev, isLocal, isProd } from 'lib/utils/environment';
+import { isLocal, isProd } from 'lib/utils/environment';
 import { OpprettSakLocal } from 'components/opprettsak/OpprettSakLocal';
-import OpprettSakTest from 'components/opprettsak/OpprettSakTest';
 
 const Page = async () => {
   return (
     <main className={styles.main}>
       {isLocal() && <OpprettSakLocal />}
-      {isDev() && <OpprettSakTest />}
       {!isProd() && <AlleSakerListe />}
     </main>
   );
