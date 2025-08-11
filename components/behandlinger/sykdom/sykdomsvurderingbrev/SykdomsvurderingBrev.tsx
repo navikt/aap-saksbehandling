12e0b01aa1ab601608cb6e14159169103dec2f76
Henrik Gundersen <henrikgun@gmail.com>
2025-08-11 15:54:28 +0200

    AAP-1179 Oppdaterer tekst (#1064)

--- components/behandlinger/sykdom/sykdomsvurderingbrev/SykdomsvurderingBrev.tsx
+++ components/behandlinger/sykdom/sykdomsvurderingbrev/SykdomsvurderingBrev.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BodyLong, Box, List, VStack } from '@navikt/ds-react';
+import { BodyLong, Box, Heading, List, VStack } from '@navikt/ds-react';
 import { useBehandlingsReferanse } from 'hooks/BehandlingHook';
 import { useLøsBehovOgGåTilNesteSteg } from 'hooks/LøsBehovOgGåTilNesteStegHook';
 import { SykdomsvurderingBrevGrunnlag, TypeBehandling } from 'lib/types/types';
@@ -38,7 +38,7 @@ export const SykdomsvurderingBrev = ({ behandlingVersjon, grunnlag, typeBehandli
       },
       vurdering: {
         type: 'textarea',
-        label: 'Innhold til vedtaksbrevet',
+        label: 'Derfor får du AAP / Derfor får du ikke AAP',
         defaultValue: grunnlag?.vurdering?.vurdering ?? undefined,
         rules: { required: 'Du må legge til innhold for vedtaksbrevet' },
       },
@@ -96,35 +96,50 @@ export const SykdomsvurderingBrev = ({ behandlingVersjon, grunnlag, typeBehandli
       <VStack gap={'4'}>
         {skalViseTidligereVurderinger && <TidligereVurderingerV3 tidligereVurderinger={historiskeVurderinger} />}
 
+        <Veiledning
+          header={'Hva skal være med i teksten?'}
+          tekst={
+            <Box>
+              <BodyLong size={'small'}>Melding om innvilgelse skal innholde en beskrivelse av</BodyLong>
+              <List size={'small'}>
+                <List.Item>hvilke opplysninger som er lagt til grunn, eksempelvis fra lege</List.Item>
+                <List.Item>hvilke hovedhensyn som har vært avgjørende for utfallet</List.Item>
+              </List>
+              <BodyLong size={'small'}>Melding om avslag skal inneholde</BodyLong>
+              <List size={'small'}>
+                <List.Item>opplysninger om vilkåret eller vilkårene som er avslått</List.Item>
+                <List.Item>begrunnelse for vilkåret eller vilkårene som er avslått</List.Item>
+                <List.Item>
+                  informasjon om hvilke av sakens opplysninger som har vært avgjørende for at vilkår/ vilkårene er
+                  ansett for ikke å være oppfylt
+                </List.Item>
+              </List>
+            </Box>
+          }
+          defaultOpen={false}
+        />
+
+        <Veiledning
+          header={'Hvor står denne teksten i brevet?'}
+          tekst={
+            <Box>
+              <BodyLong size={'small'}>Før avsnittet du skriver, står dette avsnittet:</BodyLong>
+              <Box marginBlock={'3 0'}>
+                <Heading size={'xsmall'}>Hvem kan få AAP?</Heading>
+                <BodyLong size={'small'}>
+                  For å ha rett til AAP må arbeidsevnen din må være nedsatt med minst halvparten på grunn av sykdom
+                  eller skade, og du må ha aktiv behandling eller bistand fra NAV for å kunne bedre arbeidsevnen din.
+                  Dette går fram av folketrygdlovens §§ 11-5 og 11-6.
+                </BodyLong>
+              </Box>
+            </Box>
+          }
+          defaultOpen={false}
+        />
+
         <FormField form={form} formField={formFields.vurderingSkalFyllesUt} horizontalRadio />
         {form.watch('vurderingSkalFyllesUt') === JaEllerNei.Ja && (
           <>
-            <Veiledning
-              header={'Hva skal være med i teksten?'}
-              tekst={
-                <Box>
-                  <BodyLong size={'small'}>
-                    Denne teksten blir synlig for bruker, og du må begrunne vurderingene og resultatet av
-                    saksbehandlingen. Melding om innvilgelse skal innholde en beskrivelse av
-                  </BodyLong>
-                  <List size={'small'}>
-                    <List.Item>hvilke opplysninger som er lagt til grunn, eksempelvis fra lege</List.Item>
-                    <List.Item>hvilke hovedhensyn som har vært avgjørende for utfallet</List.Item>
-                  </List>
-                  <BodyLong size={'small'}>Melding om avslag skal inneholde</BodyLong>
-                  <List size={'small'}>
-                    <List.Item>opplysninger om vilkåret eller vilkårene som er avslått</List.Item>
-                    <List.Item>begrunnelse for vilkåret eller vilkårene som er avslått</List.Item>
-                    <List.Item>
-                      informasjon om hvilke av sakens opplysninger som har vært avgjørende for at vilkår/ vilkårene er
-                      ansett for ikke å være oppfylt
-                    </List.Item>
-                  </List>
-                </Box>
-              }
-              defaultOpen={false}
-            />
-
             <FormField form={form} formField={formFields.vurdering} className={'begrunnelse'} />
           </>
         )}
