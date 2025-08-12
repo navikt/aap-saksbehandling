873a929af6ba1f2669e5e57d1979b8acbdb6d569
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2025-08-12 16:19:44 +0200

    Legger til korrekt tag på hentFlyt endepunkt

--- lib/services/saksbehandlingservice/saksbehandlingService.ts
+++ lib/services/saksbehandlingservice/saksbehandlingService.ts
@@ -42,7 +42,6 @@ import {
   OppfølgningOppgaveOpprinnelseResponse,
   OpprettDummySakDto,
   OpprettTestcase,
-  OpprettDummySakDto,
   OvergangUforeGrunnlag,
   PåklagetBehandlingGrunnlag,
   RefusjonskravGrunnlag,
