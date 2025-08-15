ca23de702ff922c4f187ebd47b0dc2a75f903b38
Henrik Gundersen <henrikgun@gmail.com>
2025-08-15 12:41:56 +0200

    AAP-1179 Fjerner tekst besluttersteget (#1090)

--- lib/utils/form.ts
+++ lib/utils/form.ts
@@ -120,7 +120,7 @@ export function mapBehovskodeTilBehovstype(kode: BehovsKode): string {
     case '5052':
       return 'Skriv brev';
     case '5053':
-      return 'Individuell begrunnelse for §§ 11-5 og 11-6 til vedtaksbrev besluttersteget';
+      return 'Individuell begrunnelse for §§ 11-5 og 11-6 til vedtaksbrev';
     case '5029':
       return 'Starttidspunkt';
     case '5999':
