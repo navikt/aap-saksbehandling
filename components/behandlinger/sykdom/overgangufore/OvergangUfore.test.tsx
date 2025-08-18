ec1b61cae72271d37863afc3973f4882afb99d34
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-08-18 13:23:09 +0200

    Fikser opp i tester

--- components/behandlinger/sykdom/overgangufore/OvergangUfore.test.tsx
+++ components/behandlinger/sykdom/overgangufore/OvergangUfore.test.tsx
@@ -6,50 +6,52 @@ import userEvent from '@testing-library/user-event';
 
 const user = userEvent.setup();
 
-describe('Førstegangsbehandling', () => {});
+describe('Førstegangsbehandling', () => {
+  it('Skal ha en overskrift', () => {
+    render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
 
-describe('Revurdering', () => {});
-
-it('Skal ha en overskrift', () => {
-  render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
-
-  const heading = screen.getByText('§ 11-18 AAP under behandling av krav om uføretrygd');
-  expect(heading).toBeVisible();
-});
-
-it('Skal ha felt for begrunnelse', () => {
-  render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
-  const begrunnelse = screen.getByRole('textbox', { name: 'Vilkårsvurdering' });
-  expect(begrunnelse).toBeVisible();
-});
-
-it('Skal ha felt for om brukeren har søkt om uføretrygd', () => {
-  render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
-  const felt = screen.getByRole('group', {
-    name: 'Har brukeren søkt om uføretrygd?',
+    const heading = screen.getByText('§ 11-18 AAP under behandling av krav om uføretrygd');
+    expect(heading).toBeVisible();
   });
-  expect(felt).toBeVisible();
+
+  it('Skal ha felt for begrunnelse', () => {
+    render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
+    const begrunnelse = screen.getByRole('textbox', { name: 'Vilkårsvurdering' });
+    expect(begrunnelse).toBeVisible();
+  });
+
+  it('Skal ha felt for om brukeren har søkt om uføretrygd', () => {
+    render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
+    const felt = screen.getByRole('group', {
+      name: 'Har brukeren søkt om uføretrygd?',
+    });
+    expect(felt).toBeVisible();
+  });
+
+  it('Viser felt om brukeren har fått vedtak om uføretrygd, dersom brukeren ikke har søkt', async () => {
+    render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
+
+    expect(finnGruppeForSoktOmUforetrygd()).toBeVisible();
+
+    await velgJa(finnGruppeForSoktOmUforetrygd());
+    expect(finnGruppeForVedtakOmUforetrygd()).toBeVisible();
+  });
+
+  it('Har brukeren rett på AAP under behandling av krav om uføretrygd etter § 11-18?', async () => {
+    render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
+
+    expect(finnGruppeForRettPåAAP()).toBeVisible();
+  });
+
+  const finnGruppeForSoktOmUforetrygd = () => screen.getByRole('group', { name: 'Har brukeren søkt om uføretrygd?' });
+  const finnGruppeForVedtakOmUforetrygd = () =>
+    screen.getByRole('group', { name: 'Har brukeren fått vedtak på søknaden om uføretrygd?' });
+  const finnGruppeForRettPåAAP = () =>
+    screen.getByRole('group', {
+      name: 'Har brukeren rett på AAP under behandling av krav om uføretrygd etter § 11-18?',
+    });
+
+  const velgJa = async (group: HTMLElement) => {
+    await user.click(within(group).getByRole('radio', { name: 'Ja' }));
+  };
 });
-
-it('Viser felt om brukeren har fått vedtak om uføretrygd, dersom han har søkt', async () => {
-  render(<OvergangUfore readOnly={false} behandlingVersjon={0} typeBehandling={'Førstegangsbehandling'} />);
-
-  expect(finnGruppeForSoktOmUforetrygd()).toBeVisible();
-
-  await velgJa(finnGruppeForSoktOmUforetrygd());
-  expect(finnGruppeForSoktOmUforetrygd()).toBeVisible();
-  await velgNei(finnGruppeForSoktOmUforetrygd());
-  expect(finnGruppeForVedtakOmUnforetrygd()).not.toBeVisible();
-});
-
-const finnGruppeForSoktOmUforetrygd = () => screen.getByRole('group', { name: 'Har brukeren søkt om uføretrygd?' });
-const finnGruppeForVedtakOmUnforetrygd = () =>
-  screen.getByRole('group', { name: 'Har brukeren fått vedtak på søknaden om uføretrygd?' });
-
-const velgJa = async (group: HTMLElement) => {
-  await user.click(within(group).getByRole('radio', { name: 'Ja' }));
-};
-
-const velgNei = async (group: HTMLElement) => {
-  await user.click(within(group).getByRole('radio', { name: 'Nei' }));
-};
