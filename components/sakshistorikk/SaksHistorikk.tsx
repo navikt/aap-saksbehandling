00b7be874c5e87578ac589777a7e530a055deac5
Tor Idland <tor.idland@nav.no>
2025-08-25 14:09:29 +0200

    putter swr-kallet til historikken i en hook sånn at vi lettere kan trigge refetch

--- components/sakshistorikk/SaksHistorikk.tsx
+++ components/sakshistorikk/SaksHistorikk.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-import useSWR from 'swr';
-import { useSaksnummer } from 'hooks/saksbehandling/BehandlingHook';
-import { clientHentSakshistorikk } from 'lib/clientApi';
 import { VStack } from '@navikt/ds-react';
 import { BehandlingsHendelserTidslinje } from 'components/sakshistorikk/BehandlingsHendelserTidslinje';
+import { useSaksHistorikk } from 'hooks/saksbehandling/SaksHistorikkHook';
 
 export function SaksHistorikk() {
-  const saksnummer = useSaksnummer();
-  const { data: saksHistorikk } = useSWR(`sak/${saksnummer}/historikk`, () => clientHentSakshistorikk(saksnummer));
-  console.log('historikk', saksHistorikk);
+  const { historikk } = useSaksHistorikk();
 
-  if (!saksHistorikk || saksHistorikk?.type === 'ERROR') return null;
+  if (!historikk || historikk?.type === 'ERROR') return null;
   return (
     <section>
       <VStack gap={'2'}>
-        {saksHistorikk.data.map((behandling, behandlingIndex) => (
+        {historikk.data.map((behandling, behandlingIndex) => (
           <BehandlingsHendelserTidslinje
             key={`behandling-${behandlingIndex}`}
             hendelser={behandling.hendelser}
