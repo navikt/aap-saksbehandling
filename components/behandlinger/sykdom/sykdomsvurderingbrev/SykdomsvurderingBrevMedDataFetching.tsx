e1cd46a5f3f31feb305bd61e0d6e8f26fef02842
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2025-08-26 14:25:01 +0200

    AAP-1059 Mellomlagring i SykdomsvurderingBrev

--- components/behandlinger/sykdom/sykdomsvurderingbrev/SykdomsvurderingBrevMedDataFetching.tsx
+++ components/behandlinger/sykdom/sykdomsvurderingbrev/SykdomsvurderingBrevMedDataFetching.tsx
@@ -1,8 +1,12 @@
-import { hentSykdomsvurderingBrevGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import {
+  hentMellomlagring,
+  hentSykdomsvurderingBrevGrunnlag,
+} from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 import { isError } from 'lib/utils/api';
 import { SykdomsvurderingBrev } from 'components/behandlinger/sykdom/sykdomsvurderingbrev/SykdomsvurderingBrev';
 import { TypeBehandling } from 'lib/types/types';
+import { Behovstype } from 'lib/utils/form';
 
 interface Props {
   behandlingsReferanse: string;
@@ -17,7 +21,11 @@ export const SykdomsvurderingBrevMedDataFetching = async ({
   typeBehandling,
   readOnly,
 }: Props) => {
-  const grunnlag = await hentSykdomsvurderingBrevGrunnlag(behandlingsReferanse);
+  const [grunnlag, initialMellomlagretVurdering] = await Promise.all([
+    hentSykdomsvurderingBrevGrunnlag(behandlingsReferanse),
+    hentMellomlagring(behandlingsReferanse, Behovstype.SYKDOMSVURDERING_BREV_KODE),
+  ]);
+
   if (isError(grunnlag)) {
     return <ApiException apiResponses={[grunnlag]} />;
   }
@@ -28,6 +36,7 @@ export const SykdomsvurderingBrevMedDataFetching = async ({
       typeBehandling={typeBehandling}
       readOnly={readOnly || !grunnlag.data.kanSaksbehandle}
       behandlingVersjon={behandlingVersjon}
+      initialMellomlagretVurdering={initialMellomlagretVurdering}
     />
   );
 };
