e0356217e67aec33cc99b129450e062cdec43b41
Henrik Gundersen <henrik.gundersen@nav.no>
2025-08-28 15:59:18 +0200

    AAP-1179 Slår på steg for sykdomsvurdering for brev i prod

--- components/behandlinger/brev/skriveBrev/SaksopplysningerKolonne.tsx
+++ components/behandlinger/brev/skriveBrev/SaksopplysningerKolonne.tsx
@@ -1,53 +1,24 @@
 import { SaksopplysningerKort } from 'components/behandlinger/brev/skriveBrev/SaksopplysningerKort';
 import styles from './SaksopplysningerKolonne.module.css';
 import { Label } from '@navikt/ds-react';
-import {
-  BistandsGrunnlag,
-  RefusjonskravGrunnlag,
-  SykdomsGrunnlag,
-  SykdomsvurderingBrevGrunnlag,
-} from 'lib/types/types';
+import { RefusjonskravGrunnlag, SykdomsvurderingBrevGrunnlag } from 'lib/types/types';
 import { formaterDatoForFrontend } from 'lib/utils/date';
 import { parse } from 'date-fns';
-import { isDev, isLocal, isProd } from 'lib/utils/environment';
 
 interface Props {
-  sykdomsgrunnlag: SykdomsGrunnlag;
-  bistandsbehovGrunnlag: BistandsGrunnlag;
   refusjonGrunnlag: RefusjonskravGrunnlag;
   sykdomsvurderingBrevGrunnlag?: SykdomsvurderingBrevGrunnlag;
 }
 
-export const SaksopplysningerKolonne = ({
-  sykdomsgrunnlag,
-  bistandsbehovGrunnlag,
-  refusjonGrunnlag,
-  sykdomsvurderingBrevGrunnlag,
-}: Props) => {
-  const gjeldendeSykdomsvurdering = sykdomsgrunnlag.sykdomsvurderinger[sykdomsgrunnlag.sykdomsvurderinger.length - 1];
-  const gjeldendeBistandsbehov = bistandsbehovGrunnlag.vurdering;
+export const SaksopplysningerKolonne = ({ refusjonGrunnlag, sykdomsvurderingBrevGrunnlag }: Props) => {
   const gjeldendeSykdomsvurderingForBrev = sykdomsvurderingBrevGrunnlag?.vurdering?.vurdering;
   const refusjonVurdering = refusjonGrunnlag.gjeldendeVurdering;
 
   return (
     <div className={styles.kolonne}>
       <Label as="p">Vilkårsvurderinger</Label>
-      {/* TODO fjern toggles når verifisert */}
-      {isProd() && gjeldendeSykdomsvurdering && (
-        <SaksopplysningerKort
-          tittel="§11-5 Nedsatt arbeidsevne og krav til årsakssammenheng"
-          begrunnelse={gjeldendeSykdomsvurdering.begrunnelse}
-        />
-      )}
-      {/* TODO fjern toggles når verifisert */}
-      {isProd() && gjeldendeBistandsbehov && (
-        <SaksopplysningerKort
-          tittel="§11-6 Behov for bistand til å skaffe seg eller beholde arbeid"
-          begrunnelse={gjeldendeBistandsbehov.begrunnelse}
-        />
-      )}
-      {/* TODO fjern toggles når verifisert */}
-      {(isDev() || isLocal()) && gjeldendeSykdomsvurderingForBrev && (
+
+      {gjeldendeSykdomsvurderingForBrev && (
         <SaksopplysningerKort
           tittel="Individuell begrunnelse for §§ 11-5 og 11-6"
           begrunnelse={gjeldendeSykdomsvurderingForBrev}
