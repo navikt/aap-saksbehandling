1d72c5b937e5f22b2fd9bceca715549a1517765c
charliemidtlyng <charlie.midtlyng@bekk.no>
2025-08-29 13:53:29 +0200

    Info-logging ved feil pga opprettelse av dummysaker

--- app/saksbehandling/api/test/opprettDummySak/route.ts
+++ app/saksbehandling/api/test/opprettDummySak/route.ts
@@ -1,7 +1,7 @@
 import { opprettDummySakTest } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
-import { logError } from 'lib/serverutlis/logger';
+import { logInfo } from 'lib/serverutlis/logger';
 import { isError } from 'lib/utils/api';
 
 export async function POST(req: NextRequest) {
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
 
   const res = await opprettDummySakTest(body);
   if (isError(res)) {
-    logError(`/test/opprettDummySak/, status: ${res.status}, message: ${res.apiException.message}`);
+    logInfo(`/test/opprettDummySak/, status: ${res.status}, message: ${res.apiException.message}`);
   }
   revalidatePath('/saksoversikt', 'page');
   return new Response(JSON.stringify(res), { status: res.status });
