8d69d476d22cfb82d39a14edde01293710cf11d7
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2025-09-01 20:00:03 +0200

    AAP-1321 Mellomlagring i Klagebehandling vurdering kontor

--- components/behandlinger/klage/klagebehandlingkontor/klagebehandlingkontor/KlagebehandlingVurderingKontorMedDataFetching.tsx
+++ components/behandlinger/klage/klagebehandlingkontor/klagebehandlingkontor/KlagebehandlingVurderingKontorMedDataFetching.tsx
@@ -1,8 +1,12 @@
 import { TypeBehandling } from 'lib/types/types';
 import { KlagebehandlingVurderingKontor } from './KlagebehandlingVurderingKontor';
-import { hentKlagebehandlingKontorGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import {
+  hentKlagebehandlingKontorGrunnlag,
+  hentMellomlagring,
+} from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { isError } from 'lib/utils/api';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
+import { Behovstype } from 'lib/utils/form';
 
 interface Props {
   behandlingsreferanse: string;
@@ -17,7 +21,11 @@ export const KlagebehandlingVurderingKontorMedDataFetching = async ({
   readOnly,
   typeBehandling,
 }: Props) => {
-  const grunnlag = await hentKlagebehandlingKontorGrunnlag(behandlingsreferanse);
+  const [grunnlag, initialMellomlagretVurdering] = await Promise.all([
+    hentKlagebehandlingKontorGrunnlag(behandlingsreferanse),
+    hentMellomlagring(behandlingsreferanse, Behovstype.VURDER_KLAGE_KONTOR),
+  ]);
+
   if (isError(grunnlag)) {
     return <ApiException apiResponses={[grunnlag]} />;
   }
@@ -27,6 +35,7 @@ export const KlagebehandlingVurderingKontorMedDataFetching = async ({
       behandlingVersjon={behandlingVersjon}
       readOnly={readOnly || !grunnlag.data.harTilgangTilÅSaksbehandle}
       typeBehandling={typeBehandling}
+      initialMellomlagretVurdering={initialMellomlagretVurdering}
     />
   );
 };
