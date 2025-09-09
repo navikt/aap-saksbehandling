c9ea167036173b43969f93aac9f53583dfc3758b
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-09-09 14:58:21 +0200

    test til returgraf

---
+++ components/produksjonsstyring/avklaringsbehovreturer/AvklaringsbehovReturer.test.tsx
@@ -0,0 +1,36 @@
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { AvklaringsbehovReturer } from './AvklaringsbehovReturer';
+
+const user = userEvent.setup();
+
+const data = [
+  {
+    returerPerAvklaringsbehov: [
+      {
+        avklaringsbehov: '5006',
+        returFra: 'SENDT_TILBAKE_FRA_BESLUTTER',
+        returÅrsak: 'MANGLENDE_UTREDNING',
+        antallÅpneBehandlinger: 1,
+        gjennomsnittTidFraRetur: 1291692.105611,
+      },
+    ],
+    totalt: 1,
+  },
+];
+
+describe('AvklaringsbehovReturer', () => {
+  beforeEach(() => {
+    render(<AvklaringsbehovReturer data={data} />);
+  });
+
+  it('kan switche av årsak ', async () => {
+    expect(screen.getByRole('columnheader', { name: /Årsak til retur/i })).toBeInTheDocument();
+
+    const årsakSwitch = screen.getByRole('checkbox', { name: /Årsak til retur/i });
+    await user.click(årsakSwitch);
+
+    expect(screen.queryByRole('columnheader', { name: /Årsak til retur/i })).not.toBeInTheDocument();
+  });
+});
