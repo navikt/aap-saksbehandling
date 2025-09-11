4487241cd561bbef86a4b476f173b652da68461b
Steffen Hageland <steffen.hageland@nav.no>
2025-09-11 17:09:04 +0200

    Steg for oppholdskrav

---
+++ app/saksbehandling/api/behandling/los-periodisert-behov/route.ts
@@ -0,0 +1,29 @@
+import { løsPeriodisertAvklaringsbehov } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { NextRequest } from 'next/server';
+import { getErrorMessage } from 'lib/utils/errorUtil';
+import { logError, logWarning } from 'lib/serverutlis/logger';
+import { isError } from 'lib/utils/api';
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  try {
+    const løsbehovRes = await løsPeriodisertAvklaringsbehov(body);
+
+    if (isError(løsbehovRes)) {
+      if (løsbehovRes.status >= 500) {
+        logError(
+          `/løs-periodisert-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
+        );
+      } else {
+        logWarning(
+          `/løs-periodisert-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
+        );
+      }
+    }
+    return new Response(JSON.stringify(løsbehovRes), { status: løsbehovRes.status });
+  } catch (error) {
+    logWarning(`/løs-periodisert-behov ${body.behov?.behovstype}`, error);
+    return new Response(JSON.stringify({ message: getErrorMessage(error) }), { status: 500 });
+  }
+}
---
+++ components/behandlinger/oppholdskrav/OppholdskravFormInput.tsx
@@ -0,0 +1,126 @@
+import { Button, HStack, ReadMore, VStack } from '@navikt/ds-react';
+import { DateInputWrapper } from 'components/form/dateinputwrapper/DateInputWrapper';
+import { TextAreaWrapper } from 'components/form/textareawrapper/TextAreaWrapper';
+import { RadioGroupJaNei } from 'components/form/radiogroupjanei/RadioGroupJaNei';
+import { JaEllerNei } from 'lib/utils/form';
+import { ComboboxWrapper } from 'components/form/comboboxwrapper/ComboboxWrapper';
+import { isNotEmpty } from 'components/behandlinger/oppholdskrav/oppholdskrav-utils';
+import { TextFieldWrapper } from 'components/form/textfieldwrapper/TextFieldWrapper';
+import { VisningModus } from 'hooks/saksbehandling/visning/VisningHook';
+import { TrashIcon } from '@navikt/aksel-icons';
+import { alleLandUtenNorge } from 'lib/utils/countries';
+import { UseFormReturn } from 'react-hook-form';
+import { OppholdskravForm } from 'components/behandlinger/oppholdskrav/types';
+import { useState } from 'react';
+import { validerDato } from 'lib/validation/dateValidation';
+
+type Props = {
+  form: UseFormReturn<OppholdskravForm>;
+  visningModus: VisningModus;
+  readOnly: boolean;
+  index: number;
+  harTidligereVurderinger: boolean;
+  onRemove: () => void;
+};
+
+const landSelectOptions = [alleLandUtenNorge[0], { label: 'Annet', value: 'ANNET' }, ...alleLandUtenNorge.slice(1)];
+
+export const OppholdskravFormInput = ({
+  readOnly,
+  harTidligereVurderinger,
+  index,
+  visningModus,
+  form,
+  onRemove,
+}: Props) => {
+  const { control, watch } = form;
+  const [spinnerRemove, setSpinnerRemove] = useState(false);
+
+  const handleRemove = (): void => {
+    setSpinnerRemove(true);
+    setTimeout(() => {
+      onRemove();
+      setSpinnerRemove(false);
+    }, 500);
+  };
+
+  return (
+    <VStack gap="4">
+      <DateInputWrapper
+        name={`vurderinger.${index}.fraDato`}
+        label="Vurderingen gjelder fra"
+        control={control}
+        rules={{
+          required: 'Du må velge fra hvilken dato vurderingen gjelder fra',
+          validate: (value) => validerDato(value as string),
+        }}
+        readOnly={readOnly || (index === 0 && !harTidligereVurderinger)}
+      />
+      <ReadMore size={'small'} header="Hvordan legge til sluttdato?">
+        For å legge til en sluttdato på denne vurderingen velger du “Legg til ny vurdering”. Det oppretter en ny
+        vurdering, der du kan ha et annet utfall og en ny “gjelder fra” dato, som da vil gi sluttdato på den foregående
+        (denne) vurderingen. Sluttdatoen for denne vurderingen blir satt til dagen før den nye vurderingen sin “gjelder
+        fra” dato.
+      </ReadMore>
+      <TextAreaWrapper
+        name={`vurderinger.${index}.begrunnelse`}
+        control={control}
+        label="Vilkårsvurdering"
+        rules={{
+          required: 'Du må fylle ut en vilkårsvurdering',
+        }}
+        readOnly={readOnly}
+      />
+      <RadioGroupJaNei
+        name={`vurderinger.${index}.oppfyller`}
+        control={control}
+        label="Oppfyller brukeren vilkårene i § 11-3?"
+        horisontal={true}
+        rules={{ required: 'Du må ta stilling til om brukeren oppfyller vilkårene' }}
+        readOnly={readOnly}
+      />
+
+      {watch(`vurderinger.${index}.oppfyller`) === JaEllerNei.Nei && (
+        <>
+          <HStack>
+            <ComboboxWrapper
+              name={`vurderinger.${index}.land`}
+              control={control}
+              label="Hvilket land oppholder brukeren seg i?"
+              options={landSelectOptions}
+              rules={{ validate: (value) => (isNotEmpty(value) ? undefined : 'Du må velge et land') }}
+              readOnly={readOnly}
+            />
+          </HStack>
+
+          {watch(`vurderinger.${index}.land`) === 'ANNET' && (
+            <HStack>
+              <TextFieldWrapper
+                name={`vurderinger.${index}.landAnnet`}
+                control={control}
+                type="text"
+                label="Navn på land"
+                rules={{ required: 'Du må skrive inn navnet på landet' }}
+                readOnly={readOnly}
+              />
+            </HStack>
+          )}
+        </>
+      )}
+      {(visningModus === VisningModus.AKTIV_MED_AVBRYT || visningModus === VisningModus.AKTIV_UTEN_AVBRYT) &&
+        (index !== 0 || harTidligereVurderinger) && (
+          <HStack>
+            <Button
+              loading={spinnerRemove}
+              aria-label="Fjern vurdering"
+              variant="secondary"
+              size="small"
+              icon={<TrashIcon />}
+              onClick={handleRemove}
+              type="button"
+            ></Button>
+          </HStack>
+        )}
+    </VStack>
+  );
+};
