0cc4eacdcc3d2e28430e479ab0f60a7541ba4d53
Joacim Thomassen <joacim.thomassen@nav.no>
2025-09-12 10:58:01 +0200

    AAP-1308: Korriger visning av refusjonskrav i brevsteget

--- components/behandlinger/brev/skriveBrev/SaksopplysningerKolonne.tsx
+++ components/behandlinger/brev/skriveBrev/SaksopplysningerKolonne.tsx
@@ -1,7 +1,7 @@
 import { SaksopplysningerKort } from 'components/behandlinger/brev/skriveBrev/SaksopplysningerKort';
 import styles from './SaksopplysningerKolonne.module.css';
 import { Label } from '@navikt/ds-react';
-import { RefusjonskravGrunnlag, SykdomsvurderingBrevGrunnlag } from 'lib/types/types';
+import { RefusjonskravGrunnlag, SykdomsvurderingBrevGrunnlag, RefusjonkravVurderingResponse } from 'lib/types/types';
 import { formaterDatoForFrontend } from 'lib/utils/date';
 import { parse } from 'date-fns';
 
@@ -10,9 +10,25 @@ interface Props {
   sykdomsvurderingBrevGrunnlag?: SykdomsvurderingBrevGrunnlag;
 }
 
+// Using own groupBy for now.. replace with ES2024's Map.groupBy later..
+function groupByPreES2024<T, K>(array: T[], getKey: (item: T) => K): Map<K, T[]> {
+  const result = new Map<K, T[]>();
+  for (const item of array) {
+    const key = getKey(item);
+    if (!result.has(key)) {
+      result.set(key, []);
+    }
+    result.get(key)!.push(item);
+  }
+  return result;
+}
+
 export const SaksopplysningerKolonne = ({ refusjonGrunnlag, sykdomsvurderingBrevGrunnlag }: Props) => {
   const gjeldendeSykdomsvurderingForBrev = sykdomsvurderingBrevGrunnlag?.vurdering?.vurdering;
-  const refusjonVurderinger = refusjonGrunnlag.gjeldendeVurderinger;
+  const refusjonVurderinger = refusjonGrunnlag?.gjeldendeVurderinger?.filter(vurdering => vurdering.harKrav === true);
+  const refusjonVurderingerNavKontorGruppert : Map<string, RefusjonkravVurderingResponse[]> =
+    (refusjonVurderinger && refusjonVurderinger.length > 0) ?
+        groupByPreES2024(refusjonVurderinger, (vurdering) => vurdering.navKontor) : new Map();
 
   return (
     <div className={styles.kolonne}>
@@ -24,29 +40,30 @@ export const SaksopplysningerKolonne = ({ refusjonGrunnlag, sykdomsvurderingBrev
           begrunnelse={gjeldendeSykdomsvurderingForBrev}
         />
       )}
-      {refusjonVurderinger &&
-        refusjonVurderinger.length > 0 &&
-        refusjonVurderinger
-          .filter((refusjonVurdering) => refusjonVurdering.harKrav === true)
-          .map((refusjonVurdering, index) => {
-            return (
+      {refusjonVurderinger
+          && refusjonVurderinger.length > 0 && (
               <SaksopplysningerKort
-                key={refusjonVurdering.navKontor ?? index}
-                tittel={`Refusjonskrav ${refusjonVurdering.navKontor}`}
-                begrunnelse={`Det er refusjonskrav mot sosialstønad. Refusjonskravet gjelder fra
-                              ${
-                                refusjonVurdering.fom
-                                  ? formaterDatoForFrontend(parse(refusjonVurdering.fom, 'yyyy-MM-dd', new Date()))
-                                  : '-'
-                              }
-                              ${
-                                refusjonVurdering.tom
-                                  ? `til ${formaterDatoForFrontend(parse(refusjonVurdering.tom, 'yyyy-MM-dd', new Date()))}`
-                                  : ''
-                              }`}
+                tittel={`Refusjonskrav`}
+                begrunnelse={`Det er refusjonskrav mot sosialstønad. Refusjonskravet gjelder:
+                             ${Array.from(refusjonVurderingerNavKontorGruppert.entries())
+                                 .map(([navKontor, vurderinger]) => `
+${navKontor}${vurderinger
+    .map(vurdering => `
+          ${
+            vurdering.fom
+              ? formaterDatoForFrontend(parse(vurdering.fom, 'yyyy-MM-dd', new Date()))
+              : '-'
+          } til ${
+             vurdering.tom
+               ? formaterDatoForFrontend(parse(vurdering.tom, 'yyyy-MM-dd', new Date()))
+               : ''
+          }`).join('')}
+                                 `).join('')
+                             }
+                `}
               />
-            );
-          })}
+        )
+      }
     </div>
   );
 };
