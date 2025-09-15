2c1d39705cf25a61a609430578da73f5b8cec86b
Tor Idland <90849097+tidnav@users.noreply.github.com>
2025-09-15 15:08:21 +0200

    AAP-1399 Manuell skadedato på yrkesskade (#1186)
    * Kun typeendringer for manuell yrkesskade
    
    * Ny komponent YrkesskadeVurderingTabell som støtter manglendeYrkesskadeDato
    
    * Få DateInputWrapper til å funke
    
    * validering på manuellyrkesskadedato
    
    Co-authored-by: Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
    
    * feature toggle isdev for å vise yrkesskade med manuell skadedato
    
    Co-authored-by: Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
    
    * unødvendig kommentar
    
    ---------
    
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>
    Co-authored-by: Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>

---
+++ components/behandlinger/sykdom/yrkesskade/YrkesskadeVurderingTabell.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { useFieldArray, UseFormReturn } from 'react-hook-form';
+import { TableStyled } from 'components/tablestyled/TableStyled';
+import { Checkbox, Table } from '@navikt/ds-react';
+import { formaterDatoForFrontend } from 'lib/utils/date';
+import { DateInputWrapper } from 'components/form/dateinputwrapper/DateInputWrapper';
+import { validerDato } from 'lib/validation/dateValidation';
+import {
+  YrkesskadeMedSkadeDatoFormFields,
+  YrkesskadeMedSkadeDatoSak,
+} from 'components/behandlinger/sykdom/yrkesskade/YrkesskadeMedManuellYrkesskadeDato';
+
+interface Props {
+  form: UseFormReturn<YrkesskadeMedSkadeDatoFormFields>;
+  readOnly: boolean;
+}
+
+export const YrkesskadeVurderingTabell = ({ form, readOnly }: Props) => {
+  let { fields: relevanteYrkesskadeSaker, update } = useFieldArray({
+    name: 'relevanteYrkesskadeSaker',
+    control: form.control,
+  });
+
+  function oppdaterTilknytning(index: number, erTilknyttet: boolean, yrkesskade: YrkesskadeMedSkadeDatoSak) {
+    update(index, {
+      ref: yrkesskade.ref,
+      kilde: yrkesskade.kilde,
+      skadedato: yrkesskade.skadedato,
+      manuellYrkesskadeDato: yrkesskade.manuellYrkesskadeDato,
+      saksnummer: yrkesskade.saksnummer,
+      erTilknyttet,
+    });
+  }
+
+  return (
+    <TableStyled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell textSize={'small'}>Tilknytt yrkesskade</Table.HeaderCell>
+          <Table.HeaderCell textSize={'small'}>Saksnummer</Table.HeaderCell>
+          <Table.HeaderCell textSize={'small'}>Kilde</Table.HeaderCell>
+          <Table.HeaderCell textSize={'small'}>Skadedato</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      {relevanteYrkesskadeSaker.length > 0 && (
+        <Table.Body>
+          {relevanteYrkesskadeSaker.map((yrkesskade, index) => (
+            <Table.Row key={yrkesskade.ref}>
+              <Table.DataCell textSize={'small'}>
+                <Checkbox
+                  size={'small'}
+                  hideLabel
+                  value={yrkesskade.ref}
+                  checked={yrkesskade.erTilknyttet}
+                  onChange={(e) => oppdaterTilknytning(index, e.target.checked, yrkesskade)}
+                  readOnly={readOnly}
+                >
+                  Tilknytt yrkesskade til vurdering
+                </Checkbox>
+              </Table.DataCell>
+              <Table.DataCell textSize={'small'}>{yrkesskade.saksnummer}</Table.DataCell>
+              <Table.DataCell textSize={'small'}>{yrkesskade.kilde}</Table.DataCell>
+              <Table.DataCell textSize={'small'}>
+                {yrkesskade.skadedato ? (
+                  formaterDatoForFrontend(yrkesskade.skadedato)
+                ) : (
+                  <DateInputWrapper
+                    name={`relevanteYrkesskadeSaker.${index}.manuellYrkesskadeDato`}
+                    control={form.control}
+                    readOnly={!yrkesskade.erTilknyttet || readOnly}
+                    rules={{
+                      validate: (value) => {
+                        if (!yrkesskade.erTilknyttet) {
+                          return;
+                        } else if (!value) {
+                          return 'Du må angi dato for yrkesskade';
+                        }
+                        return validerDato(value as string);
+                      },
+                    }}
+                    hideLabel={true}
+                  />
+                )}
+              </Table.DataCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      )}
+    </TableStyled>
+  );
+};
