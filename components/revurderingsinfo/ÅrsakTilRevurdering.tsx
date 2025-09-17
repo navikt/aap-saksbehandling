53520aba9cc1d80a6b6b42c9469a96a210935622
thao1 <thao.than.nguyen@nav.no>
2025-09-17 11:11:53 +0200

    AAP-1151: Fjernet visning av 'Revurdering avbrutt' fra blå boks 'Årsak til revurdering'

--- components/revurderingsinfo/ÅrsakTilRevurdering.tsx
+++ components/revurderingsinfo/ÅrsakTilRevurdering.tsx
@@ -22,45 +22,37 @@ export const ÅrsakTilRevurdering = ({ vurderingsbehovOgÅrsaker }: Props) => {
       style={{ backgroundColor: 'var(--a-surface-info-subtle)' }}
     >
       <ExpansionCard.Header>
-        <>
-          <HStack wrap={false} gap="4" align="center">
-            <TasklistStartIcon aria-hidden fontSize={'1.5rem'} />
-            <ExpansionCard.Title size={'small'}>
-              <Label>{tittel}</Label>
-            </ExpansionCard.Title>
-          </HStack>
-        </>
+        <HStack wrap={false} gap="4" align="center">
+          <TasklistStartIcon aria-hidden fontSize={'1.5rem'} />
+          <ExpansionCard.Title size={'small'}>
+            <Label>{tittel}</Label>
+          </ExpansionCard.Title>
+        </HStack>
       </ExpansionCard.Header>
       <ExpansionCard.Content>
         <VStack gap={'3'}>
-          {vurderingsbehovOgÅrsaker.map((vurderingsbehovOgÅrsak, index) => {
-            const opprettetTid = formaterDatoForFrontend(vurderingsbehovOgÅrsak.opprettet ?? '');
-
-            const årsakTilOpprettelseTekst = mapTilÅrsakTilOpprettelseTilTekst(vurderingsbehovOgÅrsak.årsak);
-            const erManueltAvbrutt =
-              vurderingsbehovOgÅrsak.vurderingsbehov.some((v) => v.type === 'REVURDERING_AVBRUTT') &&
-              årsakTilOpprettelseTekst === 'Manuell opprettelse';
-
-            return (
-              <Box key={index}>
-                <HStack gap={'2'} align={'end'}>
-                  <Label size={'small'}>
-                    {vurderingsbehovOgÅrsak.vurderingsbehov
-                      .map((vurderingsbehov) => formaterVurderingsbehov(vurderingsbehov.type))
-                      .join(', ')}
-                  </Label>
-                  <Detail textColor={'subtle'}>
-                    {erManueltAvbrutt
-                      ? `Manuelt avbrutt ${opprettetTid}`
-                      : `${årsakTilOpprettelseTekst} ${opprettetTid}`}
-                  </Detail>
-                </HStack>
-                {vurderingsbehovOgÅrsak.beskrivelse && (
-                  <BodyLong size={'small'}>Begrunnelse: {vurderingsbehovOgÅrsak.beskrivelse}</BodyLong>
-                )}
-              </Box>
-            );
-          })}
+          {vurderingsbehovOgÅrsaker
+            .filter(({ vurderingsbehov }) =>
+              !vurderingsbehov.some((v) => v.type === 'REVURDERING_AVBRUTT')
+            )
+            .map(({ vurderingsbehov, opprettet, årsak, beskrivelse }, index) => {
+              const opprettetTid = formaterDatoForFrontend(opprettet ?? '');
+              return (
+                <Box key={index}>
+                  <HStack gap="2" align="end">
+                    <Label size="small">
+                      {vurderingsbehov.map((v) => formaterVurderingsbehov(v.type)).join(', ')}
+                    </Label>
+                    <Detail textColor="subtle">
+                      {mapTilÅrsakTilOpprettelseTilTekst(årsak)} {opprettetTid}
+                    </Detail>
+                  </HStack>
+                  {beskrivelse && (
+                    <BodyLong size="small">Begrunnelse: {beskrivelse}</BodyLong>
+                  )}
+                </Box>
+              );
+            })}
         </VStack>
       </ExpansionCard.Content>
     </ExpansionCard>
