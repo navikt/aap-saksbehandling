1409c07e376d592e11b6211c16fb8883b1eea8ec
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-09-17 11:19:07 +0200

    Ikke logg utdatert versjonsnummer som error

--- app/postmottak/api/post/los-behov/route.ts
+++ app/postmottak/api/post/los-behov/route.ts
@@ -1,8 +1,8 @@
-import { løsAvklaringsbehov } from 'lib/services/postmottakservice/postmottakservice';
-import { NextRequest } from 'next/server';
-import { logError, logWarning } from 'lib/serverutlis/logger';
-import { isError } from 'lib/utils/api';
-import { getErrorMessage } from 'lib/utils/errorUtil';
+import {løsAvklaringsbehov} from 'lib/services/postmottakservice/postmottakservice';
+import {NextRequest} from 'next/server';
+import {logError, logInfo, logWarning} from 'lib/serverutlis/logger';
+import {ApiException, ErrorResponseBody, isError} from 'lib/utils/api';
+import {getErrorMessage} from 'lib/utils/errorUtil';
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -11,13 +11,30 @@ export async function POST(req: NextRequest) {
     const løsbehovRes = await løsAvklaringsbehov(body);
 
     if (isError(løsbehovRes)) {
-      logError(
-        `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
-      );
+      if (shouldLogError(løsbehovRes)) {
+        logError(
+          `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
+        );
+      } else {
+        logInfo(
+          `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
+        );
+      }
     }
-    return new Response(JSON.stringify(løsbehovRes), { status: løsbehovRes.status });
+    return new Response(JSON.stringify(løsbehovRes), {status: løsbehovRes.status});
   } catch (error) {
     logWarning(`/løs-behov ${body.behov?.behovstype}`, error);
-    return new Response(JSON.stringify({ message: getErrorMessage(error) }), { status: 500 });
+    return new Response(JSON.stringify({message: getErrorMessage(error)}), {status: 500});
   }
+}
+
+const ignoreMessages = [
+  /Behandlingen har blitt oppdatert. Versjonsnummer\[\d+] ulikt fra siste\[\d+]/,
+  /Forsøker å løse avklaringsbehov .+\(kode='.+'\) som er definert i et steg etter nåværende steg\[.+] .+\(.+\). Skal løses i steg\[.+]/,
+]
+function shouldLogError(response: ErrorResponseBody<ApiException>): boolean {
+  if (ignoreMessages.some(regexp => regexp.test(response.apiException.message))) {
+    return false
+  }
+  return true
 }
