7236f90ae51aa943f7317e4c74f3cec9097dfd4e
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-09-17 13:08:03 +0200

    Fjern error-logging hvis løsAvklaringsbehov feiler
    For struktuererte feilmeldinger fra behandlingsflyt, så har
    behandlingsflyt allerede logget error om nødvendig.

--- app/postmottak/api/post/los-behov/route.ts
+++ app/postmottak/api/post/los-behov/route.ts
@@ -1,7 +1,7 @@
 import {løsAvklaringsbehov} from 'lib/services/postmottakservice/postmottakservice';
 import {NextRequest} from 'next/server';
-import {logError, logInfo, logWarning} from 'lib/serverutlis/logger';
-import {ApiException, ErrorResponseBody, isError} from 'lib/utils/api';
+import {logInfo, logWarning} from 'lib/serverutlis/logger';
+import {isError} from 'lib/utils/api';
 import {getErrorMessage} from 'lib/utils/errorUtil';
 
 export async function POST(req: NextRequest) {
@@ -11,30 +11,18 @@ export async function POST(req: NextRequest) {
     const løsbehovRes = await løsAvklaringsbehov(body);
 
     if (isError(løsbehovRes)) {
-      if (shouldLogError(løsbehovRes)) {
-        logError(
-          `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
-        );
-      } else {
-        logInfo(
-          `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
-        );
-      }
+      /* Dette er et strukturert error-objekt som er konstruert av behandlingsflyt.
+       * Behandlingsflyt vil alltid ta en vurdering om det trengs å logge en error.
+       * Så hvis det er behov for å logge error, så har behandlingsflyt logget error.
+       * Derfor er det alltid tilstrekkelig å logge det som info her.
+       */
+      logInfo(
+        `/postmottak/løs-behov, behovstype: ${body.behov?.behovstype}, message: ${løsbehovRes.apiException.message}`
+      );
     }
     return new Response(JSON.stringify(løsbehovRes), {status: løsbehovRes.status});
   } catch (error) {
     logWarning(`/løs-behov ${body.behov?.behovstype}`, error);
     return new Response(JSON.stringify({message: getErrorMessage(error)}), {status: 500});
   }
-}
-
-const ignoreMessages = [
-  /Behandlingen har blitt oppdatert. Versjonsnummer\[\d+] ulikt fra siste\[\d+]/,
-  /Forsøker å løse avklaringsbehov .+\(kode='.+'\) som er definert i et steg etter nåværende steg\[.+] .+\(.+\). Skal løses i steg\[.+]/,
-]
-function shouldLogError(response: ErrorResponseBody<ApiException>): boolean {
-  if (ignoreMessages.some(regexp => regexp.test(response.apiException.message))) {
-    return false
-  }
-  return true
 }
