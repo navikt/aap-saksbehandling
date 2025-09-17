a3b88458647436834cb2e5a5aea22678fecff5f4
Tor Idland <tor.idland@nav.no>
2025-09-17 12:59:46 +0200

    Forbedret visning i ny sakshistorikk (dev)

--- components/sakshistorikk/BehandlingsHendelse.tsx
+++ components/sakshistorikk/BehandlingsHendelse.tsx
@@ -1,5 +1,5 @@
 import { BehandlingsHistorikk, Vurderingsbehov } from 'lib/types/types';
-import { BodyShort, Detail, Process, VStack } from '@navikt/ds-react';
+import { BodyShort, Detail, Label, Process, VStack } from '@navikt/ds-react';
 import { formaterDatoMedTidspunktForFrontend } from 'lib/utils/date';
 import { HistorikkEvent, mapEventTilIkon, mapEventTilString } from 'components/sakshistorikk/oversettelser';
 import { mapGrunnTilString, mapTilVenteÅrsakTekst } from 'lib/utils/oversettelser';
@@ -17,44 +17,42 @@ export const BehandlingsHendelse = ({ hendelse }: Props) => {
   return (
     <Process.Event {...ikonProp}>
       <div>
-        <BodyShort size={'small'}>{mapEventTilString(hendelse.hendelse as HistorikkEvent)}</BodyShort>
-        <Detail textColor={'subtle'}>
+        <Label size={'medium'}>{mapEventTilString(hendelse.hendelse as HistorikkEvent)}</Label>
+        <BodyShort size={'small'} textColor={'subtle'}>
           {`${formaterDatoMedTidspunktForFrontend(hendelse.tidspunkt)} ${mapUtførtAvTilTekst(hendelse.utførtAv) && '· '}${mapUtførtAvTilTekst(hendelse.utførtAv)}`}
-        </Detail>
+        </BodyShort>
         <VStack gap={'2'} paddingBlock={'2 2'}>
-          <section>
-            {finnesÅrsak && (
-              <>
-                <Detail weight={'semibold'}>Årsak</Detail>
-                {hendelse.årsakTilSattPåVent && <Detail>{mapTilVenteÅrsakTekst(hendelse.årsakTilSattPåVent)}</Detail>}
-                <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
-                  {hendelse.årsakerTilRetur.map((årsak, i) => (
-                    <li key={i}>
-                      <Detail> {årsak.årsak === 'ANNET' ? årsak.årsakFritekst : mapGrunnTilString(årsak.årsak)}</Detail>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-            {finnesVurderingsbehov && (
-              <>
-                <Detail weight={'semibold'}>Vurderingsbehov</Detail>
-                <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
-                  {hendelse.årsakerTilOpprettelse.map((årsak, i) => (
-                    <li key={i}>
-                      <Detail>{formaterVurderingsbehov(årsak as Vurderingsbehov)}</Detail>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-            {hendelse.begrunnelse && (
-              <VStack>
-                <Detail weight={'semibold'}>Begrunnelse</Detail>
-                <BodyShort size={'small'}>{hendelse.begrunnelse}</BodyShort>
-              </VStack>
-            )}
-          </section>
+          {finnesÅrsak && (
+            <VStack>
+              <Detail weight={'semibold'}>Årsak</Detail>
+              {hendelse.årsakTilSattPåVent && <Detail>{mapTilVenteÅrsakTekst(hendelse.årsakTilSattPåVent)}</Detail>}
+              <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
+                {hendelse.årsakerTilRetur.map((årsak, i) => (
+                  <li key={i}>
+                    <Detail> {årsak.årsak === 'ANNET' ? årsak.årsakFritekst : mapGrunnTilString(årsak.årsak)}</Detail>
+                  </li>
+                ))}
+              </ul>
+            </VStack>
+          )}
+          {finnesVurderingsbehov && (
+            <VStack>
+              <Detail weight={'semibold'}>Vurderingsbehov</Detail>
+              <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
+                {hendelse.årsakerTilOpprettelse.map((årsak, i) => (
+                  <li key={i}>
+                    <Detail>{formaterVurderingsbehov(årsak as Vurderingsbehov)}</Detail>
+                  </li>
+                ))}
+              </ul>
+            </VStack>
+          )}
+          {hendelse.begrunnelse && (
+            <VStack>
+              <Detail weight={'semibold'}>Begrunnelse</Detail>
+              <Detail>{hendelse.begrunnelse}</Detail>
+            </VStack>
+          )}
         </VStack>
       </div>
     </Process.Event>
@@ -65,7 +63,9 @@ function mapUtførtAvTilTekst(utførtAv?: string | null) {
   switch (utførtAv) {
     case 'Kelvin':
       return 'Automatisk';
-    default:
+    case null:
       return '';
+    default:
+      return utførtAv;
   }
 }
