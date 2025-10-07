ba4f11389c6aadce9a25764790c96f91a47fa54a
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-10-07 10:31:44 +0200

    Skal kun kunne opprette aktivitetspliktbehandling dersom det finnes en avsluttet førstegangsbehandling * Trekker ut hjelpemetoder for behandling

--- components/saksoversikt/SakMedBehandlinger.tsx
+++ components/saksoversikt/SakMedBehandlinger.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button, Heading, HStack, Table, VStack } from '@navikt/ds-react';
-import { SaksInfo, ÅrsakTilOpprettelse } from 'lib/types/types';
+import { SaksInfo } from 'lib/types/types';
 import { capitalize } from 'lodash';
 import { SakDevTools } from 'components/saksoversikt/SakDevTools';
 import { useRouter } from 'next/navigation';
@@ -9,81 +9,23 @@ import { formaterDatoMedTidspunktForFrontend } from 'lib/utils/date';
 import { BehandlingButtons } from 'components/saksoversikt/BehandlingButtons';
 import { isLocal } from 'lib/utils/environment';
 import { formaterVurderingsbehov } from 'lib/utils/vurderingsbehov';
-
-const formaterBehandlingType = (behandlingtype: string) => {
-  switch (behandlingtype) {
-    case 'ae0034':
-      return 'Førstegangsbehandling';
-    case 'ae0028':
-      return 'Revurdering';
-    case 'ae0058':
-      return 'Klage';
-    case 'svar-fra-andreinstans':
-      return 'Svar fra Nav Klageinstans';
-    case 'oppfølgingsbehandling':
-      return 'Oppfølgingsoppgave';
-    case 'aktivitetsplikt':
-      return 'Aktivitetsplikt § 11-7';
-    case 'aktivitetsplikt11-9':
-      return 'Aktivitetsplikt § 11-9';
-    default:
-      return `Ukjent behandlingtype (${behandlingtype})`;
-  }
-};
-
-const formatterÅrsakTilOpprettelseTilTekst = (årsakTilOpprettelse: ÅrsakTilOpprettelse): string => {
-  switch (årsakTilOpprettelse) {
-    case 'SØKNAD':
-      return 'Søknad';
-    case 'MANUELL_OPPRETTELSE':
-      return 'Manuelt opprettet';
-    case 'HELSEOPPLYSNINGER':
-      return 'Helseopplysninger';
-    case 'ANNET_RELEVANT_DOKUMENT':
-      return 'Annet relevant dokument';
-    case 'OMGJØRING_ETTER_KLAGE':
-      return 'Omgjøring etter klage';
-    case 'OMGJØRING_ETTER_SVAR_FRA_KLAGEINSTANS':
-      return 'Omgjøring etter svar fra klageinstans';
-    case 'FASTSATT_PERIODE_PASSERT':
-      return 'Fastsatt periode passert';
-    case 'FRITAK_MELDEPLIKT':
-      return 'Fritak meldeplikt';
-    case 'MELDEKORT':
-      return 'Meldekort';
-    case 'AKTIVITETSMELDING':
-      return 'Aktivitetsmelding';
-    case 'OPPFØLGINGSOPPGAVE':
-      return 'Manuelt opprettet';
-    case 'OPPFØLGINGSOPPGAVE_SAMORDNING_GRADERING':
-      return 'Maksdato annen full ytelse';
-    case 'SVAR_FRA_KLAGEINSTANS':
-      return 'Svar fra klageinstans';
-    case 'KLAGE':
-      return 'Klage';
-    case 'ENDRING_I_REGISTERDATA':
-      return 'Endring i register';
-    case 'AKTIVITETSPLIKT':
-      return 'Aktivitetsplikt';
-    default:
-      return 'Ukjent årsak';
-  }
-};
+import {
+  Behandlingstype,
+  erAvsluttetFørstegangsbehandling,
+  erFørstegangsbehandling,
+  erTrukket,
+  formaterBehandlingType,
+  formatterÅrsakTilOpprettelseTilTekst,
+} from 'lib/utils/behandling';
 
 export const SakMedBehandlinger = ({ sak }: { sak: SaksInfo }) => {
   const router = useRouter();
 
-  const kanRevurdere = !!sak.behandlinger.filter(
-    (behandling) =>
-      behandling.type === 'ae0034' &&
-      behandling.status !== 'OPPRETTET' &&
-      !behandling.vurderingsbehov.includes('SØKNAD_TRUKKET')
-  ).length;
+  const kanRevurdere = sak.behandlinger.some(
+    (behandling) => erFørstegangsbehandling(behandling) && behandling.status !== 'OPPRETTET' && !erTrukket(behandling)
+  );
 
-  const kanRegistrerebrudd =
-    sak.behandlinger.filter(
-      (behandling) => behandling.type === 'ae0034' && !behandling.vurderingsbehov.includes('SØKNAD_TRUKKET')
-    ).length > 0;
+  const kanRegistrerebrudd = sak.behandlinger.some((behandling) => erAvsluttetFørstegangsbehandling(behandling));
 
   return (
     <VStack gap="8">
@@ -146,7 +88,7 @@ export const SakMedBehandlinger = ({ sak }: { sak: SaksInfo }) => {
           {sak?.behandlinger?.map((behandling) => (
             <Table.Row key={behandling.referanse}>
               <Table.DataCell>{formaterDatoMedTidspunktForFrontend(behandling.opprettet)}</Table.DataCell>
-              <Table.DataCell>{formaterBehandlingType(behandling.type)}</Table.DataCell>
+              <Table.DataCell>{formaterBehandlingType(behandling.type as Behandlingstype)}</Table.DataCell>
               <Table.DataCell>{formatterÅrsakTilOpprettelseTilTekst(behandling.årsakTilOpprettelse)}</Table.DataCell>
               <Table.DataCell>{capitalize(behandling.status)}</Table.DataCell>
               <Table.DataCell>
---
+++ lib/utils/behandling.ts
@@ -0,0 +1,88 @@
+import { BehandlingInfo, ÅrsakTilOpprettelse } from 'lib/types/types';
+
+// TODO: Oppdater BehandlinginfoDTO til å returnere enum i stedet for string slik at vi kan få disse fra backend
+export enum Behandlingstype {
+  Førstegangsbehandling = 'ae0034',
+  Revurdering = 'ae0028',
+  Klage = 'ae0058',
+  SvarFraAndreinstans = 'svar-fra-andreinstans',
+  Oppfølgingsbehandling = 'oppfølgingsbehandling',
+  Aktivitetsplikt11_7 = 'aktivitetsplikt',
+  Aktivitetsplikt11_9 = 'aktivitetsplikt11-9',
+}
+
+export function erFørstegangsbehandling(behandling: BehandlingInfo): boolean {
+  return behandling.type === Behandlingstype.Førstegangsbehandling;
+}
+
+export function erAvsluttet(behandling: BehandlingInfo) {
+  return behandling.status in ['IVERKSETTES', 'AVSLUTTET'];
+}
+
+export function erTrukket(behandling: BehandlingInfo) {
+  return behandling.vurderingsbehov.includes('SØKNAD_TRUKKET');
+}
+
+export function erAvsluttetFørstegangsbehandling(behandling: BehandlingInfo): boolean {
+  return erFørstegangsbehandling(behandling) && erAvsluttet(behandling) && !erTrukket(behandling);
+}
+
+export function formaterBehandlingType(behandlingtype: Behandlingstype): string {
+  switch (behandlingtype) {
+    case Behandlingstype.Førstegangsbehandling:
+      return 'Førstegangsbehandling';
+    case Behandlingstype.Revurdering:
+      return 'Revurdering';
+    case Behandlingstype.Klage:
+      return 'Klage';
+    case Behandlingstype.SvarFraAndreinstans:
+      return 'Svar fra Nav Klageinstans';
+    case Behandlingstype.Oppfølgingsbehandling:
+      return 'Oppfølgingsoppgave';
+    case Behandlingstype.Aktivitetsplikt11_7:
+      return 'Aktivitetsplikt § 11-7';
+    case Behandlingstype.Aktivitetsplikt11_9:
+      return 'Aktivitetsplikt § 11-9';
+    default:
+      return `Ukjent behandlingtype (${behandlingtype})`;
+  }
+}
+
+export function formatterÅrsakTilOpprettelseTilTekst(årsakTilOpprettelse: ÅrsakTilOpprettelse): string {
+  switch (årsakTilOpprettelse) {
+    case 'SØKNAD':
+      return 'Søknad';
+    case 'MANUELL_OPPRETTELSE':
+      return 'Manuelt opprettet';
+    case 'HELSEOPPLYSNINGER':
+      return 'Helseopplysninger';
+    case 'ANNET_RELEVANT_DOKUMENT':
+      return 'Annet relevant dokument';
+    case 'OMGJØRING_ETTER_KLAGE':
+      return 'Omgjøring etter klage';
+    case 'OMGJØRING_ETTER_SVAR_FRA_KLAGEINSTANS':
+      return 'Omgjøring etter svar fra klageinstans';
+    case 'FASTSATT_PERIODE_PASSERT':
+      return 'Fastsatt periode passert';
+    case 'FRITAK_MELDEPLIKT':
+      return 'Fritak meldeplikt';
+    case 'MELDEKORT':
+      return 'Meldekort';
+    case 'AKTIVITETSMELDING':
+      return 'Aktivitetsmelding';
+    case 'OPPFØLGINGSOPPGAVE':
+      return 'Manuelt opprettet';
+    case 'OPPFØLGINGSOPPGAVE_SAMORDNING_GRADERING':
+      return 'Maksdato annen full ytelse';
+    case 'SVAR_FRA_KLAGEINSTANS':
+      return 'Svar fra klageinstans';
+    case 'KLAGE':
+      return 'Klage';
+    case 'ENDRING_I_REGISTERDATA':
+      return 'Endring i register';
+    case 'AKTIVITETSPLIKT':
+      return 'Aktivitetsplikt';
+    default:
+      return 'Ukjent årsak';
+  }
+}
