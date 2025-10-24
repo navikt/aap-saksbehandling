4f4c8d745ce5d9b22c8f977be671f4a7f5e33eb2
Steffen Hageland <steffen.hageland@nav.no>
2025-10-24 12:46:51 +0200

    Sletter gammel visning for barnetillegg, og endrer så ny brukes også i prod

--- components/behandlinger/barnetillegg/Barnetillegg.tsx
+++ components/behandlinger/barnetillegg/Barnetillegg.tsx
@@ -1,12 +1,10 @@
 import { hentFlyt } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { GruppeSteg } from 'components/gruppesteg/GruppeSteg';
 import { BarnetilleggVurderingMedDataFetching } from 'components/behandlinger/barnetillegg/barnetilleggvurdering/BarnetilleggVurderingMedDataFetching';
-import { OldBarnetilleggVurderingMedDataFetching } from 'components/behandlinger/barnetillegg/old-barnetilleggvurdering/OldBarnetilleggVurderingMedDataFetching';
 import { StegSuspense } from 'components/stegsuspense/StegSuspense';
 import { isError } from 'lib/utils/api';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 import { getStegData } from 'lib/utils/steg';
-import { isProd } from 'lib/utils/environment';
 
 interface Props {
   behandlingsreferanse: string;
@@ -29,17 +27,7 @@ export const Barnetillegg = async ({ behandlingsreferanse }: Props) => {
       aktivtSteg={flyt.data.aktivtSteg}
     >
       <StegSuspense>
-        {isProd() ? (
-          <OldBarnetilleggVurderingMedDataFetching
-            behandlingsreferanse={behandlingsreferanse}
-            stegData={barnetilleggSteg}
-          />
-        ) : (
-          <BarnetilleggVurderingMedDataFetching
-            behandlingsreferanse={behandlingsreferanse}
-            stegData={barnetilleggSteg}
-          />
-        )}
+        <BarnetilleggVurderingMedDataFetching behandlingsreferanse={behandlingsreferanse} stegData={barnetilleggSteg} />
       </StegSuspense>
     </GruppeSteg>
   );
