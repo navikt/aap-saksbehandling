4daecf440ce4b68f8a089dad3867b2bbedb4a785
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2025-10-28 14:01:39 +0100

    AAP-1505 Send med navident i manuell revurdering-hendelse (#1329)

--- app/saksbehandling/sak/[saksId]/revurdering/page.tsx
+++ app/saksbehandling/sak/[saksId]/revurdering/page.tsx
@@ -2,17 +2,18 @@ import { Box, Page as AkselPage } from '@navikt/ds-react';
 import { hentSak, hentSakPersoninfo } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { OpprettRevurdering } from 'components/saksoversikt/opprettrevurdering/OpprettRevurdering';
 import { SaksinfoBanner } from 'components/saksinfobanner/SaksinfoBanner';
+import { hentBrukerInformasjon } from 'lib/services/azure/azureUserService';
 
 export default async function Page(props: { params: Promise<{ saksId: string }> }) {
   const params = await props.params;
-  const [sak, personInfo] = await Promise.all([hentSak(params.saksId), hentSakPersoninfo(params.saksId)]);
+  const [sak, personInfo, brukerInformasjon] = await Promise.all([hentSak(params.saksId), hentSakPersoninfo(params.saksId), hentBrukerInformasjon()]);
 
   return (
     <AkselPage>
       <SaksinfoBanner personInformasjon={personInfo} sak={sak} />
 
       <Box marginBlock="8">
-        <OpprettRevurdering sak={sak} redirect={true} />
+        <OpprettRevurdering sak={sak} redirect={true} navIdent={brukerInformasjon.NAVident} />
       </Box>
     </AkselPage>
   );
--- components/behandlinger/klage/omgjøring/OmgjøringMedDataFetching.tsx
+++ components/behandlinger/klage/omgjøring/OmgjøringMedDataFetching.tsx
@@ -3,6 +3,7 @@ import { hentKlageresultat, hentSak } from 'lib/services/saksbehandlingservice/s
 import { OmgjøringVurdering } from 'components/behandlinger/klage/omgjøring/OmgjøringVurdering';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 import { isError } from 'lib/utils/api';
+import { hentBrukerInformasjon } from 'lib/services/azure/azureUserService';
 
 // TODO: Slett dersom vi lander på helautomatisk revurdering for klage
 export const OmgjøringMedDataFetching = async ({
@@ -15,11 +16,11 @@ export const OmgjøringMedDataFetching = async ({
   typeBehandling: TypeBehandling;
   readOnly: boolean;
 }) => {
-  const [klageresultat, sak] = await Promise.all([hentKlageresultat(behandlingsreferanse), hentSak(saksnummer)]);
+  const [klageresultat, sak, brukerInformasjon] = await Promise.all([hentKlageresultat(behandlingsreferanse), hentSak(saksnummer), hentBrukerInformasjon()]);
 
   if (isError(klageresultat)) {
     return <ApiException apiResponses={[klageresultat]} />;
   }
 
-  return <OmgjøringVurdering sak={sak} klageresultat={klageresultat.data} />;
+  return <OmgjøringVurdering sak={sak} klageresultat={klageresultat.data} navIdent={brukerInformasjon.NAVident} />;
 };
