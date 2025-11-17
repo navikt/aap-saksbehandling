57de4c97e16f743323160f42c939a0b4545091f5
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2025-11-17 15:57:21 +0100

    Ord fiks

--- components/behandlinger/samordning/samordningsosial/SamordningSosialstønad.tsx
+++ components/behandlinger/samordning/samordningsosial/SamordningSosialstønad.tsx
@@ -18,8 +18,8 @@ export const SamordningSosialstønad = ({ grunnlag }: Props) => {
       <VilkårsKort heading="§11-29 Refusjonskrav sosialstønad" steg="REFUSJON_KRAV" defaultOpen={true}>
         <BodyShort size={'small'} spacing>
           Nav-kontor har meldt om perioder der brukeren kan ha mottatt sosialstønad. Disse kan føre til refusjonskrav på
-          etterbetaling. Etterbetaling settes på vent i opp til 21 dager i påvente av refusjonskrav fra nav-kontor.
-          Følgende nav-kontor vil varsles automatisk via Gosys når vedtak fattes i saken.
+          etterbetaling. Etterbetaling settes på vent i opp til 21 dager i påvente av refusjonskrav fra Nav-kontor.
+          Følgende Nav-kontor vil varsles automatisk via Gosys når vedtak fattes i saken.
         </BodyShort>
         <TableStyled>
           <Table.Header>
