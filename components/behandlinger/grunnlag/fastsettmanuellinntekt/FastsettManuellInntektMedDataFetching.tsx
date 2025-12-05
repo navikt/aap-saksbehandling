3ba0d5c40d4dacc178bc4564b7a327c03f0867ae
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-12-05 14:28:50 +0100

    Hent beregningstidspunkt før EØS-kortet vises

--- components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektMedDataFetching.tsx
+++ components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektMedDataFetching.tsx
@@ -1,5 +1,8 @@
 import { FastsettManuellInntekt } from 'components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntekt';
-import { hentManuellInntektGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import {
+  hentBeregningstidspunktVurdering,
+  hentManuellInntektGrunnlag,
+} from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { isError } from 'lib/utils/api';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 import { skalViseSteg, StegData } from 'lib/utils/steg';
@@ -12,12 +15,23 @@ interface Props {
 }
 
 export const FastsettManuellInntektMedDataFetching = async ({ behandlingsreferanse, stegData }: Props) => {
-  const grunnlag = await hentManuellInntektGrunnlag(behandlingsreferanse);
-
+  // Nytt kort
   if (toggles.featureManglendePGIOgEøsInntekter) {
-    if (isError(grunnlag)) {
+    const beregningstidspunkt = await hentBeregningstidspunktVurdering(behandlingsreferanse);
+    if (isError(beregningstidspunkt)) {
+      return <ApiException apiResponses={[beregningstidspunkt]} />;
+    }
+
+    const nedsattArbeidsevneDato = beregningstidspunkt.data.vurdering?.nedsattArbeidsevneDato;
+    if (!nedsattArbeidsevneDato) {
       return null;
     }
+
+    const grunnlag = await hentManuellInntektGrunnlag(behandlingsreferanse);
+    if (isError(grunnlag)) {
+      return <ApiException apiResponses={[grunnlag]} />;
+    }
+
     return (
       <FastsettManuellInntektInfo
         behandlingsversjon={stegData.behandlingVersjon}
@@ -26,6 +40,9 @@ export const FastsettManuellInntektMedDataFetching = async ({ behandlingsreferan
       />
     );
   }
+
+  // Eksisterende kort
+  const grunnlag = await hentManuellInntektGrunnlag(behandlingsreferanse);
 
   if (isError(grunnlag)) {
     return <ApiException apiResponses={[grunnlag]} />;
