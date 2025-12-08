29b74e78da301144750f089136a011ee5353b2b8
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-12-08 13:58:58 +0100

    AAP-1639 Flytt tabell ut i egen fil

---
+++ components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektTabell.tsx
@@ -0,0 +1,80 @@
+import { ErrorMessage, Label, Table, VStack } from '@navikt/ds-react';
+import { TableStyled } from 'components/tablestyled/TableStyled';
+import { TextFieldWrapper } from 'components/form/textfieldwrapper/TextFieldWrapper';
+import { formaterTilNok } from 'lib/utils/string';
+import styles from 'components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektTabell.module.css';
+import { UseFormReturn } from 'react-hook-form';
+import { FastsettManuellInntektForm, Tabellår } from 'components/behandlinger/grunnlag/fastsettmanuellinntekt/types';
+
+interface Props {
+  form: UseFormReturn<FastsettManuellInntektForm>;
+  tabellår: Tabellår[];
+}
+
+export const FastsettManuellInntektTabell = ({ tabellår, form }: Props) => {
+  const regnUtTotalbeløpPerÅr = (ferdigLignetPGI: number, beregnetPGI: number, eøsInntekt: number): string => {
+    const total =
+      beregnetPGI > 0 ? Number(beregnetPGI) + Number(eøsInntekt) : Number(ferdigLignetPGI) + Number(eøsInntekt);
+    return formaterTilNok(total);
+  };
+
+  return (
+    <VStack gap={'2'}>
+      <Label size={'small'}>Hvilke år skal inntekt overstyres?</Label>
+      <TableStyled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell textSize={'small'}>År</Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'}>Ferdig lignet PGI</Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'}>Beregnet PGI</Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'}>EØS inntekt</Table.HeaderCell>
+            <Table.HeaderCell textSize={'small'}>Totalt</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body data-testid={'inntektstabell'}>
+          {tabellår.map((år, index) => {
+            const ferdigLignetPGI = form.watch(`tabellår.${index}.ferdigLignetPGI`);
+            const beregnetPGI = form.watch(`tabellår.${index}.beregnetPGI`);
+            const eøsInntekt = form.watch(`tabellår.${index}.eøsInntekt`);
+            return (
+              <Table.Row key={år.år}>
+                <Table.DataCell textSize={'small'}>{år.år}</Table.DataCell>
+                <Table.DataCell textSize={'small'} data-testid={'ferdigLignetPGI'}>
+                  {år.ferdigLignetPGI ? formaterTilNok(år.ferdigLignetPGI) : '-'}
+                </Table.DataCell>
+                <Table.DataCell textSize={'small'} data-testid={'beregnetPGI'}>
+                  <TextFieldWrapper
+                    className={styles.inntektfelt}
+                    name={`tabellår.${index}.beregnetPGI`}
+                    control={form.control}
+                    type={'number'}
+                    hideLabel={true}
+                    readOnly={år.ferdigLignetPGI !== undefined}
+                  />
+                </Table.DataCell>
+                <Table.DataCell textSize={'small'} data-testid={'eøsInntekt'}>
+                  <TextFieldWrapper
+                    className={styles.inntektfelt}
+                    name={`tabellår.${index}.eøsInntekt`}
+                    control={form.control}
+                    type={'number'}
+                    hideLabel={true}
+                  />
+                </Table.DataCell>
+                <Table.DataCell data-testid={'totalt'} textSize={'small'}>
+                  {regnUtTotalbeløpPerÅr(ferdigLignetPGI ?? 0, beregnetPGI ?? 0, eøsInntekt ?? 0)}
+                </Table.DataCell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </TableStyled>
+      {form.formState.errors.tabellår && (
+        <ErrorMessage size={'small'} showIcon>
+          {form.formState.errors.tabellår[0]?.message}
+        </ErrorMessage>
+      )}
+    </VStack>
+  );
+};
