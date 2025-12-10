e88c2736e0eabff3603d8664a7ab92af71f2fe87
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2025-12-10 14:07:07 +0100

    Update components/behandlinger/grunnlag/inntektsbortfall/Inntektsbortfall.test.tsx
    Co-authored-by: Fredrik Meyer <fredrik.meyer@nav.no>

--- components/behandlinger/grunnlag/inntektsbortfall/Inntektsbortfall.test.tsx
+++ components/behandlinger/grunnlag/inntektsbortfall/Inntektsbortfall.test.tsx
@@ -11,7 +11,7 @@ describe('Inntektsbortfall', () => {
   beforeEach(() => render(<Inntektsbortfall behandlingVersjon={1} readOnly={true} />));
   it('skal ha en alert', () => {
     const alert = screen.getByText(
-      'Brukeren er over 62 år og må vurderes for § 11-4 andre ledd. Det er ikke støttet i Kelvin enda. Saken må settes på vent i påvente av at funksjonaliteten er ferdig utviklet'
+      'Brukeren er over 62 år og må vurderes for § 11-4 andre ledd. Det er ikke støttet i Kelvin enda. Saken må settes på vent i påvente av at funksjonaliteten er ferdig utviklet.'
     );
     expect(alert).toBeVisible();
   });
