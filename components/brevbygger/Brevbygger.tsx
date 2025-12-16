2c47ede9859449c74940fb568c8c953347784af0
Øivind Stensrud <oivind.stensrud@nav.no>
2025-12-16 13:03:20 +0100

    Flytter valg av mottakere til egen fil

--- components/brevbygger/Brevbygger.tsx
+++ components/brevbygger/Brevbygger.tsx
@@ -17,11 +17,10 @@ import { useRouter } from 'next/navigation';
 import { useLøsBehovOgGåTilNesteSteg } from 'hooks/saksbehandling/LøsBehovOgGåTilNesteStegHook';
 import { Behovstype } from 'lib/utils/form';
 import { useBehandlingsReferanse } from 'hooks/saksbehandling/BehandlingHook';
-import { useConfigForm } from 'components/form/FormHook';
 import { useCallback, useEffect, useState } from 'react';
-import { FormField } from 'components/form/FormField';
 import { isSuccess } from 'lib/utils/api';
 import { revalidateFlyt } from 'lib/actions/actions';
+import { VelgeMottakere } from 'components/brevbygger/VelgeMottakere';
 
 export interface AlternativFormField {
   verdi: string;
@@ -229,50 +228,3 @@ export const Brevbygger = ({
     </HGrid>
   );
 };
-
-function VelgeMottakere({
-  setMottakere,
-  readOnly,
-  fullmektig,
-  brukerNavn,
-  bruker,
-}: {
-  setMottakere: (mottakere: Mottaker[]) => void;
-  readOnly: boolean;
-  brukerNavn: string;
-  bruker: Mottaker;
-  fullmektig: Mottaker;
-}) {
-  interface FormFields {
-    mottakere: ('BRUKER' | 'FULLMEKTIG')[];
-  }
-
-  const fullmektigNavn = fullmektig.navnOgAdresse?.navn;
-
-  const { formFields, form } = useConfigForm<FormFields>(
-    {
-      mottakere: {
-        type: 'checkbox',
-        label: 'Velg mottakere',
-        options: [
-          { label: brukerNavn ? `${brukerNavn} (Bruker)` : 'Bruker', value: 'BRUKER' },
-          { label: fullmektigNavn ? `${fullmektigNavn} (Fullmektig)` : 'Fullmektig', value: 'FULLMEKTIG' },
-        ],
-        defaultValue: ['FULLMEKTIG'],
-      },
-    },
-    { readOnly }
-  );
-
-  const valgteMottakere = form.watch('mottakere');
-  useEffect(() => {
-    const mottakere = valgteMottakere.map((mottaker) => (mottaker === 'BRUKER' ? bruker : fullmektig));
-    setMottakere(mottakere);
-  }, [bruker, fullmektig, setMottakere, valgteMottakere]);
-
-  return (
-    <div>
-      <FormField form={form} formField={formFields.mottakere} />
-    </div>
-  );
-}
