b4e4c152e832996beaebff6b047c40fbcd4d0038
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2025-12-16 14:03:04 +0100

    Fiks sorting av tidligere vurderinger for fastsett manuell inntekt

--- components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektNy.tsx
+++ components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektNy.tsx
@@ -16,6 +16,7 @@ import { deepEqual } from 'components/tidligerevurderinger/TidligereVurderingerU
 import { useFieldArray } from 'react-hook-form';
 import { FastsettManuellInntektTabell } from 'components/behandlinger/grunnlag/fastsettmanuellinntekt/FastsettManuellInntektTabell';
 import { FastsettManuellInntektForm, Tabellår } from 'components/behandlinger/grunnlag/fastsettmanuellinntekt/types';
+import { sorterEtterNyesteDato } from 'lib/utils/date';
 
 interface Props {
   behandlingsversjon: number;
@@ -118,7 +119,9 @@ export const FastsettManuellInntektNy = ({
   }
 
   const visHovedinnhold = !formReadOnly || grunnlag.manuelleVurderinger !== null;
-  const historiskeVurderinger = grunnlag.historiskeManuelleVurderinger;
+  const vurderinger = grunnlag.historiskeManuelleVurderinger?.sort((a, b) => {
+    return sorterEtterNyesteDato(a.vurdertAv.dato, b.vurdertAv.dato);
+  });
 
   return (
     <VilkårskortMedFormOgMellomlagringNyVisning
@@ -141,26 +144,25 @@ export const FastsettManuellInntektNy = ({
       visningActions={visningActions}
       formReset={() => form.reset(mellomlagretVurdering ? JSON.parse(mellomlagretVurdering.data) : undefined)}
     >
-      {behandlingErRevurdering && historiskeVurderinger && (
+      {behandlingErRevurdering && vurderinger && (
         <TidligereVurderinger
-          data={historiskeVurderinger}
-          getErGjeldende={(v) => deepEqual(v, historiskeVurderinger[historiskeVurderinger.length - 1])}
+          data={vurderinger}
+          getErGjeldende={(v) => deepEqual(v, vurderinger.at(0))}
           getVurdertAvIdent={(v) => v.vurdertAv.ident}
           getVurdertDato={(v) => v.vurdertAv.dato}
-          getFomDato={(v) => v.fomDato}
           grupperPåOpprettetDato={true}
-          customElement={(selectedIndex) => {
+          customElement={(valgtVurderingIndex) => {
             const pgi = grunnlag.registrerteInntekterSisteRelevanteAr;
             const tabelldata = byggTabellData({
               sisteÅr: grunnlag.sisteRelevanteÅr,
               pgi: pgi,
-              manuelleInntekter: historiskeVurderinger.at(selectedIndex)?.årsVurderinger || [],
+              manuelleInntekter: vurderinger.at(valgtVurderingIndex)?.årsVurderinger || [],
             });
             return (
               <>
                 <VStack>
                   <Label size="small">{formFields.begrunnelse.label}</Label>
-                  <BodyShort size="small">{historiskeVurderinger.at(selectedIndex)?.begrunnelse}</BodyShort>
+                  <BodyShort size="small">{vurderinger.at(valgtVurderingIndex)?.begrunnelse}</BodyShort>
                 </VStack>
                 <VStack>
                   <FastsettManuellInntektTabell form={form} tabellår={tabelldata} låstVisning={true} />
