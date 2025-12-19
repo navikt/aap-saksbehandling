797a6ac8fa9404c2705bcbcbe838d8900f294f53
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-12-19 16:18:57 +0100

    Infoboks ved manglende institusjonsopphold

---
+++ components/behandlinger/institusjonsopphold/helseinstitusjon/ManglendeInstitusjonsOpphold.tsx
@@ -0,0 +1,17 @@
+import { ExpansionCard } from '@navikt/ds-react';
+import { InformationSquareFillIcon } from '@navikt/aksel-icons';
+
+import styles from 'components/behandlinger/institusjonsopphold/helseinstitusjon/ManglendeInstitusjonsOpphold.module.css';
+
+export const ManglendeInstitusjonsOpphold = () => {
+  return (
+    <ExpansionCard size={'small'} aria-label={'Institusjonsopphold'} defaultOpen={true} className={styles.infobox}>
+      <div className={styles.content}>
+        <InformationSquareFillIcon />
+        <span>
+          Det finnes institusjonsopphold for bruker, men institusjonsoppholdet varer for kort til å gi reduksjon av AAP.
+        </span>
+      </div>
+    </ExpansionCard>
+  );
+};
