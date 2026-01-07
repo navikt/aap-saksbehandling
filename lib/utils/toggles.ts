cd69afaf69669ce76993837d35ce4feab77c1f11
Steffen Hageland <steffen.hageland@nav.no>
2026-01-07 15:05:04 +0100

    Legger inn en liten melding om å bruke unleash-toggle i toggles

--- lib/utils/toggles.ts
+++ lib/utils/toggles.ts
@@ -3,6 +3,8 @@ import { isProd } from 'lib/utils/environment';
 /**
  * Samle release toggles på ett sted - slik at man får en enkel oversikt over hvilke toggles som faktisk er i bruk
  */
+
+// Deprecated: Om mulig bruk heller feature-toggles fra unleash. Disse ligger in "lib/services/unleash/unleashToggles.ts"
 export const toggles = {
   featureSimulering: !isProd(),
   featureFerieISykepengeperiode: !isProd(), // Finnes ikke i unleash - kun frontend-toggle
