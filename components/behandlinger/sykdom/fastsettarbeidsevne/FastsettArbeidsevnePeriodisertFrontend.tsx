bab521d888e2631ee7ca0af01fa06d55acfd917d
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2026-01-08 11:56:32 +0100

    Bruker nytt løs-behov endepunkt for fastsett arbeidsevne  (#1555)
    * Endrer fastsettarbeidsevneperiodisert til å bruke nytt endepunkt ved løs behov
    
    * Bruker periodiserte felter for arbeidsevne
    
    ---------
    
    Co-authored-by: Henrik Gundersen <henrik.gundersen@nav.no>

--- components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevnePeriodisertFrontend.tsx
+++ components/behandlinger/sykdom/fastsettarbeidsevne/FastsettArbeidsevnePeriodisertFrontend.tsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { FormEvent } from 'react';
 import { useLøsBehovOgGåTilNesteSteg } from 'hooks/saksbehandling/LøsBehovOgGåTilNesteStegHook';
 
-import { useFieldArray } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { gyldigDatoEllerNull, validerDato } from 'lib/validation/dateValidation';
-import { ArbeidsevneGrunnlag, ArbeidsevneVurdering, MellomlagretVurdering } from 'lib/types/types';
+import {
+  ArbeidsevneGrunnlag,
+  LøsPeriodisertBehovPåBehandling,
+  MellomlagretVurdering,
+  PeriodisertArbeidsevneVurderingDto,
+} from 'lib/types/types';
 import { useBehandlingsReferanse } from 'hooks/saksbehandling/BehandlingHook';
-import { formaterDatoForBackend, formaterDatoForFrontend } from 'lib/utils/date';
+import { formaterDatoForBackend, formaterDatoForFrontend, parseDatoFraDatePicker } from 'lib/utils/date';
 import { Behovstype } from 'lib/utils/form';
-import { parse } from 'date-fns';
+import { parse, parseISO } from 'date-fns';
 
-import { HStack, Link, VStack } from '@navikt/ds-react';
+import { HStack, Label, Link, VStack } from '@navikt/ds-react';
 import { pipe } from 'lib/utils/functional';
-import { erProsent } from 'lib/utils/validering';
-import { useConfigForm } from 'components/form/FormHook';
+import { erProsent, validerPeriodiserteVurderingerRekkefølge } from 'lib/utils/validering';
 import { TextAreaWrapper } from 'components/form/textareawrapper/TextAreaWrapper';
 import { TextFieldWrapper } from 'components/form/textfieldwrapper/TextFieldWrapper';
 import { DateInputWrapper } from 'components/form/dateinputwrapper/DateInputWrapper';
@@ -22,10 +25,10 @@ import { useMellomlagring } from 'hooks/saksbehandling/MellomlagringHook';
 import { useVilkårskortVisning } from 'hooks/saksbehandling/visning/VisningHook';
 import { VilkårskortPeriodisert } from 'components/vilkårskort/vilkårskortperiodisert/VilkårskortPeriodisert';
 import { NyVurderingExpandableCard } from 'components/periodisering/nyvurderingexpandablecard/NyVurderingExpandableCard';
-import { finnesFeilForVurdering } from 'lib/utils/formerrors';
-import { TidligereVurderinger } from 'components/tidligerevurderinger/TidligereVurderinger';
-import { ValuePair } from 'components/form/FormField';
-import { deepEqual } from 'components/tidligerevurderinger/TidligereVurderingerUtils';
+import { finnesFeilForVurdering, mapPeriodiserteVurderingerErrorList } from 'lib/utils/formerrors';
+import { parseDatoFraDatePickerOgTrekkFra1Dag } from 'components/behandlinger/oppholdskrav/oppholdskrav-utils';
+import { TidligereVurderingExpandableCard } from 'components/periodisering/tidligerevurderingexpandablecard/TidligereVurderingExpandableCard';
+import { SpørsmålOgSvar } from 'components/sporsmaalogsvar/SpørsmålOgSvar';
 
 interface Props {
   grunnlag: ArbeidsevneGrunnlag;
@@ -34,17 +37,20 @@ interface Props {
   initialMellomlagretVurdering?: MellomlagretVurdering;
 }
 
-interface Arbeidsevnevurderinger {
+interface ArbeidsevneVurderingForm {
   begrunnelse: string;
-  arbeidsevne: string;
-  fom: string;
+  arbeidsevne: number | undefined;
+  fraDato: string | undefined;
+  vurdertAv?: {
+    ansattnavn: string | null | undefined;
+    ident: string;
+    dato: string;
+  };
 }
 
-interface FastsettArbeidsevneFormFields {
-  arbeidsevnevurderinger: Arbeidsevnevurderinger[];
+interface FastsettArbeidsevneForm {
+  vurderinger: ArbeidsevneVurderingForm[];
 }
-
-type DraftFormFields = Partial<FastsettArbeidsevneFormFields>;
 
 const ANTALL_TIMER_FULL_UKE = 37.5;
 
@@ -67,7 +73,7 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
   initialMellomlagretVurdering,
 }: Props) => {
   const behandlingsreferanse = useBehandlingsReferanse();
-  const { løsBehovOgGåTilNesteSteg, isLoading, status, løsBehovOgGåTilNesteStegError } =
+  const { løsPeriodisertBehovOgGåTilNesteSteg, isLoading, status, løsBehovOgGåTilNesteStegError } =
     useLøsBehovOgGåTilNesteSteg('FASTSETT_ARBEIDSEVNE');
 
   const { mellomlagretVurdering, lagreMellomlagring, slettMellomlagring, nullstillMellomlagretVurdering } =
@@ -79,77 +85,63 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
     mellomlagretVurdering
   );
 
-  const vedtatteVurderinger = grunnlag.gjeldendeVedtatteVurderinger ?? [];
+  const defaultValues =
+    initialMellomlagretVurdering != null
+      ? (JSON.parse(initialMellomlagretVurdering.data) as FastsettArbeidsevneForm)
+      : getDefaultValuesFromGrunnlag(grunnlag);
 
-  const defaultValues: DraftFormFields = initialMellomlagretVurdering
-    ? JSON.parse(initialMellomlagretVurdering.data)
-    : mapVurderingerToDraftFormFields(grunnlag.vurderinger);
-
-  const { form } = useConfigForm<FastsettArbeidsevneFormFields>({
-    arbeidsevnevurderinger: {
-      type: 'fieldArray',
-      defaultValue: defaultValues.arbeidsevnevurderinger,
-    },
+  const form = useForm<FastsettArbeidsevneForm>({
+    defaultValues,
   });
 
-  const {
-    fields: arbeidsevneVurderinger,
-    append,
-    remove,
-  } = useFieldArray({
-    control: form.control,
-    name: 'arbeidsevnevurderinger',
-  });
+  const vedtatteVurderinger = grunnlag?.sisteVedtatteVurderinger ?? [];
 
-  function byggFelter(vurdering: ArbeidsevneVurdering): ValuePair[] {
-    return [
-      {
-        label: 'Vilkårsvurdering',
-        value: vurdering.begrunnelse,
-      },
-      {
-        label: 'Oppgi arbeidsevnen som ikke er utnyttet i prosent',
-        value: vurdering.arbeidsevne.toString(),
-      },
-    ];
+  const { fields, append, remove } = useFieldArray({ name: 'vurderinger', control: form.control });
+
+  function onAddPeriode() {
+    append({
+      begrunnelse: '',
+      fraDato: fields.length === 0 ? formaterDatoForFrontend(new Date()) : undefined,
+      arbeidsevne: undefined,
+    });
   }
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    form.handleSubmit((data) => {
-      løsBehovOgGåTilNesteSteg(
-        {
-          behandlingVersjon: behandlingVersjon,
-          referanse: behandlingsreferanse,
-          behov: {
-            behovstype: Behovstype.FASTSETT_ARBEIDSEVNE_KODE,
-            arbeidsevneVurderinger: data.arbeidsevnevurderinger.map((vurdering) => ({
-              arbeidsevne: Number.parseInt(vurdering.arbeidsevne, 10),
-              begrunnelse: vurdering.begrunnelse,
-              fraDato: formaterDatoForBackend(parse(vurdering.fom, 'dd.MM.yyyy', new Date())),
-            })),
-          },
-        },
-        () => {
-          nullstillMellomlagretVurdering();
-          visningActions.onBekreftClick();
-        }
-      );
-    })(event);
-  };
+  function onSubmit(data: FastsettArbeidsevneForm) {
+    const erPerioderGyldige = validerPeriodiserteVurderingerRekkefølge({
+      form,
+      nyeVurderinger: data.vurderinger,
+      grunnlag,
+      tidligsteDatoMåMatcheMedRettighetsperiode: false,
+    });
+    if (!erPerioderGyldige) {
+      return;
+    }
+    const losning: LøsPeriodisertBehovPåBehandling = {
+      behandlingVersjon: behandlingVersjon,
+      referanse: behandlingsreferanse,
+      behov: {
+        behovstype: Behovstype.FASTSETT_ARBEIDSEVNE_KODE,
+        løsningerForPerioder: data.vurderinger.map((periode, index) => {
+          const isLast = index === data.vurderinger.length - 1;
+          const tilDato = isLast
+            ? undefined
+            : parseDatoFraDatePickerOgTrekkFra1Dag(data.vurderinger[index + 1].fraDato);
+          return mapFormTilDto(periode, tilDato != null ? formaterDatoForBackend(tilDato) : undefined);
+        }),
+      },
+    };
+
+    løsPeriodisertBehovOgGåTilNesteSteg(losning, () => {
+      nullstillMellomlagretVurdering();
+      visningActions.onBekreftClick();
+    });
+  }
+
+  const foersteNyePeriode = fields.length > 0 ? form.watch('vurderinger.0.fraDato') : null;
+  const errorList = mapPeriodiserteVurderingerErrorList<FastsettArbeidsevneForm>(form.formState.errors);
 
   const showAsOpen =
-    (!!grunnlag?.vurderinger && grunnlag.vurderinger.length >= 1) || initialMellomlagretVurdering !== undefined;
-  const errors =
-    form.formState.errors?.arbeidsevnevurderinger && Array.isArray(form.formState.errors?.arbeidsevnevurderinger)
-      ? form.formState.errors?.arbeidsevnevurderinger
-      : [];
-  const errorList = errors.reduce((acc, errVurdering) => {
-    const errors = Object.values(errVurdering || {})
-      // @ts-ignore
-      .map((errField) => ({ ref: `#${errField?.ref?.name}`, message: errField?.message }))
-      .filter((el) => el.message);
-    return [...acc, ...errors];
-  }, []);
+    (grunnlag?.nyeVurderinger && grunnlag.nyeVurderinger.length >= 1) || initialMellomlagretVurdering !== undefined;
 
   return (
     <VilkårskortPeriodisert
@@ -157,24 +149,19 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
       steg={'FASTSETT_ARBEIDSEVNE'}
       vilkårTilhørerNavKontor={true}
       defaultOpen={showAsOpen}
-      onSubmit={handleSubmit}
+      onSubmit={form.handleSubmit(onSubmit)}
       status={status}
       isLoading={isLoading}
       løsBehovOgGåTilNesteStegError={løsBehovOgGåTilNesteStegError}
-      vurdertAvAnsatt={grunnlag?.vurderinger?.[0]?.vurdertAv}
+      vurdertAvAnsatt={grunnlag?.sisteVedtatteVurderinger?.[0]?.vurdertAv}
+      kvalitetssikretAv={grunnlag?.sisteVedtatteVurderinger?.[0]?.kvalitetssikretAv}
       mellomlagretVurdering={mellomlagretVurdering}
       onLagreMellomLagringClick={() => lagreMellomlagring(form.watch())}
-      onDeleteMellomlagringClick={() => {
-        slettMellomlagring(() => {
-          form.reset(
-            grunnlag.vurderinger ? mapVurderingerToDraftFormFields(grunnlag.vurderinger) : emptyDraftFormFields()
-          );
-        });
-      }}
+      onDeleteMellomlagringClick={() => slettMellomlagring(() => form.reset(getDefaultValuesFromGrunnlag(grunnlag)))}
       visningModus={visningModus}
       visningActions={visningActions}
       formReset={() => form.reset(mellomlagretVurdering ? JSON.parse(mellomlagretVurdering.data) : undefined)}
-      onLeggTilVurdering={() => append({ begrunnelse: '', arbeidsevne: '', fom: '' })}
+      onLeggTilVurdering={onAddPeriode}
       errorList={errorList}
     >
       {!formReadOnly && (
@@ -184,21 +171,32 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
           </Link>
         </VStack>
       )}
-      {vedtatteVurderinger.length != 0 && (
-        <TidligereVurderinger
-          data={vedtatteVurderinger}
-          buildFelter={byggFelter}
-          getErGjeldende={(v) => deepEqual(v, vedtatteVurderinger[vedtatteVurderinger.length - 1])}
-          getFomDato={(v) => v.fraDato}
-        />
-      )}
-      {arbeidsevneVurderinger.map((vurdering, index) => (
+      {vedtatteVurderinger.map((vurdering) => (
+        <TidligereVurderingExpandableCard
+          key={vurdering.fom}
+          fom={parseISO(vurdering.fom)}
+          tom={vurdering.tom != null ? parseISO(vurdering.tom) : null}
+          foersteNyePeriodeFraDato={foersteNyePeriode != null ? parseDatoFraDatePicker(foersteNyePeriode) : null}
+          oppfylt={vurdering.arbeidsevne > 0}
+          vurdertAv={vurdering.vurdertAv}
+        >
+          <VStack gap={'5'}>
+            <SpørsmålOgSvar spørsmål="Vurderingen gjelder fra?" svar={formaterDatoForFrontend(vurdering.fom)} />
+            <SpørsmålOgSvar spørsmål="Vilkårsvurdering" svar={vurdering.begrunnelse} />
+            <SpørsmålOgSvar
+              spørsmål="Oppgi arbeidsevnen som ikke er utnyttet i prosent"
+              svar={vurdering.arbeidsevne.toString()}
+            />
+          </VStack>
+        </TidligereVurderingExpandableCard>
+      ))}
+      {fields.map((vurdering, index) => (
         <NyVurderingExpandableCard
           key={vurdering.id}
-          fraDato={gyldigDatoEllerNull(form.watch(`arbeidsevnevurderinger.${index}.fom`))}
+          fraDato={gyldigDatoEllerNull(form.watch(`vurderinger.${index}.fraDato`))}
           oppfylt={undefined}
-          nestePeriodeFraDato={gyldigDatoEllerNull(form.watch(`arbeidsevnevurderinger.${index + 1}.fom`))}
-          isLast={index === arbeidsevneVurderinger.length - 1}
+          nestePeriodeFraDato={gyldigDatoEllerNull(form.watch(`vurderinger.${index + 1}.fraDato`))}
+          isLast={index === vedtatteVurderinger.length - 1}
           vurdertAv={undefined}
           finnesFeil={finnesFeilForVurdering(index, errorList)}
           readonly={formReadOnly}
@@ -210,7 +208,7 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
           <HStack justify={'space-between'}>
             <DateInputWrapper
               control={form.control}
-              name={`arbeidsevnevurderinger.${index}.fom`}
+              name={`vurderinger.${index}.fraDato`}
               label={'Vurderingen gjelder fra'}
               rules={{
                 required: 'Du må angi datoen arbeidsevnen gjelder fra',
@@ -225,33 +223,39 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
               'Vurder om brukeren har en arbeidsevne som ikke er utnyttet. Hvis det ikke legges inn en vurdering, har brukeren rett på full ytelse.'
             }
             control={form.control}
-            name={`arbeidsevnevurderinger.${index}.begrunnelse`}
+            name={`vurderinger.${index}.begrunnelse`}
             rules={{ required: 'Du må begrunne vurderingen din' }}
             className={'begrunnelse'}
             readOnly={formReadOnly}
           />
           <HStack gap={'3'}>
-            <TextFieldWrapper
-              control={form.control}
-              name={`arbeidsevnevurderinger.${index}.arbeidsevne`}
-              type={'text'}
-              label={'Oppgi arbeidsevnen som ikke er utnyttet i prosent'}
-              rules={{
-                required: 'Du må angi hvor stor arbeidsevne brukeren har',
-                validate: (value) => {
-                  const valueAsNumber = Number(value);
-                  if (isNaN(valueAsNumber)) {
-                    return 'Prosent må være et tall';
-                  } else if (!erProsent(valueAsNumber)) {
-                    return 'Prosent kan bare være mellom 0 og 100';
-                  }
-                },
-              }}
-              readOnly={formReadOnly}
-              className="prosent_input"
-            />
-            <VStack paddingBlock={'1'} justify={'end'}>
-              {regnOmTilTimer(form.watch(`arbeidsevnevurderinger.${index}.arbeidsevne`))}
+            <VStack gap={'2'}>
+              <Label size={'small'}>Oppgi arbeidsevnen som ikke er utnyttet i prosent</Label>
+              <HStack gap={'2'}>
+                <TextFieldWrapper
+                  control={form.control}
+                  name={`vurderinger.${index}.arbeidsevne`}
+                  type={'text'}
+                  label={'Oppgi arbeidsevnen som ikke er utnyttet i prosent'}
+                  hideLabel={true}
+                  rules={{
+                    required: 'Du må angi hvor stor arbeidsevne brukeren har',
+                    validate: (value) => {
+                      const valueAsNumber = Number(value);
+                      if (isNaN(valueAsNumber)) {
+                        return 'Prosent må være et tall';
+                      } else if (!erProsent(valueAsNumber)) {
+                        return 'Prosent kan bare være mellom 0 og 100';
+                      }
+                    },
+                  }}
+                  readOnly={formReadOnly}
+                  className="prosent_input"
+                />
+                <VStack paddingBlock={'1'} justify={'end'}>
+                  {regnOmTilTimer(form.watch(`vurderinger.${index}.arbeidsevne`)?.toString() ?? '')}
+                </VStack>
+              </HStack>
             </VStack>
           </HStack>
         </NyVurderingExpandableCard>
@@ -260,17 +264,39 @@ export const FastsettArbeidsevnePeriodisertFrontend = ({
   );
 };
 
-function mapVurderingerToDraftFormFields(vurderinger?: ArbeidsevneGrunnlag['vurderinger']): DraftFormFields {
+function getDefaultValuesFromGrunnlag(grunnlag: ArbeidsevneGrunnlag | undefined): FastsettArbeidsevneForm {
+  if (grunnlag == null || (grunnlag.nyeVurderinger.length === 0 && grunnlag.sisteVedtatteVurderinger.length === 0)) {
+    // Vi har ingen tidligere vurderinger eller nye vurderinger, legg til en tom array med vurderinger
+    return {
+      vurderinger: [],
+    };
+  }
+
+  // Vi har allerede data lagret, vis enten de som er lagret i grunnlaget her eller tom liste
   return {
-    arbeidsevnevurderinger:
-      vurderinger?.map((vurdering) => ({
-        begrunnelse: vurdering.begrunnelse,
-        arbeidsevne: vurdering.arbeidsevne.toString(),
-        fom: formaterDatoForFrontend(vurdering.fraDato),
-      })) || [],
+    vurderinger: grunnlag?.nyeVurderinger.map((vurdering) => ({
+      begrunnelse: vurdering.begrunnelse,
+      fraDato: formaterDatoForFrontend(vurdering.fom),
+      arbeidsevne: vurdering.arbeidsevne,
+      vurdertAv:
+        vurdering.vurdertAv != null
+          ? {
+              ansattnavn: vurdering.vurdertAv.ansattnavn,
+              ident: vurdering.vurdertAv.ident,
+              dato: vurdering.vurdertAv.dato,
+            }
+          : undefined,
+    })),
   };
 }
-
-function emptyDraftFormFields(): DraftFormFields {
-  return { arbeidsevnevurderinger: [] };
+function mapFormTilDto(
+  periodeForm: ArbeidsevneVurderingForm,
+  tilDato: string | undefined | null
+): PeriodisertArbeidsevneVurderingDto {
+  return {
+    begrunnelse: periodeForm.begrunnelse,
+    fom: formaterDatoForBackend(parse(periodeForm.fraDato!, 'dd.MM.yyyy', new Date())),
+    tom: tilDato,
+    arbeidsevne: periodeForm.arbeidsevne ?? 0,
+  };
 }
