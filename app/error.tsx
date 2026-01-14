5e3a9778bee25d1f9e9e493abae1ea46c37e6e46
Nicolas N. <1224956+Watercolours@users.noreply.github.com>
2026-01-14 14:21:28 +0100

    fjerne edb-mannen og oppdatere feilsider (#1601)
    * fjerne edb-mannen og oppdatere feilsider
    
    * glemte siste versjon av 404-sidene
    
    * fjerne import

--- app/error.tsx
+++ app/error.tsx
@@ -1,35 +1,50 @@
 'use client';
 
-import Image from 'next/image';
-import { BodyShort, Heading, VStack } from '@navikt/ds-react';
-import ErrorBilde from '../public/error.jpg';
+import { BodyShort, Box, Heading, HGrid, Label, Link, Page, VStack } from '@navikt/ds-react';
+import { useParams } from 'next/navigation';
+import { formaterDatoMedTidspunktForFrontend } from 'lib/utils/date';
 
 interface Props {
   error: Error & { digest?: string };
-  reset: () => void;
 }
 
 //500 Page
 const Error = ({ error }: Props) => {
+  const { saksId } = useParams<{ saksId?: string }>();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        flexDirection: 'column',
-        marginBlockStart: '2rem',
-      }}
-    >
-      <Heading level="2" size="medium" spacing>
-        Det har oppstått en feil 🙃.
-      </Heading>
-      <VStack align={'start'} gap={'4'}>
-        <BodyShort>{error.message}</BodyShort>
-        <BodyShort>{error?.digest}</BodyShort>
-        <Image src={ErrorBilde} alt="404" width={500} height={500} />
-      </VStack>
-    </div>
+    <Page>
+      <Page.Block width="md" gutters>
+        <Box marginBlock="8" padding="4">
+          <VStack gap="4" marginBlock="8">
+            <Heading level="2" size="large">
+              En feil har oppstått!
+            </Heading>
+
+            <BodyShort>
+              Du kan prøve igjen. Dersom feilen vedvarer kan du melde problemet i Porten med skjermbilde av denne siden.
+            </BodyShort>
+
+            <HGrid columns={2} gap="2">
+              {saksId && (
+                <>
+                  <Label>Saksnummer:</Label>
+                  <BodyShort>{saksId}</BodyShort>
+                </>
+              )}
+
+              <Label>Tidspunkt:</Label>
+              <BodyShort>{formaterDatoMedTidspunktForFrontend(new Date())}</BodyShort>
+
+              <Label>Feilmelding:</Label>
+              <BodyShort spacing>{error.message}</BodyShort>
+            </HGrid>
+
+            <Link href={'/oppgave'}>Gå til oppgavelisten</Link>
+          </VStack>
+        </Box>
+      </Page.Block>
+    </Page>
   );
 };
 
