e281413ba5bba1d30b8868c78f667f7b75377d07
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-01-21 13:04:57 +0100

    Bedre feilmeldingen ved bruk av hook uten provider

--- hooks/saksbehandling/AccordionTilstandHook.tsx
+++ hooks/saksbehandling/AccordionTilstandHook.tsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import { AccordionTilstandContext } from 'context/saksbehandling/AccordionTilstandContext';
+import { useParams } from 'next/navigation';
 
 export function useAccordionTilstand() {
+  const params = useParams<{ aktivGruppe: string }>();
   const context = useContext(AccordionTilstandContext);
 
   if (!context) {
     throw new Error(
-      'useAccordionTilstand må brukes innenfor AccordionTilstandProvider, wrap expandable cards i accordionGroup komponent'
+      `useAccordionTilstand kan bare brukes innenfor AccordionTilstandProvider. Sørg for at NyVurderingExpandableCard er pakket inn i AccordionTilstandProvider. Denne feilen skjer i et vilkårskort i steggruppen ${params.aktivGruppe}`
     );
   }
 
