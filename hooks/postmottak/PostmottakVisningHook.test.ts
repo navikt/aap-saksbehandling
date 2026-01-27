5b494b7597828d40a9412d39830cd0121eb26e08
Martin Aasen Fromreide <martin.aasen.fromreide@nav.no>
2026-01-27 11:19:03 +0100

    Rydd opp i tester

--- hooks/postmottak/PostmottakVisningHook.test.ts
+++ hooks/postmottak/PostmottakVisningHook.test.ts
@@ -1,35 +1,15 @@
 import { describe, expect, it } from 'vitest';
-import { MellomlagretVurdering } from 'lib/types/types';
-import { Behovstype } from 'lib/utils/form';
 import { VisningModus } from 'lib/types/visningTypes';
 import { hentVisning } from 'lib/utils/visning';
 
-const mellomlagring: MellomlagretVurdering = {
-  avklaringsbehovkode: Behovstype.AVKLAR_SYKDOM_KODE,
-  behandlingId: { id: 0 },
-  data: 'hello pello',
-  vurdertAv: '',
-  vurdertDato: '',
-};
-
-describe('Visning av vilkårskort i førstegangsbehandling', () => {
+describe('Visning av vilkårskort i postmottak', () => {
   it('skal være LÅST_MED_ENDRE hvis readOnly=false, erAktivtSteg=false, mellomlagring=undefined', () => {
     const verdi = hentVisning(false, false, undefined);
     expect(verdi).toBe(VisningModus.LÅST_MED_ENDRE);
   });
 
-  it('skal være AKTIV_MED_AVBRYT hvis readOnly=false, erAktivtSteg=false, mellomlagring=definert', () => {
-    const verdi = hentVisning(false, false, mellomlagring);
-    expect(verdi).toBe(VisningModus.AKTIV_MED_AVBRYT);
-  });
-
   it('skal være AKTIV_UTEN_AVBRYT hvis readOnly=false, erAktivtSteg=true, mellomlagring=undefined', () => {
     const verdi = hentVisning(false, true, undefined);
-    expect(verdi).toBe(VisningModus.AKTIV_UTEN_AVBRYT);
-  });
-
-  it('skal være AKTIV_UTEN_AVBRYT hvis readOnly=false, erAktivtSteg=true, mellomlagring=definert', () => {
-    const verdi = hentVisning(false, true, mellomlagring);
     expect(verdi).toBe(VisningModus.AKTIV_UTEN_AVBRYT);
   });
 
@@ -38,18 +18,8 @@ describe('Visning av vilkårskort i førstegangsbehandling', () => {
     expect(verdi).toBe(VisningModus.LÅST_UTEN_ENDRE);
   });
 
-  it('skal være LÅST_UTEN_ENDRE hvis readOnly=true, erAktivtSteg=false, mellomlagring=definert', () => {
-    const verdi = hentVisning(true, false, mellomlagring);
-    expect(verdi).toBe(VisningModus.LÅST_UTEN_ENDRE);
-  });
-
   it('skal være LÅST_UTEN_ENDRE hvis readOnly=true, erAktivtSteg=true, mellomlagring=undefined', () => {
     const verdi = hentVisning(true, true, undefined);
-    expect(verdi).toBe(VisningModus.LÅST_UTEN_ENDRE);
-  });
-
-  it('skal være LÅST_UTEN_ENDRE hvis readOnly=true, erAktivtSteg=true, mellomlagring=definert', () => {
-    const verdi = hentVisning(true, true, mellomlagring);
     expect(verdi).toBe(VisningModus.LÅST_UTEN_ENDRE);
   });
 });
