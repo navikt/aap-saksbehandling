296e830e95b94e7bc475834dad4f88eb707cfe3f
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-02-04 14:29:00 +0100

    Fjerner toggling av SosialRefusjon og forenkler refusjonskomponenten

--- components/behandlinger/sykdom/refusjon/RefusjonMedDataFetching.tsx
+++ components/behandlinger/sykdom/refusjon/RefusjonMedDataFetching.tsx
@@ -4,8 +4,6 @@ import { isError } from 'lib/utils/api';
 import { Behovstype } from 'lib/utils/form';
 import { skalViseSteg, StegData } from 'lib/utils/steg';
 import { Refusjon } from 'components/behandlinger/sykdom/refusjon/Refusjon';
-import { GammelRefusjon } from 'components/behandlinger/sykdom/refusjon/GammelRefusjon';
-import { unleashService } from 'lib/services/unleash/unleashService';
 
 interface Props {
   behandlingsReferanse: string;
@@ -26,23 +24,12 @@ export const RefusjonMedDataFetching = async ({ behandlingsReferanse, stegData }
     return null;
   }
 
-  if (!unleashService.isEnabled('SosialRefusjon')) {
-    return (
-      <GammelRefusjon
-        grunnlag={refusjonGrunnlag.data}
-        readOnly={stegData.readOnly || !refusjonGrunnlag.data.harTilgangTilÅSaksbehandle}
-        behandlingVersjon={stegData.behandlingVersjon}
-        initialMellomlagretVurdering={initialMellomlagretVurdering}
-      />
-    );
-  } else {
-    return (
-      <Refusjon
-        grunnlag={refusjonGrunnlag.data}
-        readOnly={stegData.readOnly || !refusjonGrunnlag.data.harTilgangTilÅSaksbehandle}
-        behandlingVersjon={stegData.behandlingVersjon}
-        initialMellomlagretVurdering={initialMellomlagretVurdering}
-      />
-    );
-  }
+  return (
+    <Refusjon
+      grunnlag={refusjonGrunnlag.data}
+      readOnly={stegData.readOnly || !refusjonGrunnlag.data.harTilgangTilÅSaksbehandle}
+      behandlingVersjon={stegData.behandlingVersjon}
+      initialMellomlagretVurdering={initialMellomlagretVurdering}
+    />
+  );
 };
