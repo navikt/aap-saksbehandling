fc6fcfa36a0193bb134b7d234983cf132f7b6046
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-02-12 13:13:33 +0100

    Fjern toggles som ikke er i bruk lenger fra unleashToggles.ts

--- lib/services/unleash/unleashToggles.ts
+++ lib/services/unleash/unleashToggles.ts
@@ -1,13 +1,7 @@
 export const FLAGS = [
-  'OvergangArbeidFrontend',
-  'PeriodisertSPEFrontend',
-  'PeriodisertOvergangUfore',
-  'BistandPeriodisert',
-  'PeriodisertNedsattArbeidsevneFrontend',
   'VisArenahistorikkKnapp',
   'ManglendePGIOgEosInntekter',
   'VisAvslagsaarsaker',
-  'SosialRefusjon',
   'DigitaliseringAvMeldekortV2Frontend',
   'RevurderStarttidspunkt',
   'VisRettigheterForVedtak',
@@ -18,15 +12,9 @@ export type FlagNames = (typeof FLAGS)[number];
 export type Flags = Record<FlagNames, boolean>;
 
 export const mockedFlags: Flags = {
-  OvergangArbeidFrontend: true,
-  PeriodisertOvergangUfore: true,
-  PeriodisertNedsattArbeidsevneFrontend: true,
-  BistandPeriodisert: true,
-  PeriodisertSPEFrontend: true,
   VisArenahistorikkKnapp: true,
   ManglendePGIOgEosInntekter: true,
   VisAvslagsaarsaker: true,
-  SosialRefusjon: true,
   DigitaliseringAvMeldekortV2Frontend: true,
   RevurderStarttidspunkt: true,
   VisRettigheterForVedtak: true,
