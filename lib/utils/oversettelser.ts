925c96bff65edc2a48d4e5b2017ca25eca84c1a8
Tor Idland <tor.idland@nav.no>
2026-02-13 13:30:00 +0100

    endrer litt oversettelser på på vent-grunner, og legger til egen grunn for venter på  avslag § 11-27

--- lib/utils/oversettelser.ts
+++ lib/utils/oversettelser.ts
@@ -108,6 +108,8 @@ export function mapTilVenteÅrsakTekst(årsak: SettPåVentÅrsaker | PostmottakS
       return 'Venter på klageimplementasjon';
     case 'VENTER_PÅ_SVAR_PÅ_FORHÅNDSVARSEL':
       return 'Venter på svar på forhåndsvarsel';
+    case 'VENTER_PÅ_FUNKSJONALITET_AVSLAG_11_27':
+      return 'Venter på manglende funksjonalitet for å avslå på § 11-27';
     case 'VENTER_PÅ_FUNKSJONALITET':
       return 'Venter på manglende funksjonalitet';
     case 'VENTER_PÅ_BEHANDLING_I_GOSYS':
