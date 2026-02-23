aa7ade3885b7c269834e578a2bad7415914c5e32
Fredrik Meyer <fredrik.meyer@nav.no>
2026-02-23 08:31:25 +0100

    Oppdater vurderingsaarsaker + tekstendringer

--- components/postmottak/digitaliserdokument/DigitaliserDokument.tsx
+++ components/postmottak/digitaliserdokument/DigitaliserDokument.tsx
@@ -35,6 +35,7 @@ export const DigitaliserDokument = ({
 }: Props) => {
   const [kategori, setKategori] = useState<KategoriserDokumentKategori | undefined>(grunnlag.vurdering?.kategori);
   const { løsBehovOgGåTilNesteSteg, status, isLoading } = usePostmottakLøsBehovOgGåTilNesteSteg('DIGITALISER_DOKUMENT');
+  const isRevurderingStarttidspunktEnabled = useFeatureFlag('RevurderStarttidspunkt');
 
   const nyDigitaliseringAvMeldekortEnabled = useFeatureFlag('DigitaliseringAvMeldekortV2Frontend');
 
@@ -90,6 +91,7 @@ export const DigitaliserDokument = ({
           grunnlag={grunnlag}
           readOnly={readOnly}
           isLoading={isLoading}
+          isRevurderingStarttidspunktEnabled={isRevurderingStarttidspunktEnabled}
         />
       )}
     </VStack>
