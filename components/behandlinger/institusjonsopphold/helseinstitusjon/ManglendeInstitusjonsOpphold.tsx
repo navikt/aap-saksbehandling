9700ef67a63dc2d1b10672454db9391829bd9bd4
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-03-02 10:05:56 +0100

    Slett gammel Helseinstitusjon

--- components/behandlinger/institusjonsopphold/helseinstitusjon/ManglendeInstitusjonsOpphold.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ManglendeOpphold } from 'components/behandlinger/institusjonsopphold/helseinstitusjon/ManglendeOpphold';
-
-export const ManglendeInstitusjonsOpphold = () => {
-  return <ManglendeOpphold />;
-};
