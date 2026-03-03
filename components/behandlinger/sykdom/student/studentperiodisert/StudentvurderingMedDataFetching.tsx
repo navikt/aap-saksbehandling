f89d0184327f6262ee226773430b56a24e4dde0b
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-03-03 14:02:17 +0100

    Prodsetter periodisering av student

--- components/behandlinger/sykdom/student/studentperiodisert/StudentvurderingMedDataFetching.tsx
+++ components/behandlinger/sykdom/student/studentperiodisert/StudentvurderingMedDataFetching.tsx
@@ -3,9 +3,7 @@ import { isError } from 'lib/utils/api';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 import { Behovstype } from 'lib/utils/form';
 import { skalViseSteg, StegData } from 'lib/utils/steg';
-import { Studentvurdering } from 'components/behandlinger/sykdom/student/student/Studentvurdering';
 import { StudentVurderingPeriodisert } from 'components/behandlinger/sykdom/student/studentperiodisert/StudentVurderingPeriodisert';
-import { unleashService } from 'lib/services/unleash/unleashService';
 
 interface Props {
   behandlingsreferanse: string;
@@ -28,15 +26,8 @@ export const StudentvurderingMedDataFetching = async ({ behandlingsreferanse, st
     return null;
   }
 
-  return unleashService.isEnabled('studentVurderingPeriodisert') ? (
+  return (
     <StudentVurderingPeriodisert
-      grunnlag={grunnlag.data}
-      readOnly={stegData.readOnly || !grunnlag.data.harTilgangTilÅSaksbehandle}
-      behandlingVersjon={stegData.behandlingVersjon}
-      initialMellomlagretVurdering={initialMellomlagretVurdering}
-    />
-  ) : (
-    <Studentvurdering
       grunnlag={grunnlag.data}
       readOnly={stegData.readOnly || !grunnlag.data.harTilgangTilÅSaksbehandle}
       behandlingVersjon={stegData.behandlingVersjon}
