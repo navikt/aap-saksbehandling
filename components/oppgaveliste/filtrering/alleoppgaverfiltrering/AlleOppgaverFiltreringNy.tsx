c940f2f205305b39c9cbb55093b9785bd363febe
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-03-05 11:04:36 +0100

    Tillat filtrering innad av forhåndsdefinerte køer i oppgavestyring

---
+++ components/oppgaveliste/filtrering/alleoppgaverfiltrering/AlleOppgaverFiltreringNy.tsx
@@ -0,0 +1,179 @@
+'use client';
+
+import { BodyShort, Box, Button, Chips, Detail, HGrid, HStack, VStack } from '@navikt/ds-react';
+
+import styles from '../Filtrering.module.css';
+import { Dispatch, SetStateAction, useState, useTransition } from 'react';
+import { FilterIcon, XMarkIcon } from '@navikt/aksel-icons';
+import { FormFields } from 'components/form/FormHook';
+import { FormField } from 'components/form/FormField';
+import { FieldPath, UseFormReturn } from 'react-hook-form';
+import { FormFieldsFilter } from 'components/oppgaveliste/mineoppgaverny/MineOppgaverNy';
+import { aktiveFiltreringer } from 'components/oppgaveliste/filtrering/filtreringUtils';
+import { avreserverOppgaveClient } from 'lib/oppgaveClientApi';
+import { isSuccess } from 'lib/utils/api';
+import { useTildelOppgaver } from 'context/oppgave/TildelOppgaverContext';
+
+interface Props {
+  form: UseFormReturn<FormFieldsFilter>;
+  formFields: FormFields<FieldPath<FormFieldsFilter>, FormFieldsFilter>;
+  antallOppgaver?: number;
+  valgteRader: number[];
+  setValgteRader: Dispatch<SetStateAction<number[]>>;
+  revalidateFunction: () => void;
+}
+
+export const AlleOppgaverFiltreringNy = ({
+  form,
+  formFields,
+  antallOppgaver,
+  valgteRader,
+  revalidateFunction,
+  setValgteRader,
+}: Props) => {
+  const [åpneFilter, setÅpneFilter] = useState(false);
+  const [isPendingFrigi, startTransitionFrigi] = useTransition();
+  const { visModal, setOppgaveIder } = useTildelOppgaver();
+
+  const aktiveFilter = aktiveFiltreringer(form.watch());
+
+  const frigiValgteOppgaver = async (oppgaver: number[]) => {
+    startTransitionFrigi(async () => {
+      const res = await avreserverOppgaveClient(oppgaver);
+      if (isSuccess(res)) {
+        revalidateFunction();
+        setValgteRader([]);
+      }
+    });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <HStack justify={'space-between'} align={'center'} className={styles.filtreringTop}>
+        <HStack gap={'4'}>
+          {valgteRader.length > 0 && (
+            <>
+              <HStack gap={'2'} align={'baseline'}>
+                <Detail>{valgteRader.length} oppgaver valgt.</Detail>
+                <Button
+                  onClick={() => frigiValgteOppgaver(valgteRader)}
+                  loading={isPendingFrigi}
+                  type={'button'}
+                  size={'small'}
+                  variant={'secondary'}
+                >
+                  Frigi valgte oppgaver
+                </Button>
+                <Button
+                  onClick={() => {
+                    setOppgaveIder(valgteRader);
+                    visModal();
+                    setValgteRader([]);
+                  }}
+                  type={'button'}
+                  size={'small'}
+                  variant={'secondary'}
+                >
+                  Tildel valgte oppgaver
+                </Button>
+              </HStack>
+              <div className={styles.divider} />
+            </>
+          )}
+
+          <HStack gap={'2'} align={'center'}>
+            <Button
+              icon={åpneFilter ? <XMarkIcon /> : <FilterIcon />}
+              iconPosition={'right'}
+              variant={'secondary'}
+              size={'small'}
+              onClick={() => setÅpneFilter(!åpneFilter)}
+            >
+              {åpneFilter ? 'Lukk filter' : 'Filtrer listen'}
+            </Button>
+            {aktiveFilter.length > 0 && (
+              <HStack gap={'2'}>
+                <BodyShort>Filtre: </BodyShort>
+                <Chips size={'small'}>
+                  {aktiveFilter.map((filter) => (
+                    <Chips.Removable
+                      key={filter.value}
+                      onClick={() => {
+                        const values = form.watch(filter.key);
+                        if (Array.isArray(values)) {
+                          const arrayUtenValgtFilter = values.filter((value) => value !== filter.value);
+                          form.setValue(filter.key, arrayUtenValgtFilter);
+                        } else {
+                          form.setValue(filter.key, undefined);
+                        }
+                      }}
+                    >
+                      {filter.label}
+                    </Chips.Removable>
+                  ))}
+                </Chips>
+              </HStack>
+            )}
+          </HStack>
+        </HStack>
+
+        <Detail>Totalt {antallOppgaver} oppgaver</Detail>
+      </HStack>
+      {åpneFilter && (
+        <div className={styles.filtreringwrapper}>
+          <div className={styles.filtrering}>
+            <HGrid columns={{ sm: 1, md: 2, lg: 4, xl: 5 }} gap={'2'}>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.behandlingstyper} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <VStack gap={'4'}>
+                  <BodyShort size={'small'} weight={'semibold'}>
+                    Behandling opprettet
+                  </BodyShort>
+                  <FormField form={form} formField={formFields.behandlingOpprettetFom} />
+                  <FormField form={form} formField={formFields.behandlingOpprettetTom} />
+                </VStack>
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.årsaker} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.avklaringsbehov} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.statuser} />
+              </BoxWrapper>
+            </HGrid>
+            <HStack gap={'2'}>
+              <Button
+                size={'small'}
+                variant={'tertiary'}
+                onClick={() => {
+                  form.reset();
+                }}
+              >
+                Nullstill
+              </Button>
+            </HStack>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+function BoxWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      height={'fit-content'}
+      borderColor={'border-divider'}
+      borderWidth={'2'}
+      borderRadius={'xlarge'}
+      paddingInline={'4'}
+      paddingBlock={'4'}
+    >
+      {children}
+    </Box>
+  );
+}
---
+++ components/oppgaveliste/filtrering/ledigeoppgaverfiltrering/LedigeOppgaverFiltreringNy.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import { BodyShort, Box, Button, Chips, Detail, HGrid, HStack, VStack } from '@navikt/ds-react';
+
+import styles from '../Filtrering.module.css';
+import { useState } from 'react';
+import { FilterIcon, XMarkIcon } from '@navikt/aksel-icons';
+import { FormFields } from 'components/form/FormHook';
+import { FormField } from 'components/form/FormField';
+import { FieldPath, UseFormReturn } from 'react-hook-form';
+import { FormFieldsFilter } from 'components/oppgaveliste/mineoppgaverny/MineOppgaverNy';
+import { aktiveFiltreringer } from 'components/oppgaveliste/filtrering/filtreringUtils';
+
+interface Props {
+  form: UseFormReturn<FormFieldsFilter>;
+  formFields: FormFields<FieldPath<FormFieldsFilter>, FormFieldsFilter>;
+  antallOppgaver?: number;
+}
+
+export const LedigeOppgaverFiltreringNy = ({ form, formFields, antallOppgaver }: Props) => {
+  const [åpneFilter, setÅpneFilter] = useState(false);
+
+  const aktiveFilter = aktiveFiltreringer(form.watch());
+
+  return (
+    <div className={styles.wrapper}>
+      <HStack justify={'space-between'} align={'center'} className={styles.filtreringTop}>
+        <HStack gap={'2'} align={'center'}>
+          <Button
+            icon={åpneFilter ? <XMarkIcon /> : <FilterIcon />}
+            iconPosition={'right'}
+            variant={'secondary'}
+            size={'small'}
+            onClick={() => setÅpneFilter(!åpneFilter)}
+          >
+            {åpneFilter ? 'Lukk filter' : 'Filtrer listen'}
+          </Button>
+          {aktiveFilter.length > 0 && (
+            <HStack gap={'2'}>
+              <BodyShort>Filtre: </BodyShort>
+              <Chips size={'small'}>
+                {aktiveFilter.map((filter) => (
+                  <Chips.Removable
+                    key={filter.value}
+                    onClick={() => {
+                      const values = form.watch(filter.key);
+                      if (Array.isArray(values)) {
+                        const arrayUtenValgtFilter = values.filter((value) => value !== filter.value);
+                        form.setValue(filter.key, arrayUtenValgtFilter);
+                      } else {
+                        form.setValue(filter.key, undefined);
+                      }
+                    }}
+                  >
+                    {filter.label}
+                  </Chips.Removable>
+                ))}
+              </Chips>
+            </HStack>
+          )}
+        </HStack>
+        <Detail>Totalt {antallOppgaver} oppgaver</Detail>
+      </HStack>
+      {åpneFilter && (
+        <div className={styles.filtreringwrapper}>
+          <div className={styles.filtrering}>
+            <HGrid columns={{ sm: 1, md: 2, lg: 4, xl: 5 }} gap={'2'}>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.behandlingstyper} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <VStack gap={'4'}>
+                  <BodyShort size={'small'} weight={'semibold'}>
+                    Behandling opprettet
+                  </BodyShort>
+                  <FormField form={form} formField={formFields.behandlingOpprettetFom} />
+                  <FormField form={form} formField={formFields.behandlingOpprettetTom} />
+                </VStack>
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.årsaker} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.avklaringsbehov} />
+              </BoxWrapper>
+              <BoxWrapper>
+                <FormField form={form} formField={formFields.statuser} />
+              </BoxWrapper>
+            </HGrid>
+            <HStack gap={'2'}>
+              <Button
+                size={'small'}
+                variant={'tertiary'}
+                onClick={() => {
+                  form.reset();
+                }}
+              >
+                Nullstill
+              </Button>
+            </HStack>
+          </div>
+        </div>
+      )}{' '}
+    </div>
+  );
+};
+
+function BoxWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      height={'fit-content'}
+      borderColor={'border-divider'}
+      borderWidth={'2'}
+      borderRadius={'xlarge'}
+      paddingInline={'4'}
+      paddingBlock={'4'}
+    >
+      {children}
+    </Box>
+  );
+}
