9dd68151ef567fc650e8eee983a2da37d1897372
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2026-03-17 07:02:41 +0100

    Legg til barnepensjon i revurderingslisten (#1854)

--- components/postmottak/digitaliserdokument/annetrelevantdokument/DigitaliserAnnetRelevantDokument.tsx
+++ components/postmottak/digitaliserdokument/annetrelevantdokument/DigitaliserAnnetRelevantDokument.tsx
@@ -20,6 +20,7 @@ interface Props extends Submittable {
   grunnlag: DigitaliseringsGrunnlag;
   readOnly: boolean;
   isLoading: boolean;
+  inkluderBarnepensjon: boolean;
 }
 
 function mapTilAnnetRelevantDokumentKontrakt(data: AnnetRelevantDokumentFormFields) {
@@ -31,12 +32,20 @@ function mapTilAnnetRelevantDokumentKontrakt(data: AnnetRelevantDokumentFormFiel
   return JSON.stringify(dokument);
 }
 
-export const DigitaliserAnnetRelevantDokument = ({ grunnlag, readOnly, submit, isLoading }: Props) => {
+export const DigitaliserAnnetRelevantDokument = ({
+  grunnlag,
+  readOnly,
+  submit,
+  isLoading,
+  inkluderBarnepensjon = false,
+}: Props) => {
   const annetRelevantDokumentGrunnlag: AnnetRelevantDokument = grunnlag.vurdering?.strukturertDokumentJson
     ? JSON.parse(grunnlag.vurdering?.strukturertDokumentJson)
     : {};
 
-  const vurderingsbehov = vurderingsbehovOptions();
+  const vurderingsbehov = vurderingsbehovOptions().filter(
+    (option) => inkluderBarnepensjon || option.value !== 'REVURDER_SAMORDNING_BARNEPENSJON'
+  );
   const defaultÅrsakOptions: string[] = (annetRelevantDokumentGrunnlag.årsakerTilBehandling || [])
     .map((årsakFraGrunnlag) => vurderingsbehov.find((årsak) => årsak.value === årsakFraGrunnlag))
     .filter((e) => e !== undefined)
