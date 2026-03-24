847d550296f54c343204ec73168ef0826220a433
Tor Idland <tor.idland@nav.no>
2026-03-24 12:59:12 +0100

    fjern rettighetsdata fra underveisgrunnlag-tabell

--- components/behandlinger/underveis/underveisgrunnlag/UnderveisgrunnlagMedDatafetching.tsx
+++ components/behandlinger/underveis/underveisgrunnlag/UnderveisgrunnlagMedDatafetching.tsx
@@ -1,36 +1,24 @@
 import { Underveisgrunnlag } from 'components/behandlinger/underveis/underveisgrunnlag/Underveisgrunnlag';
-import { hentRettighetsdata, hentUnderveisGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { hentUnderveisGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
-import { isError, isSuccess } from 'lib/utils/api';
+import { isError } from 'lib/utils/api';
 
 interface Props {
   behandlingsreferanse: string;
   readOnly: boolean;
   behandlingVersjon: number;
-  saksnummer: string;
 }
 
 export const UnderveisgrunnlagMedDataFetching = async ({
   behandlingsreferanse,
   readOnly,
   behandlingVersjon,
-  saksnummer,
 }: Props) => {
-  const [grunnlag, rettighetsdataRespons] = await Promise.all([
-    hentUnderveisGrunnlag(behandlingsreferanse),
-    hentRettighetsdata(saksnummer),
-  ]);
+  const [grunnlag] = await Promise.all([hentUnderveisGrunnlag(behandlingsreferanse)]);
 
   if (isError(grunnlag)) {
     return <ApiException apiResponses={[grunnlag]} />;
   }
 
-  return (
-    <Underveisgrunnlag
-      readOnly={readOnly}
-      behandlingVersjon={behandlingVersjon}
-      grunnlag={grunnlag.data}
-      rettighetsdata={isSuccess(rettighetsdataRespons) ? rettighetsdataRespons.data : []}
-    />
-  );
+  return <Underveisgrunnlag readOnly={readOnly} behandlingVersjon={behandlingVersjon} grunnlag={grunnlag.data} />;
 };
