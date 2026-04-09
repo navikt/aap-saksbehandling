65e509896b370f7ed2e91319eba0050e4a1f7ab7
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-04-09 15:33:21 +0200

    Legger på mer informasjon i errormelding for å se hvilke tilfeller loggene spammes

--- hooks/saksbehandling/FlytHook.tsx
+++ hooks/saksbehandling/FlytHook.tsx
@@ -45,7 +45,9 @@ export function useRequiredFlyt(): { flyt: BehandlingFlytOgTilstand; refetchFlyt
   );
 
   if (isError(flyt)) {
-    throw new Error(`Feil oppsto ved henting av påkrevd flyt: ${flyt.apiException.message}`);
+    throw new Error(
+      `Feil oppsto ved henting av påkrevd flyt: ${flyt.apiException.message} med kode ${flyt.apiException.code} og status ${flyt.status}`
+    );
   } else if (!flyt) {
     throw new Error('Kunne ikke finne påkrevd flyt');
   }
