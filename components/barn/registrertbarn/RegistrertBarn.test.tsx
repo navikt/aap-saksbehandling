1b848bd5ae8cdce622d73dee0bb487b1287f543f
Hein Haraldsen <hein.haraldsen@nav.no>
2026-04-13 11:05:39 +0200

    Fikser tester

--- components/barn/registrertbarn/RegistrertBarn.test.tsx
+++ components/barn/registrertbarn/RegistrertBarn.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 import { RegistrertBarn } from 'components/barn/registrertbarn/RegistrertBarn';
 import { IdentifisertBarn } from 'lib/types/types';
 import { kalkulerAlder } from 'components/behandlinger/alder/Alder';
+import { FeatureFlagProvider } from 'context/UnleashContext';
+import { mockedFlags } from 'lib/services/unleash/unleashToggles';
 
 const registrertBarn: IdentifisertBarn = {
   ident: {
@@ -13,20 +15,50 @@ const registrertBarn: IdentifisertBarn = {
   forsorgerPeriode: { fom: '2020-03-03', tom: '2038-03-03' },
 };
 
+const registrertDødtBarn: IdentifisertBarn = {
+  ident: {
+    identifikator: '98765432121',
+    aktivIdent: true,
+  },
+  fodselsDato: '2020-01-01',
+  dodsDato: '2020-01-03',
+};
+
 describe('registrert barn', () => {
   it('har en heading med navn,pid og alder', () => {
-    render(<RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />);
+    render(
+      <FeatureFlagProvider flags={mockedFlags}>
+        <RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />
+      </FeatureFlagProvider>
+    );
     const forventetAlder = kalkulerAlder(new Date(registrertBarn.fodselsDato!!));
     expect(screen.getByText(`TOR NADO, ${registrertBarn?.ident?.identifikator} (${forventetAlder})`)).toBeVisible();
   });
 
   it('har en tekst som viser at det er et folkeregistrert barn', () => {
-    render(<RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />);
+    render(
+      <FeatureFlagProvider flags={mockedFlags}>
+        <RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />
+      </FeatureFlagProvider>
+    );
     expect(screen.getByText('Folkeregistrert barn')).toBeVisible();
   });
 
-  it('viser forsørgerperiode', () => {
-    render(<RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />);
-    expect(screen.getByText(/^Forsørgerperiode: 03.03.2020 - 03.03.2038$/)).toBeVisible();
+  it('viser fyller 18 år', () => {
+    render(
+      <FeatureFlagProvider flags={mockedFlags}>
+        <RegistrertBarn registrertBarn={registrertBarn} navn={'TOR NADO'} />
+      </FeatureFlagProvider>
+    );
+    expect(screen.getByText('Fyller 18 år: 03.03.2038')).toBeVisible();
+  });
+
+  it('viser dødsdato', () => {
+    render(
+      <FeatureFlagProvider flags={mockedFlags}>
+        <RegistrertBarn registrertBarn={registrertDødtBarn} navn={'TOR NADO'} />
+      </FeatureFlagProvider>
+    );
+    expect(screen.getByText('Død: 03.01.2020')).toBeVisible();
   });
 });
--- components/barn/registrertbarn/RegistrertBarn.tsx
+++ components/barn/registrertbarn/RegistrertBarn.tsx
@@ -5,6 +5,7 @@ import { IdentifisertBarn } from 'lib/types/types';
 import styles from './RegistrertBarn.module.css';
 import { kalkulerAlder } from 'components/behandlinger/alder/Alder';
 import { ChildEyesIcon } from '@navikt/aksel-icons';
+import { useFeatureFlag } from '../../../context/UnleashContext';
 
 interface Props {
   registrertBarn: IdentifisertBarn;
@@ -12,34 +13,58 @@ interface Props {
 }
 
 export const RegistrertBarn = ({ registrertBarn, navn }: Props) => {
-  return (
-    <section className={styles.barn}>
-      <div>
-        <ChildEyesIcon title={`registrert barn ${registrertBarn.ident}`} fontSize={'2rem'} />
-      </div>
-      <div className={styles.tekst}>
-        <Detail className={styles.detailgray}>Folkeregistrert barn</Detail>
-        <BodyShort size={'small'}>
-          {navn}, {registrertBarn?.ident?.identifikator} (
-          {registrertBarn.fodselsDato ? kalkulerAlder(new Date(registrertBarn.fodselsDato)) : 'Ukjent alder'})
-          {registrertBarn.dodsDato ? (
-            <Tag variant="alt1" data-color="neutral">
-              Død
-            </Tag>
-          ) : null}
-        </BodyShort>
-        <BodyShort size={'small'}>
-          {registrertBarn.fodselsDato
-            ? 'Født:' + formaterDatoForFrontend(registrertBarn.fodselsDato)
-            : 'Født: ' + 'Ukjent dato'}
-        </BodyShort>
-        <BodyShort size={'small'}>
-          {registrertBarn.dodsDato
-            ? 'Død:' + formaterDatoForFrontend(registrertBarn.dodsDato)
-            : 'Fyller 18 år: ' +
-              `${registrertBarn?.forsorgerPeriode?.tom ? `${formaterDatoForFrontend(registrertBarn.forsorgerPeriode.tom)}` : 'Ukjent dato'}`}
-        </BodyShort>
-      </div>
-    </section>
-  );
+  const dodsDatoFeatureToggle = useFeatureFlag('DodsdatoBarn');
+  if (dodsDatoFeatureToggle)
+    return (
+      <section className={styles.barn}>
+        <div>
+          <ChildEyesIcon title={`registrert barn ${registrertBarn.ident}`} fontSize={'2rem'} />
+        </div>
+        <div className={styles.tekst}>
+          <Detail className={styles.detailgray}>Folkeregistrert barn</Detail>
+          <BodyShort size={'small'}>
+            {navn}, {registrertBarn?.ident?.identifikator} (
+            {registrertBarn.fodselsDato ? kalkulerAlder(new Date(registrertBarn.fodselsDato)) : 'Ukjent alder'})
+            {registrertBarn.dodsDato ? (
+              <Tag variant="alt1" data-color="neutral">
+                Død
+              </Tag>
+            ) : null}
+          </BodyShort>
+          <BodyShort size={'small'}>
+            {registrertBarn.fodselsDato
+              ? 'Født:' + formaterDatoForFrontend(registrertBarn.fodselsDato)
+              : 'Født: ' + 'Ukjent dato'}
+          </BodyShort>
+          <BodyShort size={'small'}>
+            {registrertBarn.dodsDato
+              ? 'Død: ' + formaterDatoForFrontend(registrertBarn.dodsDato)
+              : 'Fyller 18 år: ' +
+                `${registrertBarn?.forsorgerPeriode?.tom ? `${formaterDatoForFrontend(registrertBarn.forsorgerPeriode.tom)}` : 'Ukjent dato'}`}
+          </BodyShort>
+        </div>
+      </section>
+    );
+  else
+    return (
+      <section className={styles.barn}>
+        <div>
+          <ChildEyesIcon title={`registrert barn ${registrertBarn.ident}`} fontSize={'2rem'} />
+        </div>
+        <div className={styles.tekst}>
+          <Detail className={styles.detailgray}>Folkeregistrert barn</Detail>
+          <BodyShort size={'small'}>
+            {navn}, {registrertBarn?.ident?.identifikator} (
+            {registrertBarn.fodselsDato ? kalkulerAlder(new Date(registrertBarn.fodselsDato)) : 'Ukjent alder'})
+          </BodyShort>
+          <BodyShort size={'small'}>
+            Forsørgerperiode:{' '}
+            {registrertBarn.forsorgerPeriode?.fom
+              ? formaterDatoForFrontend(registrertBarn.forsorgerPeriode.fom)
+              : 'Ukjent'}
+            {` - ${registrertBarn?.forsorgerPeriode?.tom ? `${formaterDatoForFrontend(registrertBarn.forsorgerPeriode.tom)}` : ''}`}
+          </BodyShort>
+        </div>
+      </section>
+    );
 };
