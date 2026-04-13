414a3eeb72792a50052a9368e33381a3c07f32a1
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-04-13 12:19:45 +0200

    Legg til nytt vilkårskort uten mellomlagring
    Vi har flere steder i koden der mellomlagring ikke er nødvendig, men hvor et vilkårskort fortsatt trengs. Dette nye vilkårskortet gjør det mulig å unngå unødvendige props.

--- components/behandlinger/revurdering/avbrytVurdering/vurdering/AvbrytRevurderingVurdering.tsx
+++ components/behandlinger/revurdering/avbrytVurdering/vurdering/AvbrytRevurderingVurdering.tsx
@@ -8,7 +8,7 @@ import { Behovstype } from 'lib/utils/form';
 import { FormEvent } from 'react';
 import { AvbrytRevurderingGrunnlag } from 'lib/types/types';
 import { useVilkårskortVisning } from 'hooks/saksbehandling/visning/VisningHook';
-import { VilkårskortMedFormOgMellomlagring } from 'components/vilkårskort/vilkårskortmedformogmellomlagring/VilkårskortMedFormOgMellomlagring';
+import { VilkårskortMedForm } from 'components/vilkårskort/vilkårskortmedform/VilkårskortMedForm';
 
 interface Props {
   behandlingVersjon: number;
@@ -77,7 +77,7 @@ export const AvbrytRevurderingVurdering = ({ grunnlag, readOnly, behandlingVersj
   };
 
   return (
-    <VilkårskortMedFormOgMellomlagring
+    <VilkårskortMedForm
       heading={'Avbryt revurdering'}
       steg={'AVBRYT_REVURDERING'}
       onSubmit={handleSubmit}
@@ -85,14 +85,12 @@ export const AvbrytRevurderingVurdering = ({ grunnlag, readOnly, behandlingVersj
       isLoading={isLoading}
       løsBehovOgGåTilNesteStegError={løsBehovOgGåTilNesteStegError}
       vilkårTilhørerNavKontor={false}
-      onDeleteMellomlagringClick={undefined}
-      mellomlagretVurdering={undefined}
       visningModus={visningModus}
       visningActions={visningActions}
       formReset={() => form.reset()}
     >
       <FormField form={form} formField={formFields.aarsak} className="årsak" />
       <FormField form={form} formField={formFields.begrunnelse} className="begrunnelse" />
-    </VilkårskortMedFormOgMellomlagring>
+    </VilkårskortMedForm>
   );
 };
