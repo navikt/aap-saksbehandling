ec542527e260815cf19a8e5271416c803c15e086
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-04-29 21:20:30 +0200

    Legg til umami i dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,32 @@ import { InnloggetBrukerContextProvider } from 'context/InnloggetBrukerContext';
 import { FeatureFlagProvider } from 'context/UnleashContext';
 import { getAllFlags } from 'lib/services/unleash/unleashService';
 import { Metadata } from 'next';
+import { isDev } from 'lib/utils/environment';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Kelvin',
   description: 'Saksbehandlingssystem for AAP',
 };
 
+const umamiSporingskode = 'ebb233f3-6c6d-4b9f-b84d-9a11a3c2f16f';
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const [brukerInformasjon, roller] = await Promise.all([hentBrukerInformasjon(), hentRollerForBruker()]);
 
   return (
     <html lang="nb">
+      <head>
+        {isDev() && (
+          <Script
+            defer
+            id="umami"
+            strategy="afterInteractive"
+            src="https://cdn.nav.no/team-researchops/sporing/sporing-dev.js"
+            data-website-id={umamiSporingskode}
+          />
+        )}
+      </head>
       <body>
         <FeatureFlagProvider flags={getAllFlags()}>
           <InnloggetBrukerContextProvider bruker={brukerInformasjon}>
