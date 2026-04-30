f561b6d76393ebc79ff217f39853c95eaac286e1
Tor Idland <tor.idland@nav.no>
2026-04-30 19:57:34 +0200

    personside som bruker kryptertIdent fra url til å hente ident

--- components/saksinfobanner/SaksinfoBanner.tsx
+++ components/saksinfobanner/SaksinfoBanner.tsx
@@ -80,6 +80,12 @@ export const SaksinfoBanner = ({
       <div className={styles.saksinfo}>
         <HStack gap={'space-8'} align="center">
           <Label size="small">
+            {/*@ts-ignore*/}
+            <Link href={`/person/${personInformasjon.kryptertIdent}`} title="Personside">
+              Personside
+            </Link>
+          </Label>
+          <Label size="small">
             <Link href={`/saksbehandling/sak/${sak.saksnummer}`} title="Tilbake til saksoversikt">
               {storForbokstavIHvertOrd(personInformasjon.navn)}
             </Link>
