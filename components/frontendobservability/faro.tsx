34ce54b372cf4c09a7b8e4d55000e4e0ba2778d8
Tor Idland <tor.idland@nav.no>
2026-05-05 12:11:02 +0200

    grafana faro for frontend observability

---
+++ components/frontendobservability/faro.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect } from 'react';
+import { faro, getWebInstrumentations, initializeFaro, LogLevel } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+export default function Faro({ collectorUrl }: { collectorUrl?: string }) {
+  useEffect(() => {
+    if (faro.api) return; // already initialized
+
+    try {
+      initializeFaro({
+        url: collectorUrl || 'https://telemetry.nav.no/collect',
+        paused: window.location.hostname === 'localhost',
+        app: {
+          name: 'saksbehandling',
+        },
+        instrumentations: [
+          ...getWebInstrumentations(),
+          new TracingInstrumentation({
+            instrumentationOptions: {
+              propagateTraceHeaderCorsUrls: [/https:\/\/[^/]+\.nav\.no\/.*/],
+            },
+          }),
+        ],
+        consoleInstrumentation: {
+          disabledLevels: [LogLevel.DEBUG, LogLevel.TRACE], // capture log, info, warn, error
+        },
+      });
+    } catch (e) {
+      console.warn('Faro initialization failed', e);
+    }
+  }, [collectorUrl]);
+
+  return null;
+}
