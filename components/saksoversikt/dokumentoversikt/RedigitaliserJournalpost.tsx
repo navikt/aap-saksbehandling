0d08acc871818d215f0892624c24d1383fe18d48
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-05-18 14:40:55 +0200

    oppdater tekst for redigitaliseringsknapper avkreft bekreft

--- components/saksoversikt/dokumentoversikt/RedigitaliserJournalpost.tsx
+++ components/saksoversikt/dokumentoversikt/RedigitaliserJournalpost.tsx
@@ -69,10 +69,10 @@ export const RedigitaliserJournalpost = ({
 
       <Modal.Footer>
         <Button variant="primary" onClick={redigitaliser} loading={isLoading}>
-          Ja, redigitaliser
+          Bekreft
         </Button>
         <Button variant="secondary" disabled={isLoading}>
-          Nei, avbryt
+          Avbryt
         </Button>
       </Modal.Footer>
     </Modal>
