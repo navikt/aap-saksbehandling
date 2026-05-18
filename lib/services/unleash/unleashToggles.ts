9343b60b89807d7545918e678c967e61796367cb
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-05-18 14:07:53 +0200

    Legg til detaljert data i maritim 11-2 check

--- lib/services/unleash/unleashToggles.ts
+++ lib/services/unleash/unleashToggles.ts
@@ -11,6 +11,7 @@ export const FLAGS = [
   'BegrunnelseForIkkeSendBrev',
   'YrkesskadeNyeFelter',
   'HastemarkeringerFoerst',
+  'ForutgaaendeGap',
 ] as const;
 
 export type FlagNames = (typeof FLAGS)[number];
@@ -29,4 +30,5 @@ export const mockedFlags: Flags = {
   BegrunnelseForIkkeSendBrev: true,
   YrkesskadeNyeFelter: true,
   HastemarkeringerFoerst: true,
+  ForutgaaendeGap: true,
 };
