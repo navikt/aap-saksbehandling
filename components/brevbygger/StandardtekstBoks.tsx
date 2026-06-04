5030f9f27a2f8259bcf62ee1183a47481612a838
Øivind Stensrud <oivind.stensrud@nav.no>
2026-06-04 13:29:07 +0200

    Lagt til manglende fil

---
+++ components/brevbygger/StandardtekstBoks.tsx
@@ -0,0 +1,13 @@
+import { Box, HGrid, Label } from '@navikt/ds-react';
+import { PadlockLockedIcon } from '@navikt/aksel-icons';
+
+export const StandardtekstBoks = () => (
+  <Box borderWidth="1" borderRadius="12" paddingInline="space-16" paddingBlock="space-8" borderColor="neutral-subtle">
+    <HGrid columns={'1fr auto'} gap={'space-2'}>
+      <PadlockLockedIcon aria-label="Låst seksjon" font-size="1.5rem" />
+      <Label size="small" style={{ opacity: '0.45' }}>
+        Dette er standardtekst og kan ikke endres.
+      </Label>
+    </HGrid>
+  </Box>
+);
