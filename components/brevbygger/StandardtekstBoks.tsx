818f7ca33138f26743fcab9eeff77fb021b21f91
Øivind Stensrud <oivind.stensrud@nav.no>
2026-06-08 12:04:46 +0200

    Trekker ut inline-styling

--- components/brevbygger/StandardtekstBoks.tsx
+++ components/brevbygger/StandardtekstBoks.tsx
@@ -1,11 +1,13 @@
 import { Box, HGrid, Label } from '@navikt/ds-react';
 import { PadlockLockedIcon } from '@navikt/aksel-icons';
 
+import styles from './StandardtekstBoks.module.css';
+
 export const StandardtekstBoks = () => (
   <Box borderWidth="1" borderRadius="12" paddingInline="space-16" paddingBlock="space-8" borderColor="neutral-subtle">
     <HGrid columns={'auto 1fr'} gap={'space-2'}>
       <PadlockLockedIcon aria-label="Låst seksjon" fontSize="1.5rem" />
-      <Label size="small" style={{ opacity: '0.45' }}>
+      <Label size="small" className={styles.mutedLabel}>
         Dette er standardtekst og kan ikke endres.
       </Label>
     </HGrid>
