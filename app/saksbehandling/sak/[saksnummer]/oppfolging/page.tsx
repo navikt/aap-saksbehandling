a806027e2714da74673acf1e09d1fc25a5decdbc
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-06-09 14:42:58 +0200

    Trekk ut data fra sakPersonInfo til egen hook

--- app/saksbehandling/sak/[saksnummer]/oppfolging/page.tsx
+++ app/saksbehandling/sak/[saksnummer]/oppfolging/page.tsx
@@ -1,16 +1,16 @@
 import { Box, Page as AkselPage } from '@navikt/ds-react';
-import { hentSak, hentSakPersoninfo } from 'lib/services/saksbehandlingservice/saksbehandlingService';
+import { hentSak } from 'lib/services/saksbehandlingservice/saksbehandlingService';
 import { SaksinfoBanner } from 'components/saksinfobanner/SaksinfoBanner';
 import { OpprettOppfølgingsBehandling } from 'components/saksoversikt/opprettoppfølgingsbehandling/OpprettOppfølgingsbehandling';
 
 export default async function Page(props: { params: Promise<{ saksnummer: string }> }) {
   const params = await props.params;
 
-  const [sak, personInfo] = await Promise.all([hentSak(params.saksnummer), hentSakPersoninfo(params.saksnummer)]);
+  const sak = await hentSak(params.saksnummer);
 
   return (
     <AkselPage>
-      <SaksinfoBanner personInformasjon={personInfo} sak={sak} />
+      <SaksinfoBanner sak={sak} />
       <Box marginBlock="space-32">
         <OpprettOppfølgingsBehandling saksnummer={sak.saksnummer} />
       </Box>
