93ea39354e7f9719f6e61e1b6b7508f0f6d7c2a0
SelmaBergstrand <78426277+SelmaBergstrand@users.noreply.github.com>
2026-07-16 12:10:19 +0200

    AAP-2235 Bumper oppgavetyper og fikser tester etter endringer på OppgaveDto (#2276)

--- lib/services/oppgaveservice/oppgaveservice.ts
+++ lib/services/oppgaveservice/oppgaveservice.ts
@@ -49,6 +49,8 @@ export async function hentOppgave(behandlingReferanse: string) {
     const mockResponse: FetchResponse<Oppgave> = {
       type: 'SUCCESS',
       data: {
+        id: 123,
+        personIdent: '123456',
         behandlingRef: 'dsfad',
         avklaringsbehovKode: '5008',
         behandlingOpprettet: '2025-08-20',
@@ -61,9 +63,6 @@ export async function hentOppgave(behandlingReferanse: string) {
         versjon: 0,
         vurderingsbehov: [],
         årsakerTilBehandling: [],
-        enhetForKø: '4491',
-        erPåVent: false,
-        erÅpen: true,
       },
     };
 
