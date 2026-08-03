1f27f8ff33ca2f7c1e1ceffd566d745b37d428ec
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-03 08:43:54 +0200

    Skru på Umami i prod

--- app/api/umami/route.ts
+++ app/api/umami/route.ts
@@ -1,13 +1,20 @@
 import umami, { UmamiEventData } from '@umami/node';
-import { UmamiKelvinEvent } from 'lib/types/types';
 import { logWarning } from 'lib/serverutlis/logger';
+import { UmamiKelvinEvent } from 'lib/types/types';
+import { isDev, isProd } from 'lib/utils/environment';
 import { NextResponse } from 'next/server';
-import { isDev } from 'lib/utils/environment';
 
-const umamiSporingskode = 'ebb233f3-6c6d-4b9f-b84d-9a11a3c2f16f';
+const umamiSporingskodeDev = 'ebb233f3-6c6d-4b9f-b84d-9a11a3c2f16f';
+const umamiSporingskodeProd = 'b2b87dc6-5a1c-4212-ac64-818f80fd12f2';
 if (isDev()) {
   umami.init({
-    websiteId: umamiSporingskode,
+    websiteId: umamiSporingskodeDev,
+    hostUrl: process.env.UMAMI_HOST_URL,
+  });
+}
+if (isProd()) {
+  umami.init({
+    websiteId: umamiSporingskodeProd,
     hostUrl: process.env.UMAMI_HOST_URL,
   });
 }
@@ -38,9 +45,7 @@ export async function POST(req: Request) {
     ...(eventData.brevtype ? { brevtype: eventData.brevtype } : {}),
   };
   try {
-    if (isDev()) {
-      await umami.track(eventData.name, filtrertEventData);
-    }
+    await umami.track(eventData.name, filtrertEventData);
   } catch {
     logWarning(`umami-event feilet: ${eventData.name}`);
   }
