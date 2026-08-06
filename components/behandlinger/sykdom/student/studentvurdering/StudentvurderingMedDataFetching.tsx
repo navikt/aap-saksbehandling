d22e602ed19f858edbc41d176106914bd49f3bc9
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-08-06 08:34:00 +0200

    AAP-2194 Fjern toggle for nytt studentsteg

--- components/behandlinger/sykdom/student/studentvurdering/StudentvurderingMedDataFetching.tsx
+++ components/behandlinger/sykdom/student/studentvurdering/StudentvurderingMedDataFetching.tsx
@@ -1,11 +1,9 @@
 import { hentMellomlagring, hentStudentGrunnlag } from 'lib/services/saksbehandlingservice/saksbehandlingService';
-import { unleashService } from 'lib/services/unleash/unleashService';
 import { isError } from 'lib/utils/api';
 import { Behovstype } from 'lib/utils/form';
 import { StegData, skalViseSteg } from 'lib/utils/steg';
 
 import { StudentVurdering } from 'components/behandlinger/sykdom/student/studentvurdering/StudentVurdering';
-import { StudentVurderingV2 } from 'components/behandlinger/sykdom/student/studentvurdering/StudentVurderingV2';
 import { ApiException } from 'components/saksbehandling/apiexception/ApiException';
 
 interface Props {
@@ -15,7 +13,6 @@ interface Props {
 
 export const StudentvurderingMedDataFetching = async ({ behandlingsreferanse, stegData }: Props) => {
   const grunnlag = await hentStudentGrunnlag(behandlingsreferanse);
-  const skalBrukeNyttStudentsteg = unleashService.isEnabled('StudentV2');
 
   if (isError(grunnlag)) {
     return <ApiException apiResponses={[grunnlag]} />;
@@ -26,19 +23,9 @@ export const StudentvurderingMedDataFetching = async ({ behandlingsreferanse, st
   }
 
   const totalReadOnly = stegData.readOnly || !grunnlag.data.harTilgangTilÅSaksbehandle;
-  const behovstype = skalBrukeNyttStudentsteg ? Behovstype.AVKLAR_STUDENT_KODE_V2 : Behovstype.AVKLAR_STUDENT_KODE;
+  const behovstype = Behovstype.AVKLAR_STUDENT_KODE_V2;
   const initialMellomlagretVurdering = await hentMellomlagring(behandlingsreferanse, behovstype, totalReadOnly);
 
-  if (skalBrukeNyttStudentsteg) {
-    return (
-      <StudentVurderingV2
-        grunnlag={grunnlag.data}
-        readOnly={stegData.readOnly || !grunnlag.data.harTilgangTilÅSaksbehandle}
-        behandlingVersjon={stegData.behandlingVersjon}
-        initialMellomlagretVurdering={initialMellomlagretVurdering}
-      />
-    );
-  }
   return (
     <StudentVurdering
       grunnlag={grunnlag.data}
