639fcaa35014879c7638cc11a301a945ebf81272
thao1 <thao.than.nguyen@nav.no>
2026-08-13 15:01:58 +0200

    AAP-2142: Lagt til tom/sluttdato for krav vurdering. - Fikset til riktig skjermlink for avslag §11-27 i to trinnskontroll. - La til forklaring for hvordan man legge til krav vurdering for test i revurdering.

--- components/opprettsak/krav/OpprettKravVurdering.tsx
+++ components/opprettsak/krav/OpprettKravVurdering.tsx
@@ -7,14 +7,13 @@ import { SelectWrapper } from 'components/form/selectwrapper/SelectWrapper';
 import { KravType, OpprettSakFormFields } from 'components/opprettsak/OpprettSakLocal';
 import { useFieldArray, UseFormReturn, useWatch } from 'react-hook-form';
 import { formaterDatoForFrontend } from 'lib/utils/date';
-import { subDays, subMonths } from 'date-fns';
+import { startOfMonth, subMonths } from 'date-fns';
 
 interface Props {
   form: UseFormReturn<OpprettSakFormFields>;
 }
 
-const defaultDato = formaterDatoForFrontend(subMonths(new Date(), 3));
-const defaultMuligRettFra = formaterDatoForFrontend(subDays(subMonths(new Date(), 1), 5));
+const defaultDato = formaterDatoForFrontend(startOfMonth(subMonths(new Date(), 3)));
 const kravTyperMedDatofelter: ReadonlySet<KravType> = new Set(['RELEVANT_KRAV']);
 
 const KravRadFelter = ({ form, index }: { form: UseFormReturn<OpprettSakFormFields>; index: number }) => {
@@ -49,7 +48,7 @@ export const OpprettKravVurdering = ({ form }: Props) => {
             rules={{
               onChange: () => {
                 form.setValue(`kravVurderinger.${index}.søknadsdato`, defaultDato);
-                form.setValue(`kravVurderinger.${index}.muligRettFra`, defaultMuligRettFra);
+                form.setValue(`kravVurderinger.${index}.muligRettFra`, defaultDato);
               },
             }}
           >
@@ -82,7 +81,7 @@ export const OpprettKravVurdering = ({ form }: Props) => {
           append({
             kravType: 'RELEVANT_KRAV',
             søknadsdato: defaultDato,
-            muligRettFra: defaultMuligRettFra,
+            muligRettFra: defaultDato,
           })
         }
       >
