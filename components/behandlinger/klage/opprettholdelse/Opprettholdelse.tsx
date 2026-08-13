8e801545e60e41a09ce74fbe67ebb9f3f3483cd9
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2026-08-13 15:47:22 +0200

    AAP-BAU SKal kun vise info om opprettholdelse når klagen er opprettho… (#2418)

--- components/behandlinger/klage/opprettholdelse/Opprettholdelse.tsx
+++ components/behandlinger/klage/opprettholdelse/Opprettholdelse.tsx
@@ -6,6 +6,7 @@ import { StegSuspense } from 'components/stegsuspense/StegSuspense';
 import { hjemmelMap } from 'lib/utils/hjemmel';
 import { BehandlingFlytOgTilstand, Klageresultat } from 'lib/types/types';
 import { VilkårsKort } from 'components/vilkårskort/Vilkårskort';
+import { BodyShort } from '@navikt/ds-react';
 
 interface Props {
   behandlingsreferanse: string;
@@ -19,6 +20,12 @@ export const Opprettholdelse = async ({ behandlingsreferanse, flyt }: Props) => 
     return <ApiException apiResponses={[klageresultat]} />;
   }
 
+  const skalViseOpprettholdelsesInfo =
+    klageresultat.data.type === 'DELVIS_OMGJØRES' || klageresultat.data.type === 'OPPRETTHOLDES';
+  /**
+   * Alle klagesaker har OPPRETTHOLDELSE som siste steg, men det er kun de som har opprettholdelse eller
+   * delvis omgjøring hvor dette er relevant og således aktuelt å vise frem
+   */
   return (
     <GruppeSteg
       prosessering={flyt.prosessering}
@@ -28,11 +35,15 @@ export const Opprettholdelse = async ({ behandlingsreferanse, flyt }: Props) => 
       aktivtSteg={flyt.aktivtSteg}
     >
       <StegSuspense>
-        <VilkårsKort steg={'OPPRETTHOLDELSE'} heading={'Opprettholdelse'}>
-          <p>{utledTekst(flyt)}</p>
-          <p>Følgende vilkår skal opprettholdes:</p>
-          <p>{vilkårSomSkalOpprettholdes(klageresultat.data)}</p>
-        </VilkårsKort>
+        {skalViseOpprettholdelsesInfo ? (
+          <VilkårsKort steg={'OPPRETTHOLDELSE'} heading={'Opprettholdelse'}>
+            <BodyShort>{utledTekst(flyt)}</BodyShort>
+            <BodyShort>Følgende vilkår skal opprettholdes:</BodyShort>
+            <BodyShort>{vilkårSomSkalOpprettholdes(klageresultat.data)}</BodyShort>
+          </VilkårsKort>
+        ) : (
+          <BodyShort>Velg fane for å se innhold i klagesaken</BodyShort>
+        )}
       </StegSuspense>
     </GruppeSteg>
   );
