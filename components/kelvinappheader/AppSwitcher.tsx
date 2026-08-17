e3344146348ba98db3870600d02d26b0ad991520
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-17 14:49:47 +0200

    Logg lenkeklikk i AppSwitcher

--- components/kelvinappheader/AppSwitcher.tsx
+++ components/kelvinappheader/AppSwitcher.tsx
@@ -2,11 +2,12 @@
 
 import { ExternalLinkIcon, MenuGridIcon } from '@navikt/aksel-icons';
 import { Dropdown, InternalHeader } from '@navikt/ds-react';
-import { useEffect, useState } from 'react';
+import { useParamsMedType } from 'hooks/saksbehandling/BehandlingHook';
 import { clientConfig, clientHentAInntektRedirectUrl, clientHentSakPersoninfo } from 'lib/clientApi';
 import { ClientConfig } from 'lib/types/clientTypes';
 import { isError, isSuccess } from 'lib/utils/api';
-import { useParamsMedType } from 'hooks/saksbehandling/BehandlingHook';
+import { loggUmamiEksternLenkeKlikk } from 'lib/utils/umami/lenkeKlikk';
+import { useEffect, useState } from 'react';
 
 export const AppSwitcher = () => {
   const [config, setConfig] = useState<ClientConfig>();
@@ -17,6 +18,7 @@ export const AppSwitcher = () => {
   }, []);
 
   const handleAInntektClick = async (e: React.MouseEvent) => {
+    loggUmamiEksternLenkeKlikk(undefined, 'A-inntekt');
     if (saksnummer) {
       e.preventDefault();
       const response = await clientHentAInntektRedirectUrl(saksnummer);
@@ -27,6 +29,7 @@ export const AppSwitcher = () => {
   };
 
   const handleGosysClick = async (e: React.MouseEvent) => {
+    loggUmamiEksternLenkeKlikk(undefined, 'Gosys');
     if (saksnummer && config?.gosysUrl) {
       e.preventDefault();
       const response = await clientHentSakPersoninfo(saksnummer);
@@ -36,6 +39,7 @@ export const AppSwitcher = () => {
   };
 
   const handleModiaClick = async (e: React.MouseEvent) => {
+    loggUmamiEksternLenkeKlikk(undefined, 'Modia personoversikt');
     if (saksnummer && config?.modiaPersonoversiktUrl) {
       e.preventDefault();
       const response = await clientHentSakPersoninfo(saksnummer);
@@ -44,6 +48,14 @@ export const AppSwitcher = () => {
         : config.modiaPersonoversiktUrl;
       window.open(url, '_blank');
     }
+  };
+
+  const handleKunnskapsbankenClick = () => {
+    loggUmamiEksternLenkeKlikk(undefined, 'Kunnskapsbanken');
+  };
+
+  const handleInst2Click = () => {
+    loggUmamiEksternLenkeKlikk(undefined, 'INST2');
   };
 
   return (
@@ -91,10 +103,17 @@ export const AppSwitcher = () => {
             href={
               'https://navno.sharepoint.com/sites/fag-og-ytelser-Kunnskapsbank-trygdemedisin?xsdata=MDV8MDJ8fGJiOWUzNjg2NDY0ODQ3YzU3ODU3MDhkZTY4N2Y1ZjQzfDYyMzY2NTM0MWVjMzQ5NjI4ODY5OWI1NTM1Mjc5ZDBifDB8MHw2MzkwNjMwOTM0MDE1ODE2NTV8VW5rbm93bnxWR1ZoYlhOVFpXTjFjbWwwZVZObGNuWnBZMlY4ZXlKRFFTSTZJbFJsWVcxelgwRlVVRk5sY25acFkyVmZVMUJQVEU5R0lpd2lWaUk2SWpBdU1DNHdNREF3SWl3aVVDSTZJbGRwYmpNeUlpd2lRVTRpT2lKUGRHaGxjaUlzSWxkVUlqb3hNWDA9fDF8TDJOb1lYUnpMekU1T20xbFpYUnBibWRmV2tSVmQxbDZUWGxOUkVGMFRXcFZNRmw1TURCTmFteHRURlJuZWs5WFVYUk9SMUV5V2xSa2ExbFhWVFZPZWtwcVFIUm9jbVZoWkM1Mk1pOXRaWE56WVdkbGN5OHhOemN3TnpFeU5UTTNPRFUzfDcwMzFmYjg0NmMzMjQ2NTRiZTEyMDhkZTY4N2Y1ZjQyfDI4YjYwODg2NWNjMTQwOTlhYTg2YzczN2EwNDk2Zjc2&sdata=MWJMTWlzTmVudFVzRXhCZkJJdGFEbWJ0QnZQQnh4djlTdWgxUHF1OHp2TT0%3D'
             }
+            onClick={handleKunnskapsbankenClick}
           >
             Kunnskapsbanken <ExternalLinkIcon aria-hidden />
           </Dropdown.Menu.GroupedList.Item>
-          <Dropdown.Menu.GroupedList.Item as="a" target="_blank" href={config?.inst2Url} disabled={!config?.inst2Url}>
+          <Dropdown.Menu.GroupedList.Item
+            as="a"
+            target="_blank"
+            href={config?.inst2Url}
+            disabled={!config?.inst2Url}
+            onClick={handleInst2Click}
+          >
             INST2 <ExternalLinkIcon aria-hidden />
           </Dropdown.Menu.GroupedList.Item>
         </Dropdown.Menu.GroupedList>
