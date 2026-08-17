d19e2595015185bcc3d0a1c6dff32c29d6614a64
Arild Lillegård <arild.lillegard@nav.no>
2026-08-17 16:23:04 +0200

    AAP-2317 Flere endringer for å motta data fra dokumentinnhenting

---
+++ app/saksbehandling/api/dialogmelding/[saksnummer]/dialogmeldinger/route.ts
@@ -0,0 +1,16 @@
+import { clientHentDialogmeldingerForSak } from 'lib/services/dokumentinnhentingservice/dokumentinnhentingservice';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
+import { isLocal } from 'lib/utils/environment';
+import { mockResponseDokumenter } from 'lib/test/local/mockResponseDokumenter';
+
+export async function GET(_: NextApiRequest, props: { params: Promise<{ saksnummer: string }> }) {
+  if (isLocal()) {
+    return NextResponse.json(mockResponseDokumenter, { status: 200 });
+  }
+
+  const params = await props.params;
+  const data = await clientHentDialogmeldingerForSak(params.saksnummer);
+
+  return NextResponse.json(data, { status: 200 });
+}
