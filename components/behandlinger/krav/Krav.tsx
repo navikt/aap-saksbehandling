6be10f485211f7e68495836d438db2921a0ea092
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2026-08-17 14:24:21 +0200

    AAP-2363 Slett gammel krav design

--- components/behandlinger/krav/Krav.tsx
+++ components/behandlinger/krav/Krav.tsx
@@ -4,7 +4,6 @@ import { StÃ¸nadsperiodeMedDataFetching } from 'components/behandlinger/krav/stÃ
 import { GruppeSteg } from 'components/gruppesteg/GruppeSteg';
 import { StegSuspense } from 'components/stegsuspense/StegSuspense';
 import { VurderKravMedDataFetchingV2 } from 'components/behandlinger/krav/vurderkrav/VurderKravMedDataFetchingV2';
-import { VurderKravMedDataFetching } from 'components/behandlinger/krav/VurderKravMedDataFetching';
 
 interface Props {
   behandlingsreferanse: string;
@@ -26,15 +25,6 @@ export const Krav = async ({ behandlingsreferanse, flyt }: Props) => {
       behandlingVersjon={behandlingVersjon}
       aktivtSteg={flyt.aktivtSteg}
     >
-      {stegSomSkalVises.includes('KRAV') && (
-        <StegSuspense>
-          <VurderKravMedDataFetching
-            behandlingsreferanse={behandlingsreferanse}
-            behandlingVersjon={behandlingVersjon}
-            readOnly={flyt.visning.saksbehandlerReadOnly}
-          />
-        </StegSuspense>
-      )}
       {stegSomSkalVises.includes('KRAV') && (
         <StegSuspense>
           <VurderKravMedDataFetchingV2
