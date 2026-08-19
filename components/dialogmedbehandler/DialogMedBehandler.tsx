b13f049f15d9bb4fc4e792fc34e55a949e0040e7
Arild Lillegård <arild.lillegard@nav.no>
2026-08-19 13:41:53 +0200

    AAP-2317 Tilpasninger på nytt dialogdesign
    Gjorde visning av lenkeboksen i hver melding conditional, samt at
    lenkene nå bruker journalpostId og dokumentInfoId.

--- components/dialogmedbehandler/DialogMedBehandler.tsx
+++ components/dialogmedbehandler/DialogMedBehandler.tsx
@@ -1,66 +1,11 @@
 import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { Button, Label, Link, VStack } from '@navikt/ds-react';
+import { Label, Link, VStack } from '@navikt/ds-react';
 
 import { KommendeMeldinger } from 'components/dialogmedbehandler/KommendeMeldinger';
 import { Melding } from 'components/dialogmedbehandler/Melding';
 
 import styles from './DialogMedBehandler.module.css';
 import { useDialogmeldinger } from 'hooks/saksbehandling/SakDialogmeldingerHook';
-
-/*
-interface MeldingDto {
-  visningType: 'INNKOMMENDE' | 'UTGÅENDE';
-  dokumentasjonType: DokumentasjonType;
-  meldingFraNavn: string;
-  opprettetTidspunkt: string;
-  tekst: string;
-  status: 'SENDT' | 'LEVERT' | 'FEILET';
-  journalpostId: string;
-  dokumentIdListe: string[];
-}
-*/
-
-/*
-const innkommendeMeldingerMock: MeldingDto[] = [
-  {
-    visningType: 'INNKOMMENDE',
-    dokumentasjonType: 'MOTTATT_L40',
-    meldingFraNavn: 'Dr. Sonja Paracet',
-    opprettetTidspunkt: '2026-07-12',
-    tekst: 'foo',
-    status: 'LEVERT',
-  },
-];
-
-const utgåendeMeldingerMock: MeldingDto[] = [
-  {
-    visningType: 'UTGÅENDE',
-    dokumentasjonType: 'MELDING_FRA_NAV',
-    meldingFraNavn: 'Nav, Kari Normann',
-    opprettetTidspunkt: '2026-06-18',
-    tekst: 'foo',
-    status: 'LEVERT',
-  },
-  {
-    visningType: 'UTGÅENDE',
-    dokumentasjonType: 'PÅMINNELSE',
-    meldingFraNavn: 'Nav, automatisk',
-    opprettetTidspunkt: '2026-07-09',
-    tekst: 'foo',
-    status: 'FEILET',
-  },
-  {
-    visningType: 'UTGÅENDE',
-    dokumentasjonType: 'RETUR_LEGEERKLÆRING',
-    meldingFraNavn: 'Nav, Kari Normann',
-    opprettetTidspunkt: '2026-06-18',
-    tekst: 'foo',
-    status: 'SENDT',
-  },
-];
-
-const meldingerMock = [...utgåendeMeldingerMock, ...innkommendeMeldingerMock];
-*/
 
 export const DialogMedBehandler = () => {
   const { dialogmeldinger } = useDialogmeldinger();
@@ -92,6 +37,8 @@ export const DialogMedBehandler = () => {
             meldingFraNavn={dialogmelding.meldingFraNavn}
             opprettetTidspunkt={dialogmelding.opprettetTidspunkt?.toString()}
             status={dialogmelding.meldingStatus}
+            journalpostId={dialogmelding.journalpostId}
+            dokumentInfoIdListe={dialogmelding.dokumentIdListe}
           >
             {dialogmelding.tekst}
           </Melding>
@@ -99,10 +46,6 @@ export const DialogMedBehandler = () => {
       </VStack>
 
       <KommendeMeldinger />
-
-      <VStack align={'end'}>
-        <Button variant={'secondary'}>Send forespørsel til behandler</Button>
-      </VStack>
     </section>
   );
 };
