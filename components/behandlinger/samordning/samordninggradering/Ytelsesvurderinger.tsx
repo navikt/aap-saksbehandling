afd1aa4cdc12c177f9778a86bcace937643c3c80
VHollund <vetle.hollund@nav.no>
2026-09-25 11:36:29 +0200

    fjernet unødvendig kommentar
    Signed-off-by: VHollund <vetle.hollund@nav.no>

--- components/behandlinger/samordning/samordninggradering/Ytelsesvurderinger.tsx
+++ components/behandlinger/samordning/samordninggradering/Ytelsesvurderinger.tsx
@@ -118,8 +118,6 @@ export const Ytelsesvurderinger = ({ form, readOnly, fieldArray, grunnlag }: Pro
   function lagreFerieRad(verdier: FerieFormFields) {
     const rad: SamordnetYtelse = {
       periode: { fom: verdier.fom, tom: verdier.tom },
-      // Ferie i sykepengeperioden skal samordnes som 100 % sykepenger, siden sykepengeretten
-      // fortsatt løper (og dermed gi avslag på AAP etter § 11-27, jf. samordningsregelverket).
       gradering: 100,
       ytelseType: 'FERIE_I_SYKEPENGEPERIODE',
       manuell: true,
